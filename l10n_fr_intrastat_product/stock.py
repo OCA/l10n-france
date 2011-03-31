4c820b5764d687773cd72a5b594c656c15f40546
Alexis de Lattre <alexis@via.ecp.fr>
2011-04-01 00:36:23 +0200

    Started work on l10n_fr_intrastat_product

---
+++ l10n_fr_intrastat_product/stock.py
@@ -0,0 +1,40 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Report intrastat product module for OpenERP
+#    Copyright (C) 2010-2011 Akretion (http://www.akretion.com). All Rights Reserved
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from osv import osv, fields
+from tools.translate import _
+
+class stock_picking(osv.osv):
+    _inherit = "stock.picking"
+    _columns = {
+        'intrastat_transport' : fields.selection([(1, 'Transport maritime'), \
+            (2, 'Transport par chemin de fer'), \
+            (3, 'Transport par route'), \
+            (4, 'Transport par air'), \
+            (5, 'Envois postaux'), \
+            (7, 'Installations de transport fixes'), \
+            (8, 'Transport par navigation intérieure'), \
+            (9, 'Propulsion propre')], 'Type of transport', \
+            help="Select the type of transport. This information is required for the product intrastat report.")
+            }
+
+stock_picking()
+
