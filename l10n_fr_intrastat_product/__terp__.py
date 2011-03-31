cae256d8437af7786622e62a349df72a8eaa6bde
Alexis de Lattre <alexis@via.ecp.fr>
2011-04-01 00:36:23 +0200

    Started work on l10n_fr_intrastat_product

---
+++ l10n_fr_intrastat_product/__terp__.py
@@ -0,0 +1,43 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Report intrastat product module for OpenERP
+#    Copyright (C) 2010-2011 Akretion (http://www.akretion.com). All Rights Reserved
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+
+{
+    'name': 'Module for Intrastat product reporting (DEB) for France',
+    'version': '1.1',
+    'category': 'Localisation/Report Intrastat',
+    'license': 'AGPL-3',
+    'description': """This module adds support for the "Déclaration d'Echange de Biens" (DEB).
+Please contact Alexis de Lattre from Akretion for any help or question.
+    """,
+    'author': 'Akretion',
+    'website': 'http://www.akretion.com',
+    'depends': ['l10n_fr_intrastat_base', 'stock'],
+    'init_xml': [],
+    'update_xml': [
+        'security/ir.model.access.csv',
+#        'intrastat_product_view.xml',
+        'company_view.xml',
+    ],
+    'demo_xml': [],
+    'installable': True,
+    'active': False,
+}
