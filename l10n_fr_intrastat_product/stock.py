c124a3870ab13dba7644695c752d19434fd7481e
Alexis de Lattre <alexis@via.ecp.fr>
2011-04-03 17:37:07 +0200

    More work on full DEB+DES implementation. Added full support for "transport type" and partial support for the department. More factorisation between product & service module. Add date_done field. More checks.

--- l10n_fr_intrastat_product/stock.py
+++ l10n_fr_intrastat_product/stock.py
@@ -3,6 +3,7 @@
 #
 #    Report intrastat product module for OpenERP
 #    Copyright (C) 2010-2011 Akretion (http://www.akretion.com). All Rights Reserved
+#    @author Alexis de Lattre <alexis.delattre@akretion.com>
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -37,6 +38,21 @@ class stock_picking(osv.osv):
             }
 
 # Do we put a default value, taken from res_company ?
+
+    def action_invoice_create(self, cr, uid, ids, journal_id=False,
+        group=False, type='out_invoice', context=None):
+        '''Copy transport from picking to invoice'''
+
+        res = super(stock_picking, self).action_invoice_create(cr, uid, ids,
+            journal_id=journal_id, group=group, type=type, context=context)
+        invoice_obj = self.pool.get('account.invoice')
+        for picking_id in res:
+            picking = self.read(cr, uid, picking_id, ['intrastat_transport'], context=context)
+            if picking['intrastat_transport']:
+                invoice_obj.write(cr, uid, res[picking_id], {
+                    'intrastat_transport': picking['intrastat_transport'],
+                 }, context=context)
+        return res
 
 stock_picking()
 
