bab31da41666ee925bd62710c1672fa6ae4136b8
Alexis de Lattre <alexis@via.ecp.fr>
2011-04-04 11:21:00 +0200

    Converted some fields on report_intrastat_product/service to many2one (fields that are not in the official declaration or never subject to change) Add copy of department from warehouse to invoice. Import DEB don't have the partner_vat field Department can be 99 for Monaco. Copy tranport and department from report_intrastat_product in XML file First attempt at adapting the display of fields depending on the type of declaration.

--- l10n_fr_intrastat_product/stock.py
+++ l10n_fr_intrastat_product/stock.py
@@ -21,7 +21,26 @@
 ##############################################################################
 
 from osv import osv, fields
-from tools.translate import _
+
+
+class stock_warehouse(osv.osv):
+    _inherit = "stock.warehouse"
+    _columns = {
+        'intrastat_department' : fields.char('Department', size=2, help="France's department where the warehouse is located. This parameter is required for the DEB (Déclaration d'Echange de Biens)."),
+            }
+
+    def _check_intrastat_department(self, cr, uid, ids):
+        dpt_list = []
+        for dpt_to_check in self.read(cr, uid, ids, ['intrastat_department']):
+            dpt_list.append(dpt_to_check['intrastat_department'])
+        return self.pool.get('res.company').real_department_check(dpt_list)
+
+    _constraints = [
+        (_check_intrastat_department, "Error msg is in raise", ['intrastat_department']),
+    ]
+
+stock_warehouse()
+
 
 class stock_picking(osv.osv):
     _inherit = "stock.picking"
@@ -47,11 +66,32 @@ class stock_picking(osv.osv):
             journal_id=journal_id, group=group, type=type, context=context)
         invoice_obj = self.pool.get('account.invoice')
         for picking_id in res:
-            picking = self.read(cr, uid, picking_id, ['intrastat_transport'], context=context)
-            if picking['intrastat_transport']:
+            picking = self.browse(cr, uid, picking_id, context=context)
+            if picking.intrastat_transport:
                 invoice_obj.write(cr, uid, res[picking_id], {
-                    'intrastat_transport': picking['intrastat_transport'],
+                    'intrastat_transport': picking.intrastat_transport,
                  }, context=context)
+
+            location_to_search = False
+            if picking.move_lines:
+                if picking.type == 'out':
+                    location_to_search = picking.move_lines[0].location_id
+                if picking.type == 'in':
+                    location_to_search = picking.move_lines[0].location_dest_id
+            print "location_to_search =", location_to_search
+            warehouse_obj = self.pool.get('stock.warehouse')
+            warehouse_all_ids = warehouse_obj.search(cr, uid, [], context=context)
+            dpt_to_copy = False
+            for warehouse in warehouse_obj.browse(cr, uid, warehouse_all_ids, context=context):
+                if (picking.type == 'out' and location_to_search == warehouse.lot_stock_id) or (picking.type == 'in' and location_to_search == warehouse.lot_input_id):
+                    dpt_to_copy = warehouse.intrastat_department
+                    print "dpt_to_copy=", dpt_to_copy
+                    break
+            if dpt_to_copy:
+                invoice_obj.write(cr, uid, res[picking_id], {
+                    'intrastat_department': dpt_to_copy,
+                }, context=context)
+
         return res
 
 stock_picking()
