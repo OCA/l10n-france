b4d727aa45145bf3b6f5391e555d7204398aad7a
Alexis de Lattre <alexis@via.ecp.fr>
2011-04-06 01:32:39 +0200

    Renamed some fields Now manages the specifications of the different procedure_code, for example some info don't need to be declared for some procedure codes. Ability to enter DEB lines manually Stat value doesn't exist any more Tree vue of DEB lines is now identical to Cerfa

--- l10n_fr_intrastat_product/intrastat_type.py
+++ l10n_fr_intrastat_product/intrastat_type.py
@@ -20,23 +20,24 @@
 ##############################################################################
 
 from osv import osv, fields
-
+from tools.translate import _
 
 class report_intrastat_type(osv.osv):
     _name = "report.intrastat.type"
     _description = "Intrastat type"
-    _order = "name"
+    _order = "procedure_code, transaction_code"
     _columns = {
-        'name': fields.char('Name',size=64,help="Name which appear when you select the Intrastat type on the invoice."),
+        'name': fields.char('Name', size=64, help="Description of the Intrastat type."),
         'active' : fields.boolean('Active', help="The active field allows you to hide the Intrastat type without deleting it."),
         'invoice_type' : fields.selection([
-            ('out_invoice','Customer Invoice'),
-            ('in_invoice','Supplier Invoice'),
-            ('out_refund','Customer Refund'),
-            ('in_refund','Supplier Refund'),
+            ('out_invoice', 'Customer Invoice'),
+            ('in_invoice', 'Supplier Invoice'),
+            ('out_refund', 'Customer Refund'),
+            ('none', 'None'),
         ], 'Invoice type', select=True),
-        'procedure_code': fields.integer('Procedure code', required=True, help="For the 'DEB' declaration to France's customs administration, you should enter the 'code régime' here."),
-        'transaction_code': fields.integer('Transaction code', required=True, help="For the 'DEB' declaration to France's customs administration, you should enter the number 'nature de la transaction' here."),
+        'procedure_code': fields.char('Procedure code', required=True, size=2, help="For the 'DEB' declaration to France's customs administration, you should enter the 'code régime' here."),
+        'transaction_code': fields.char('Transaction code', size=2, help="For the 'DEB' declaration to France's customs administration, you should enter the number 'nature de la transaction' here."),
+        'is_fiscal_only': fields.boolean('Is fiscal only ?', help="Only fiscal data should be provided for this procedure code."),
     }
 
     _defaults = {
@@ -44,21 +45,39 @@ class report_intrastat_type(osv.osv):
     }
 
     _sql_constraints = [
-        ('invoice_type_uniq', 'unique(invoice_type)', 'You can only create one intrastat type per invoice type !'),
+        ('code_invoice_type_uniq', 'unique(procedure_code, transaction_code)', 'The pair (procedure code, transaction code) must be unique.'),
     ]
 
+    def real_code_check(self, codes):
+        if not codes['procedure_code']:
+            raise osv.except_osv(_('Error :'), _('You must enter a value for the procedure code.'))
+        if not codes['procedure_code'].isdigit():
+            raise osv.except_osv(_('Error :'), _('Procedure code must be a number.'))
+        if len(codes['procedure_code']) != 2:
+            raise osv.except_osv(_('Error :'), _('Procedure code must have 2 digits.'))
+        if codes['transaction_code']:
+            if not codes['transaction_code'].isdigit():
+                raise osv.except_osv(_('Error :'), _('Transaction code must be a number.'))
+            if len(codes['transaction_code']) != 2:
+                raise osv.except_osv(_('Error :'), _('Transaction code must have 2 digits.'))
+        return True
+
+
     def _code_check(self, cr, uid, ids):
-    # Procedure_code and transaction codes are an integers, so they always have a value
-        for intrastat_type in self.read(cr, uid, ids, ['procedure_code', 'transaction_code']):
-            if not 10 <= intrastat_type['procedure_code'] <= 99:
-                raise osv.except_osv(_('Error :'), _('Procedure code must be between 10 and 99'))
-            if not 10 <= intrastat_type['transaction_code'] <= 99:
-                raise osv.except_osv(_('Error :'), _('Transaction code must be between 10 and 99'))
+        for intrastat_type in self.read(cr, uid, ids, ['procedure_code', 'transaction_code', 'is_fiscal_only']):
+            self.real_code_check(intrastat_type)
         return True
 
     _constraints = [
         (_code_check, "Error msg in raise", ['procedure_code', 'transaction_code']),
     ]
+
+    def is_fiscal_only_on_change(self, cr, uid, ids, is_fiscal_only=False):
+        result = {}
+        result['value'] = {}
+        if is_fiscal_only:
+            result['value'].update({'transaction_code': False})
+        return result
 
 report_intrastat_type()
 
