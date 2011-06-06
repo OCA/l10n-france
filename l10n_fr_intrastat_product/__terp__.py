0d351254c0fd159a1ceb7d4bd1488772f6f055e0
Alexis de Lattre <alexis@via.ecp.fr>
2011-06-06 20:42:04 +0200

    . Implemented the fiscal representative : for example, when you ship to the EU but invoice outside of the EU, your customer needs to have a fiscal representative inside the EU, which will be used for the DEB . depend on base_vat instead of account.

--- l10n_fr_intrastat_product/__terp__.py
+++ l10n_fr_intrastat_product/__terp__.py
@@ -41,6 +41,7 @@ Please contact Alexis de Lattre from Akretion <alexis.delattre@akretion.com> for
         'intrastat_product_view.xml',
         'intrastat_type_view.xml',
         'company_view.xml',
+        'partner_view.xml',
         'product_view.xml',
         'stock_view.xml',
         'invoice_view.xml',
