876c2688ae1817f8e11edf284a5cd84d5fc8e7e1
Sylvain LE GAL <sylvain.legal@grap.coop>
2014-03-31 12:20:53 +0200

    change csv file to yml file [REF] pep8 compliance [REF] adding licence information on all files. [ADD] Sql constraint on res_country_department.code field.

--- l10n_fr_department/__openerp__.py
+++ l10n_fr_department/__openerp__.py
@@ -1,51 +1,64 @@
 # -*- encoding: utf-8 -*-
-################################################################################
-#    See Copyright and Licence Informations undermentioned.
-################################################################################
+##############################################################################
+#
+#    l10n FR Departments module for OpenERP
+#    Copyright (C) 2013-2014 GRAP (http://www.grap.coop)
+#    @author Sylvain LE GAL (https://twitter.com/legalsylvain)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
 {
-    'name': 'French Departments (Département)',
+    'name': 'French Departments (Départements)',
+    'summary': 'Populate Database with French Departments (Départements)',
     'version': '0.1',
     'category': 'base',
     'description': """
-Add French departments data
-===========================
+Populate Database with French Departments (Départements)
+========================================================
 
 Feature:
 --------
-    * Create a new model res_country_department, sub division of the res_country_state;
+    * Create a new model res_country_department, sub division of the """
+    """res_country_state;
     * Populate the table res_country_department with the french departments;
 
 Technical informations:
 -----------------------
-    * Use 3166-2:FR codifications (more detail http://fr.wikipedia.org/wiki/ISO_3166-2:FR);
+    * Use 3166-2:FR codifications (more detail"""
+    """http://fr.wikipedia.org/wiki/ISO_3166-2:FR);
 
 Copyright, Authors and Licence:
 -------------------------------
     * Copyright: 2013, Groupement Régional Alimentaire de Proximité;
     * Author: Sylvain LE GAL (https://twitter.com/legalsylvain);
-    * Licence: AGPL-3 (http://www.gnu.org/licenses/);
-    """,
+    * Licence: AGPL-3 (http://www.gnu.org/licenses/);""",
     'author': 'GRAP',
     'website': 'http://www.grap.coop',
     'license': 'AGPL-3',
     'depends': [
         'l10n_fr_state',
-        ],
+    ],
     'data': [
-            'security/ir.model.access.csv',
-            'data/res_country_department_data.yml',
-            'view/view.xml',
-            'view/action.xml',
-            'view/menu.xml',
-        ],
-    'demo': [],
-    'js': [],
-    'css': [],
-    'qweb': [],
-    'images': [],
-    'post_load': '',
-    'application': False,
-    'installable': True,
-    'auto_install': False,
-    'images': ['static/src/img/screenshots/1.png'],
+        'security/ir_model_access.yml',
+        'data/res_country_department_data.yml',
+        'view/view.xml',
+        'view/action.xml',
+        'view/menu.xml',
+    ],
+    'images': [
+        'static/src/img/screenshots/1.png'
+    ],
 }
