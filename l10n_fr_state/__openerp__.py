e089c8255dd9dee639a42a90756dbdcba0564fc1
Sylvain LE GAL <sylvain.legal@grap.coop>
2014-03-31 10:42:01 +0200

    Pep8 and licences.

--- l10n_fr_state/__openerp__.py
+++ l10n_fr_state/__openerp__.py
@@ -1,14 +1,33 @@
 # -*- encoding: utf-8 -*-
-################################################################################
-#    See Copyright and Licence Informations undermentioned.
-################################################################################
+##############################################################################
+#
+#    l10n FR States module for OpenERP
+#    Copyright (C) 2013-2014 GRAP (http://www.grap.coop)
+#    @author Sylvain LE GAL (https://twitter.com/legalsylvain)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
 {
     'name': 'French States (Région)',
+    'summary': "Populate Database with French States (Région)",
     'version': '0.1',
     'category': 'base',
     'description': """
-Add French states data
-======================
+Populate Database with French States (Région)
+=============================================
 
 Feature:
 --------
@@ -22,7 +41,7 @@ Technical information:
 
 Copyright, Authors and Licence:
 -------------------------------
-    * Copyright: 2013, Groupement Régional Alimentaire de Proximité;
+    * Copyright: 2013-2014, GRAP, Groupement Régional Alimentaire de Proximité;
     * Author: Sylvain LE GAL (https://twitter.com/legalsylvain);
     * Licence: AGPL-3 (http://www.gnu.org/licenses/);
     """,
@@ -31,20 +50,13 @@ Copyright, Authors and Licence:
     'license': 'AGPL-3',
     'depends': [
         'base',
-        ],
+    ],
     'data': [
-            'data/res_country_state_data.yml',
-        ],
-    'demo': [],
-    'js': [],
-    'css': [],
-    'qweb': [],
-    'images': [],
-    'post_load': '',
-    'application': False,
+        'data/res_country_state_data.yml',
+    ],
     'installable': True,
     'auto_install': False,
     'images': [
-            'static/src/img/screenshots/1.png'
-        ],
+        'static/src/img/screenshots/1.png'
+    ],
 }
