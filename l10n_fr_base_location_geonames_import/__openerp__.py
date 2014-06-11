fe98826f30040e952b4000fa0b97b732e9a9a4ae
Alexis de Lattre <alexis@via.ecp.fr>
2014-06-11 09:39:39 +0200

    Add France-related territories where La Postale standards apply.

--- l10n_fr_base_location_geonames_import/__openerp__.py
+++ l10n_fr_base_location_geonames_import/__openerp__.py
@@ -31,7 +31,7 @@
 French Localization Base Location Geonames Import
 =================================================
 
-This module adds some France-specific tuning for the wizard that imports better zip entries from Geonames (http://download.geonames.org/export/zip/). This wizard is provided by the module base_location_geonames_import from the projet *partner-contact-management*. This tuning aims at complying with France's postal standards published by *La Poste*.
+This module adds some France-specific tuning for the wizard that imports better zip entries from Geonames (http://download.geonames.org/export/zip/). This wizard is provided by the module base_location_geonames_import from the projet *partner-contact-management*. This tuning aims at complying with France's postal standards published by *La Poste*. This tuning will be applied for France and France-related territories where *La Poste* postal standards apply.
 
 Please contact Alexis de Lattre from Akretion <alexis.delattre@akretion.com> for any help or question about this module.
     """,
