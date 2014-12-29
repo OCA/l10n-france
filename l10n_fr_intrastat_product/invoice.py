f7c5a472328441e6d00042e60511741b66ffe65d
Alexis de Lattre <alexis@via.ecp.fr>
2014-12-29 10:08:42 +0100

    Continue the move to the new API/flake8

--- l10n_fr_intrastat_product/invoice.py
+++ l10n_fr_intrastat_product/invoice.py
@@ -1,7 +1,7 @@
 # -*- encoding: utf-8 -*-
 ##############################################################################
 #
-#    Report intrastat product module for OpenERP
+#    Report intrastat product module for Odoo
 #    Copyright (C) 2010-2014 Akretion (http://www.akretion.com)
 #    @author Alexis de Lattre <alexis.delattre@akretion.com>
 #
@@ -20,48 +20,41 @@
 #
 ##############################################################################
 
-from openerp.osv import orm, fields
+from openerp import models, fields, api
 
 
-class account_invoice(orm.Model):
+class AccountInvoice(models.Model):
     _inherit = "account.invoice"
-    _columns = {
-        'intrastat_transport': fields.selection([
-            (1, 'Transport maritime'),
-            (2, 'Transport par chemin de fer'),
-            (3, 'Transport par route'),
-            (4, 'Transport par air'),
-            (5, 'Envois postaux'),
-            (7, 'Installations de transport fixes'),
-            (8, 'Transport par navigation intérieure'),
-            (9, 'Propulsion propre')
-            ], 'Type of transport',
-            help="Type of transport of the goods. This information is "
-            "required for the product intrastat report (DEB)."),
-        'intrastat_department': fields.char(
-            'Department', size=2,
-            help="For a customer invoice, contains France's department "
-            "number from which the goods have be shipped. For a supplier "
-            "invoice, contains France's department number of reception "
-            "of the goods. This information is required for the product "
-            "intrastat report (DEB)."),
-        'intrastat_country_id': fields.many2one(
-            'res.country', 'Destination/Origin country of the goods',
-            help="For a customer invoice, contains the country to which "
-            "the goods have been shipped. For a supplier invoice, contains "
-            "the country from which the goods have been shipped."),
-        'intrastat_type_id': fields.many2one(
-            'report.intrastat.type', 'Intrastat type', ondelete='restrict'),
-        }
 
-    def _check_intrastat_department(self, cr, uid, ids):
-        dpt_list = []
-        for dpt_to_check in self.read(cr, uid, ids, ['intrastat_department']):
-            dpt_list.append(dpt_to_check['intrastat_department'])
-        return self.pool.get('res.company').real_department_check(dpt_list)
+    intrastat_transport = fields.Selection([
+        (1, 'Transport maritime'),
+        (2, 'Transport par chemin de fer'),
+        (3, 'Transport par route'),
+        (4, 'Transport par air'),
+        (5, 'Envois postaux'),
+        (7, 'Installations de transport fixes'),
+        (8, 'Transport par navigation intérieure'),
+        (9, 'Propulsion propre')
+        ], string='Type of transport',
+        help="Type of transport of the goods. This information is "
+        "required for the product intrastat report (DEB).")
+    intrastat_department = fields.Char(
+        string='Department', size=2,
+        help="For a customer invoice, contains France's department "
+        "number from which the goods have be shipped. For a supplier "
+        "invoice, contains France's department number of reception "
+        "of the goods. This information is required for the product "
+        "intrastat report (DEB).")
+    intrastat_country_id = fields.Many2one(
+        'res.country', string='Destination/Origin country of the goods',
+        help="For a customer invoice, contains the country to which "
+        "the goods have been shipped. For a supplier invoice, contains "
+        "the country from which the goods have been shipped.")
+    intrastat_type_id = fields.Many2one(
+        'report.intrastat.type', string='Intrastat type', ondelete='restrict')
 
-    _constraints = [(
-        _check_intrastat_department,
-        "Wrong Department",
-        ['intrastat_department']
-        )]
+    @api.one
+    @api.constrains('intrastat_department')
+    def _check_intrastat_department(self):
+        self.env['res.company'].real_department_check(
+            self.intrastat_department)
--- l10n_fr_intrastat_product/partner.py
+++ l10n_fr_intrastat_product/partner.py
@@ -1,7 +1,7 @@
 # -*- encoding: utf-8 -*-
 ##############################################################################
 #
-#    Report intrastat product module for OpenERP
+#    Report intrastat product module for Odoo
 #    Copyright (C) 2011-2014 Akretion (http://www.akretion.com)
 #    @author Alexis de Lattre <alexis.delattre@akretion.com>
 #
@@ -20,65 +20,49 @@
 #
 ##############################################################################
 
-from openerp.osv import orm, fields
-from openerp.tools.translate import _
+from openerp import models, fields, api, _
+from openerp.exceptions import ValidationError
 
 
-class res_partner(orm.Model):
+class ResPartner(models.Model):
     _inherit = "res.partner"
-    _columns = {
-        'intrastat_fiscal_representative': fields.many2one(
-            'res.partner', string="EU fiscal representative",
-            help="If this partner is located outside the EU but you "
-            "deliver the goods inside the UE, the partner needs to "
-            "have a fiscal representative with a VAT number inside the EU. "
-            "In this scenario, the VAT number of the fiscal representative "
-            "will be used for the Intrastat Product report (DEB)."),
-    }
+
+    intrastat_fiscal_representative = fields.Many2one(
+        'res.partner', string="EU fiscal representative",
+        help="If this partner is located outside the EU but you "
+        "deliver the goods inside the UE, the partner needs to "
+        "have a fiscal representative with a VAT number inside the EU. "
+        "In this scenario, the VAT number of the fiscal representative "
+        "will be used for the Intrastat Product report (DEB).")
 
     # Copy field 'intrastat_fiscal_representative' from company partners
     # to their contacts
-    def _commercial_fields(self, cr, uid, context=None):
-        res = super(res_partner, self)._commercial_fields(
-            cr, uid, context=context)
+    @api.model
+    def _commercial_fields(self):
+        res = super(ResPartner, self)._commercial_fields()
         res.append('intrastat_fiscal_representative')
         return res
 
-    def _check_fiscal_representative(self, cr, uid, ids):
+    @api.one
+    @api.constrains('intrastat_fiscal_representative')
+    def _check_fiscal_representative(self):
         '''The Fiscal rep. must be based in the same country as our '''
         '''company or in an intrastat country'''
-        user = self.pool.get('res.users').browse(cr, uid, uid)
-        my_company_country_id = user.company_id.partner_id.country_id.id or False
-        for partner in self.browse(cr, uid, ids):
-            if partner.intrastat_fiscal_representative:
-                if not partner.intrastat_fiscal_representative.country_id:
-                    raise orm.except_orm(
-                        _('Error :'),
-                        _("The fiscal representative '%s' of partner '%s' "
-                            "must have a country.")
-                        % (partner.intrastat_fiscal_representative.name,
-                            partner.name))
-                if (not partner.intrastat_fiscal_representative.country_id.
-                        intrastat
-                        and partner.intrastat_fiscal_representative.country_id.id
-                        != my_company_country_id):
-                    raise orm.except_orm(
-                        _('Error :'),
-                        _("The fiscal representative '%s' of partner '%s' "
-                            "must be based in an EU country.")
-                        % (partner.intrastat_fiscal_representative.name,
-                            partner.name))
-                if not partner.intrastat_fiscal_representative.vat:
-                    raise orm.except_orm(
-                        _('Error :'),
-                        _("The fiscal representative '%s' of partner '%s' "
-                            "must have a VAT number.")
-                        % (partner.intrastat_fiscal_representative.name,
-                            partner.name))
-        return True
-
-    _constraints = [(
-        _check_fiscal_representative,
-        "Wrong configuration of the EU fiscal representative",
-        ['intrastat_fiscal_representative']
-        )]
+        if self.intrastat_fiscal_representative:
+            if not self.intrastat_fiscal_representative.country_id:
+                raise ValidationError(
+                    _("The fiscal representative '%s' of partner '%s' "
+                        "must have a country.")
+                    % (self.intrastat_fiscal_representative.name, self.name))
+            if (not self.intrastat_fiscal_representative.country_id.intrastat
+                    and self.intrastat_fiscal_representative.country_id
+                    != self.env.user.company_id.partner_id.country_id):
+                raise ValidationError(
+                    _("The fiscal representative '%s' of partner '%s' "
+                        "must be based in an EU country.")
+                    % (self.intrastat_fiscal_representative.name, self.name))
+            if not self.intrastat_fiscal_representative.vat:
+                raise ValidationError(
+                    _("The fiscal representative '%s' of partner '%s' "
+                        "must have a VAT number.")
+                    % (self.intrastat_fiscal_representative.name, self.name))
