3e8247c7b82478d1c20bd37a5e26ec1b19a82b31
Ronald Portier <ronald@therp.nl>
2015-02-04 23:31:50 +0100

    [FIX] Use new API to prevent pylint errors and streamline code.

--- l10n_nl_intrastat/model/l10n_nl_intrastat.py
+++ l10n_nl_intrastat/model/l10n_nl_intrastat.py
@@ -24,7 +24,7 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-from openerp import models, fields
+from openerp import api, models, fields
 from openerp.exceptions import except_orm
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
@@ -106,18 +106,25 @@ class ReportIntrastat(models.Model):
         readonly=True,
     )
 
-    def set_draft(self, cr, uid, ids, context=None):
+    @api.one
+    def set_draft(self):
         """
         Reset report state to draft.
         """
-        return self.write(
-            cr, uid, ids, {'state': 'draft'}, context=context)
+        return self.write({'state': 'draft'})
 
-    def generate_lines(self, cr, uid, ids, context=None):
+    @api.one
+    def generate_lines(self):
         """
         Collect the data lines for the given report.
         Unlink any existing lines first.
         """
+        # Generating lines is only allowed for report in draft state:
+        if self.state != 'draft':
+            raise except_orm(
+                _('Error'),
+                _('Cannot generate reports lines in a non-draft state'))
+        # Other models:
         report_line_model = self.env['l10n_nl.report.intrastat.line']
         currency_model = self.env['res.currency']
         invoice_model = self.env['account.invoice']
@@ -125,78 +132,63 @@ class ReportIntrastat(models.Model):
 
         total_amount = 0.0
         partner_amounts_map = {}
-        localcontext = context and context.copy() or {}
-
-        # Generating lines is only allowed for report in draft state:
-        report_obj = self.browse(cr, uid, ids[0], context=context)
-        if report_obj.state != 'draft':
-            raise except_orm(
-                _('Error'),
-                _('Cannot generate reports lines in a non-draft state'))
-
         # Check wether all configuration done to generate report
-        self.env['report.intrastat.common'].check_generate_lines(
-            cr, uid, report_obj, context=context)
+        self.env['report.intrastat.common'].check_generate_lines(self)
 
         # Remove existing lines
-        line_ids = report_line_model.search(
-            cr, uid, [('report_id', '=', ids[0])], context=context)
-        report_line_model.unlink(cr, uid, line_ids, context=context)
-
-        company_obj = report_obj.company_id  # simplify access
+        for line_obj in self.line_ids:
+            line_obj.unlink()
+        # Define search for invoices for period and company:
+        company_obj = self.company_id  # simplify access
         invoice_domain = [
             ('type', 'in', ('out_invoice', 'out_refund')),
-            ('period_id', '=', report_obj.period_id.id),
+            ('period_id', '=', self.period_id.id),
             ('state', 'in', ('open', 'paid')),
             ('company_id', '=', company_obj.id),
-            ('partner_id.country_id.id', '!=', company_obj.country_id.id),
         ]
-
         # Signal invoices without a country
-        invalid_invoice_ids = invoice_model.search(
-            cr, uid,
-            invoice_domain + [
-                ('partner_id.country_id', '=', False)],
-            context=context)
-        if invalid_invoice_ids:
-            invoices = invoice_model.read(
-                cr, uid,
-                invalid_invoice_ids, ['partner_id'],
-                context=context
-            )
+        # (Should not happen, as country_id on parter is set to required)
+        invalid_invoices = invoice_model.search_read(
+            domain=invoice_domain + [('partner_id.country_id', '=', False)],
+            fields=['partner_id']
+        )
+        if invalid_invoices:
             raise except_orm(
                 _('Error'),
                 _(
                     "Missing country on the invoice addresses of the"
                     " following partners:\n%s"
-                ) % "\n".join(inv['partner_id'][1] for inv in invoices)
+                ) % "\n".join(
+                    inv['partner_id'][1] for inv in invalid_invoices)
             )
-
-        invoice_ids = invoice_model.search(
-            cr, uid,
+        # Search invoices that need intrastat reporting:
+        invoice_records = invoice_model.search(
             invoice_domain + [
-                ('partner_id.country_id.intrastat', '=', True)],
-            context=context
+                ('partner_id.country_id.intrastat', '=', True),
+                ('partner_id.country_id.id', '!=', company_obj.country_id.id),
+            ]
         )
-        invoice_line_ids = invoice_line_model.search(
-            cr, uid, [('invoice_id', 'in', invoice_ids)], context=context)
+        invoice_ids = [inv['id'] for inv in invoice_records]
+        invoice_line_records = invoice_line_model.search(
+            [('invoice_id', 'in', invoice_ids)])
 
         # Gather amounts from invoice lines
-        for line in invoice_line_model.browse(
-                cr, uid, invoice_line_ids, context=context):
+        for line in invoice_line_records:
+            # Ignore invoiceline if taxes should not be included in intrastat:
             if any(
                     tax.exclude_from_intrastat_if_present
                     for tax in line.invoice_line_tax_id):
                 continue
-            localcontext['date'] = line.invoice_id.date_invoice
+            # Report is per commercial partner:
             commercial_partner_id = (
                 line.invoice_id.partner_id.commercial_partner_id.id)
             if commercial_partner_id not in partner_amounts_map:
                 partner_amounts_map[commercial_partner_id] = {
                     'amount_product': 0.0,
                     'amount_service': 0.0,
-                    }
+                }
             amounts = partner_amounts_map[commercial_partner_id]
+            # Determine product or service:
             if (line.product_id
                     and (
                         line.product_id.type == 'service'
@@ -206,19 +198,18 @@ class ReportIntrastat(models.Model):
                 amount_type = 'amount_product'
             sign = line.invoice_id.type == 'out_refund' and -1 or 1
             amount = sign * line.price_subtotal
-
-            # Convert currency amount if necesarry:
-            line_currency_id = line.invoice_id.currency_id.id  # simplify
-            if (line_currency_id
-                    and line_currency_id != company_obj.currency_id.id):
-                amount = currency_model.compute(
-                    cr, uid, line_currency_id,
-                    company_obj.currency_id.id,
-                    amount, context=localcontext
+            # Convert currency amount if necessary:
+            line_currency_obj = line.invoice_id.currency_id  # simplify
+            invoice_date = line.invoice_id.date_invoice
+            if (line_currency_obj
+                    and line_currency_obj.id != company_obj.currency_id.id):
+                amount = (
+                    line_currency_obj.with_context(date=invoice_date).compute(
+                        amount, company_obj.currency_id, round=True)
                 )
-
-            amounts[amount_type] += amount
-            total_amount += amount
+            # Accumulate totals:
+            amounts[amount_type] += amount  # per partner and type
+            total_amount += amount  # grand total
 
         # Create report lines
         for (partner_id, vals) in partner_amounts_map.items():
@@ -226,19 +217,15 @@ class ReportIntrastat(models.Model):
                 continue
             vals.update({
                 'partner_id': partner_id,
-                'report_id': report_obj.id
+                'report_id': self.id
             })
-            report_line_model.create(
-                cr, uid, vals, context=context)
+            report_line_model.create(vals)
 
-        return self.write(
-            cr, uid, report_obj.id, {
-                'total_amount': total_amount,
-                'date_done': fields.Date.today(),
-                'state': 'done',
-            },
-            context=context
-        )
+        return self.write({
+            'total_amount': total_amount,
+            'date_done': fields.Date.today(),
+            'state': 'done',
+        })
 
     def unlink(self, cr, uid, ids, context=None):
         """
@@ -304,3 +291,5 @@ class ReportIntrastatLine(models.Model):
         readonly=True,
         digits_compute=dp.get_precision('Account'),
     )
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
