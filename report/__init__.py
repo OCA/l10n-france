94ed3357a54f00e979c1e390b2ce8a052c33d8a0
Alexis de Lattre <alexis@via.ecp.fr>
2015-03-30 12:20:15 +0200

    Add file README.rst OpenERP renamed to Odoo Add OCA as author

--- report/__init__.py
+++ report/__init__.py
@@ -1,8 +1,8 @@
 # -*- encoding: utf-8 -*-
 ##############################################################################
 #
-#    Account Trial Balance CSV module for OpenERP
-#    Copyright (C) 2014 Akretion (http://www.akretion.com)
+#    Account Trial Balance CSV module for Odoo
+#    Copyright (C) 2014-2015 Akretion (http://www.akretion.com)
 #    @author Alexis de Lattre <alexis.delattre@akretion.com>
 #
 #    This program is free software: you can redistribute it and/or modify
