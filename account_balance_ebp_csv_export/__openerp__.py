420c1eb8c0c06f9fe75bb00bbfe7114fb0ab0778
Alexis de Lattre <alexis@via.ecp.fr>
2015-03-30 12:20:15 +0200

    Add file README.rst OpenERP renamed to Odoo Add OCA as author

--- account_balance_ebp_csv_export/__openerp__.py
+++ account_balance_ebp_csv_export/__openerp__.py
@@ -1,8 +1,8 @@
 # -*- encoding: utf-8 -*-
 ##############################################################################
 #
-#    Account Balance EBP CSV export module for OpenERP
-#    Copyright (C) 2014 Akretion (http://www.akretion.com)
+#    Account Balance EBP CSV export module for Odoo
+#    Copyright (C) 2014-2015 Akretion (http://www.akretion.com)
 #    @author Alexis de Lattre <alexis.delattre@akretion.com>
 #
 #    This program is free software: you can redistribute it and/or modify
@@ -26,15 +26,7 @@
     'version': '0.1',
     'category': 'Accounting & Finance',
     'license': 'AGPL-3',
-    'description': """
-Account Balance EBP CSV export
-==============================
-
-This module adds a button *EBP csv File* next to the *Print* button on the Trial Balance wizard. The aim is to export the trial balance to software dedicated to the *Liasse fiscale* (French fiscal declaration) that accept CSV files from the popular EBP accounting software. This file has been tested with Teledec (https://www.teledec.fr/), which is a SaaS solution for the *Liasse fiscale* with support for EDI transmission.
-
-This module has been developped by Alexis de Lattre from Akretion <alexis.delattre@akretion.com>.
-    """,
-    'author': 'Akretion',
+    'author': 'Akretion,Odoo Community Association (OCA)',
     'website': 'http://www.akretion.com',
     'depends': ['report_aeroo', 'account_financial_report_webkit'],
     'data': [
