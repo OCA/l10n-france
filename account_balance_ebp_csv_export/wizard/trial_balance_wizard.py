4b282d1699e3c49b7f0d7c5f781842c62385c2c8
Alexis de Lattre <alexis.delattre@akretion.com>
2015-10-01 23:39:40 +0200

    Remove import of deleted file
    Update utf-8 header

--- account_balance_ebp_csv_export/wizard/trial_balance_wizard.py
+++ account_balance_ebp_csv_export/wizard/trial_balance_wizard.py
@@ -1,4 +1,4 @@
-# -*- encoding: utf-8 -*-
+# -*- coding: utf-8 -*-
 ##############################################################################
 #
 #    Account Balance EBP CSV export module for Odoo
