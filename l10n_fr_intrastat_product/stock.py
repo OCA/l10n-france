15441a45a97299c238d4b2951b303f4f252bb452
Alexis de Lattre <alexis.delattre@akretion.com>
2015-11-13 13:27:22 +0100

    Update FR intrastat module for intrastat v3 !

--- l10n_fr_intrastat_product/stock.py
+++ l10n_fr_intrastat_product/stock.py
@@ -20,77 +20,14 @@
 #
 ##############################################################################
 
-from openerp import models, fields, api
+from openerp import models, api
 
 
-class StockLocation(models.Model):
-    _inherit = "stock.location"
+class StockWarehouse(models.Model):
+    _inherit = 'stock.warehouse'
 
-    intrastat_department = fields.Char(
-        string='Department', size=2,
-        help="France's department where the stock location is located. "
-        "This parameter is required for the DEB "
-        "(Déclaration d'Echange de Biens).")
-
-    @api.one
-    @api.constrains('intrastat_department')
-    def _check_intrastat_department(self):
-        self.env['res.company'].real_department_check(
-            self.intrastat_department)
-
-
-class StockPicking(models.Model):
-    _inherit = "stock.picking"
-
-    @api.one
-    @api.depends(
-        'picking_type_id', 'move_lines', 'move_lines.location_dest_id',
-        'move_lines.location_id')
-    def _compute_department(self):
-        intrastat_department = False
-        start_point = False
-        if self.move_lines:
-            if (self.picking_type_id.code == 'outgoing'
-                    and self.move_lines[0].location_dest_id.usage ==
-                    'customer'):
-                start_point = self.move_lines[0].location_id
-            elif (self.picking_type_id.code == 'incoming'
-                    and self.move_lines[0].location_dest_id.usage ==
-                    'internal'):
-                start_point = self.move_lines[0].location_dest_id
-            while start_point:
-                if start_point.intrastat_department:
-                    intrastat_department = start_point.intrastat_department
-                    break
-                elif start_point.location_id:
-                    start_point = start_point.location_id
-                    continue
-                else:
-                    break
-        self.intrastat_department = intrastat_department
-
-    intrastat_transport = fields.Selection([
-        (1, 'Transport maritime'),
-        (2, 'Transport par chemin de fer'),
-        (3, 'Transport par route'),
-        (4, 'Transport par air'),
-        (5, 'Envois postaux'),
-        (7, 'Installations de transport fixes'),
-        (8, 'Transport par navigation intérieure'),
-        (9, 'Propulsion propre')], 'Type of transport',
-        help="Select the type of transport of the goods. This information "
-        "is required for the product intrastat report (DEB).")
-    intrastat_department = fields.Char(
-        compute='_compute_department', size=2, string='Intrastat Department',
-        help='Compute the source departement for a Delivery Order, '
-        'or the destination department for an Incoming Shipment.')
-
-    def _create_invoice_from_picking(
-            self, cr, uid, picking, vals, context=None):
-        '''Copy transport and department from picking to invoice'''
-        vals['intrastat_transport'] = picking.intrastat_transport
-        vals['intrastat_department'] = picking.intrastat_department
-        if picking.partner_id and picking.partner_id.country_id:
-            vals['intrastat_country_id'] = picking.partner_id.country_id.id
-        return super(StockPicking, self)._create_invoice_from_picking(
-            cr, uid, picking, vals, context=context)
+    @api.multi
+    def get_intrastat_region(self):
+        # TODO : keep compat with other countries
+        self.ensure_one()
+        return self.partner_id.department_id
