15757b6a640644bc5a297b251c1061b21187f3f3
Alexis de Lattre <alexis.delattre@akretion.com>
2015-11-13 13:27:22 +0100

    Update FR intrastat module for intrastat v3 !

--- l10n_fr_intrastat_product/__openerp__.py
+++ l10n_fr_intrastat_product/__openerp__.py
@@ -1,8 +1,8 @@
-# -*- encoding: utf-8 -*-
+# -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    Report intrastat product module for OpenERP
-#    Copyright (C) 2010-2014 Akretion (http://www.akretion.com)
+#    L10n FR intrastat product module for Odoo
+#    Copyright (C) 2010-2015 Akretion (http://www.akretion.com)
 #    @author Alexis de Lattre <alexis.delattre@akretion.com>
 #
 #    This program is free software: you can redistribute it and/or modify
@@ -23,19 +23,17 @@
 
 {
     'name': 'France Intrastat Product',
-    'version': '1.2',
+    'version': '2.0',
     'category': 'Localisation/Report Intrastat',
     'license': 'AGPL-3',
     'summary': 'Module for Intrastat product reporting (DEB) for France',
-    'description': """This module adds support for the "Déclaration d'Echange de Biens" (DEB).
-
-More information about the DEB is available on this official web page : http://www.douane.gouv.fr/page.asp?id=322
-
-Please contact Alexis de Lattre from Akretion <alexis.delattre@akretion.com> for any help or question about this module.
-    """,
     'author': 'Akretion',
     'website': 'http://www.akretion.com',
-    'depends': ['intrastat_product', 'sale_stock', 'purchase', 'l10n_fr_siret'],
+    'depends': [
+        'intrastat_product',
+        'l10n_fr_siret',
+        'l10n_fr_department',
+        ],
     'data': [
         'security/intrastat_product_security.xml',
         'security/ir.model.access.csv',
@@ -46,8 +44,8 @@ Please contact Alexis de Lattre from Akretion <alexis.delattre@akretion.com> for
         'company_view.xml',
         'partner_view.xml',
         'product_view.xml',
-        'stock_view.xml',
-        'invoice_view.xml',
+        # 'stock_view.xml',
+        # 'invoice_view.xml',
     ],
     'demo': ['intrastat_demo.xml'],
     'installable': True,
