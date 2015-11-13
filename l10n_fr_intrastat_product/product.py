b5e9472fed60aefb3895c3c80e224c89a03e7560
Alexis de Lattre <alexis.delattre@akretion.com>
2015-11-13 13:27:22 +0100

    Update FR intrastat module for intrastat v3 !

--- l10n_fr_intrastat_product/product.py
+++ l10n_fr_intrastat_product/product.py
@@ -1,9 +1,8 @@
 # -*- encoding: utf-8 -*-
 ##############################################################################
 #
-#    Report intrastat product module for OpenERP
-#    Copyright (C) 2004-2009 Tiny SPRL (http://tiny.be)
-#    Copyright (C) 2010-2014 Akretion (http://www.akretion.com)
+#    L10n FR intrastat product module for Odoo
+#    Copyright (C) 2010-2015 Akretion (http://www.akretion.com)
 #    @author Alexis de Lattre <alexis.delattre@akretion.com>
 #
 #    This program is free software: you can redistribute it and/or modify
@@ -21,47 +20,13 @@
 #
 ##############################################################################
 
-from openerp import models, fields, api, _
-from openerp.exceptions import ValidationError
+from openerp import models, fields
 
 
-class ProductUom(models.Model):
-    _inherit = "product.uom"
+class IntrastatUnit(models.Model):
+    _inherit = "intrastat.unit"
 
-    intrastat_label = fields.Char(
-        string='Intrastat Label', size=12,
-        help="Label used in the XML file export of the Intrastat "
-        "Product Report for this unit of measure.")
-
-
-class ProductSupplierinfo(models.Model):
-    _inherit = "product.supplierinfo"
-
-    origin_country_id = fields.Many2one(
-        'res.country', string='Country of Origin',
-        help="Country of origin of the product "
-        "(i.e. product 'made in ____') when purchased from this supplier. "
-        "This field is used only when the equivalent field on the product "
-        "form is empty.")
-
-    @api.constrains('origin_country_id', 'name', 'product_tmpl_id')
-    def _same_supplier_same_origin(self):
-        """Products from the same supplier should have the same origin"""
-        # Search for same supplier and same product tmpl
-        # 'name' on product_supplierinfo is a many2one to res.partner
-        same_product_same_suppliers = self.search([
-            ('product_tmpl_id', '=', self.product_tmpl_id.id),
-            ('name', '=', self.name.id)])
-        for supplieri in same_product_same_suppliers:
-            if self.origin_country_id != supplieri.origin_country_id:
-                raise ValidationError(
-                    _("For a particular product, all supplier info "
-                        "entries with the same supplier should have the "
-                        "same country of origin. But, for product '%s' "
-                        "with supplier '%s', there is one entry with "
-                        "country of origin '%s' and another entry with "
-                        "country of origin '%s'.")
-                    % (self.product_tmpl_id.name,
-                        self.name.name,
-                        self.origin_country_id.name,
-                        supplieri.origin_country_id.name))
+    fr_xml_label = fields.Char(
+        string='Label for DEB', size=12,
+        help="Label used in the XML file export of the French Intrastat "
+        "Product Declaration for this unit of measure.")
