35f5cbb2842236a1edf34adfc87b79119d4a6e1a
Sébastien BEAU <sebastien.beau@akretion.com>
2016-03-01 12:52:41 +0100

    [REF] refactor code to remove the dependency on aeroo report

--- account_balance_ebp_csv_export/wizard/trial_balance_wizard.py
+++ account_balance_ebp_csv_export/wizard/trial_balance_wizard.py
@@ -21,6 +21,70 @@
 ##############################################################################
 
 from openerp import models, api
+from openerp.report import interface
+from openerp.addons.account_financial_report_webkit.report.\
+    trial_balance import TrialBalanceWebkit
+import unicodecsv
+
+try:
+    from cStringIO import StringIO
+except ImportError:
+    from StringIO import StringIO
+
+
+class ReportEBP(interface.report_int):
+
+    def create(self, cr, uid, ids, data, context=None):
+        def f2s(value):
+            return '%.2f' % value
+
+        # Get objects and context from Webkit Parser
+        parser = TrialBalanceWebkit(
+            cr, uid, 'ebp.parser', context=context)
+        objects, new_ids, context_report_values =\
+            parser.compute_balance_data(data)
+
+        # Get the list of visible account
+        to_display_accounts = context_report_values['to_display_accounts']
+
+        # Init file and write header
+        f = StringIO()
+        w = unicodecsv.writer(f, delimiter=',', encoding='utf-8')
+        header = [
+            "Compte.Numero",
+            "Compte.Intitule",
+            "Balance.SldCptNDebit",
+            "Balance.SldCptNCredit",
+            "Balance.SldCptNSoldeD",
+            "Balance.SldCptNSoldeC",
+            ]
+        w.writerow(header)
+
+        # Write row
+        for current_account in objects:
+            if to_display_accounts.get(current_account.id) \
+                    and current_account.type != 'view':
+                if current_account.balance >= 0:
+                    bal_debit = current_account.balance
+                    bal_credit = 0
+                else:
+                    bal_credit = current_account.balance
+                    bal_debit = 0
+
+                w.writerow([
+                    current_account.code,
+                    current_account.name,
+                    f2s(current_account.debit),
+                    f2s(current_account.credit),
+                    f2s(bal_debit),
+                    f2s(bal_credit),
+                    ])
+        # Read and return File
+        f.seek(0)
+        data = f.read()
+        return (data, 'csv')
+
+ReportEBP('report.trial.balance.ebp')
 
 
 class AccountTrialBalanceWizard(models.TransientModel):
@@ -31,5 +95,5 @@ class AccountTrialBalanceWizard(models.TransientModel):
         res = super(AccountTrialBalanceWizard, self)._print_report(
             data)
         if self.env.context.get('export_type') == 'ebp-csv':
-            res['report_name'] = 'account.report.trial.balance.ebp.csv'
+            res['report_name'] = 'trial.balance.ebp'
         return res
