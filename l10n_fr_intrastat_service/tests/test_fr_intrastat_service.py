f68fd5f0f8b33dcb8f7c11f6876213371edc9b4e
Alexis de Lattre <alexis.delattre@akretion.com>
2016-04-15 06:51:20 +0200

    More demo data, more in-depth testing

--- l10n_fr_intrastat_service/tests/test_fr_intrastat_service.py
+++ l10n_fr_intrastat_service/tests/test_fr_intrastat_service.py
@@ -4,6 +4,8 @@
 # @author: Alexis de Lattre <alexis.delattre@akretion.com>
 
 from openerp.tests.common import TransactionCase
+from openerp.tools import float_compare
+from lxml import etree
 
 
 class TestFrIntrastatService(TransactionCase):
@@ -16,8 +18,24 @@ class TestFrIntrastatService(TransactionCase):
             'company_id': company.id})
         # We use the demo invoice provided by this module
         des.generate_service_lines()
-        self.assertGreaterEqual(des.total_amount, 450)
-        self.assertGreaterEqual(des.num_decl_lines, 1)
+        precision = self.env['decimal.precision'].precision_get('Account')
+        self.assertEqual(float_compare(
+            des.total_amount, 540.0, precision_digits=precision), 0)
+        self.assertEqual(des.num_decl_lines, 3)
         des.done()
         self.assertEquals(des.state, 'done')
         des.generate_xml()
+        xml_des_files = self.env['ir.attachment'].search([
+            ('res_id', '=', des.id),
+            ('res_model', '=', 'l10n.fr.intrastat.service.declaration'),
+            ('type', '=', 'binary'),
+            ])
+        self.assertEquals(len(xml_des_files), 1)
+        xml_des_file = xml_des_files[0]
+        self.assertEquals(xml_des_file.datas_fname[-4:], '.xml')
+        xml_root = etree.fromstring(xml_des_file.datas.decode('base64'))
+        company_vat_xpath = xml_root.xpath(
+            '/fichier_des/declaration_des/num_tvaFr')
+        self.assertEquals(company_vat_xpath[0].text, company.vat)
+        lines_xpath = xml_root.xpath('/fichier_des/declaration_des/ligne_des')
+        self.assertEquals(len(lines_xpath), des.num_decl_lines)
