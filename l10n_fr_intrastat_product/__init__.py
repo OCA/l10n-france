18bc68a59071b02b2b96afdac3cec9a0538aab57
Alexis de Lattre <alexis.delattre@akretion.com>
2016-04-28 10:04:11 +0200

    Remove unused files

--- l10n_fr_intrastat_product/__init__.py
+++ l10n_fr_intrastat_product/__init__.py
@@ -5,7 +5,5 @@ from . import partner
 from . import product
 from . import stock
 from . import company
-#from . import invoice
-#from . import sale
 #from . import purchase  # TODO : see if we move it to intrastat_product or NOT
 from . import intrastat_product
