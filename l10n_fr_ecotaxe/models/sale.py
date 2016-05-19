af669faeb3ce697f2bd297826564f1ee2b1b90d3
mourad-ehm <mourad.elhadj.mimoune@akretion.com>
2016-05-19 15:17:29 +0200

    [FIX] Clean Code

--- l10n_fr_ecotaxe/models/sale.py
+++ l10n_fr_ecotaxe/models/sale.py
@@ -18,9 +18,8 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-__author__ = 'mourad.elhadj.mimoune'
 
-from openerp import api, fields, models, _
+from openerp import api, fields, models
 
 
 class SaleOrderLine(models.Model):
@@ -39,8 +38,8 @@ class SaleOrderLine(models.Model):
             val = 0.0
             if ecotaxe_id:
                 taxes = ecotaxe_id.compute_all(
-                            price, qty, line.product_id,
-                            line.order_id.partner_id)['taxes']
+                    price, qty, line.product_id,
+                    line.order_id.partner_id)['taxes']
                 for t in taxes:
                         val += t.get('amount', 0.0)
 
@@ -55,25 +54,25 @@ class SaleOrder(models.Model):
 
     _inherit = "sale.order"
 
-    amount_untaxed_with_ecotaxe = fields.Float('Untaxed Amount with Ecotaxe',
-                                               store=True,
-                                               compute='_compute_ecotaxe')
-    amount_ecotaxe = fields.Float(string='Included Ecotaxe', store=True,
-                                  compute='_compute_ecotaxe')
-    amount_tax_without_ecotaxe = fields.Float(string='Other Taxes', store=True,
-                                              compute='_compute_ecotaxe')
+    amount_untaxed_with_ecotaxe = fields.Float(
+        'Untaxed Amount with Ecotaxe', store=True,
+        compute='_compute_ecotaxe')
+    amount_ecotaxe = fields.Float(
+        string='Included Ecotaxe', store=True,
+        compute='_compute_ecotaxe')
+    amount_tax_without_ecotaxe = fields.Float(
+        string='Other Taxes', store=True,
+        compute='_compute_ecotaxe')
 
     @api.multi
     @api.depends('amount_untaxed', 'order_line.tax_id',
                  'order_line.product_id', 'order_line.product_uom_qty')
     def _compute_ecotaxe(self):
         for order in self:
-            val_ecotaxe = val1 = 0.0
-            cur = order.pricelist_id.currency_id
+            val_ecotaxe = 0.0
             for line in order.order_line:
                 val_ecotaxe += line.amount_ecotaxe
             order.amount_ecotaxe = val_ecotaxe
-            order.amount_untaxed_with_ecotaxe = \
-                order.amount_untaxed + order.amount_ecotaxe
-            order.amount_tax_without_ecotaxe = \
-                order.amount_tax - order.amount_ecotaxe
+            order.amount_untaxed_with_ecotaxe = order.amount_untaxed \
+                + val_ecotaxe
+            order.amount_tax_without_ecotaxe = order.amount_tax - val_ecotaxe
