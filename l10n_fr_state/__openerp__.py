df135d6c4414ce6c8c96e076c3b89771d36ea999
Alexis de Lattre <alexis.delattre@akretion.com>
2016-07-19 10:57:05 +0200

    Update description and move it to README.rst

--- l10n_fr_state/__openerp__.py
+++ l10n_fr_state/__openerp__.py
@@ -1,33 +1,13 @@
 # -*- coding: utf-8 -*-
 # © 2013-2016 GRAP (Sylvain LE GAL https://twitter.com/legalsylvain)
+# © 2015-2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'French States (Région)',
-    'summary': 'Populate Database with French States (Région)',
+    'name': 'French States (Régions)',
+    'summary': 'Populate Database with French States (Régions)',
     'version': '9.0.0.1.1',
     'category': 'French Localization',
-    'description': """
-Populate Database with French States (Région)
-=============================================
-
-Feature:
---------
-    * Populate the table res_country_state with the french states. """
-    """(named 'Région')
-
-Technical information:
-----------------------
-    * Use 3166-2:FR codifications without country prefix (more detail """
-    """http://fr.wikipedia.org/wiki/ISO_3166-2:FR);
-    * Only populate inner states; (no Guyane, Mayotte, etc...) because """
-    """there are in the res_country table;
-
-Copyright, Authors and Licence:
--------------------------------
-    * Copyright: 2013-2014, GRAP, Groupement Régional Alimentaire de Proximité;
-    * Author: Sylvain LE GAL (https://twitter.com/legalsylvain);
-    * Licence: AGPL-3 (http://www.gnu.org/licenses/);""",
     'author': "GRAP,Akretion,Odoo Community Association (OCA)",
     'website': 'http://www.grap.coop',
     'license': 'AGPL-3',
