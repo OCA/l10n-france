409fcebf6679968712f3007fb171305884ee963f
Alexis de Lattre <alexis.delattre@akretion.com>
2016-07-24 10:55:22 +0200

    FIX computed field department_id to make it work with overseas departments
    Add missing README.rst

--- l10n_fr_department/__openerp__.py
+++ l10n_fr_department/__openerp__.py
@@ -1,52 +1,13 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    l10n FR Departments module for OpenERP
-#    Copyright (C) 2013-2014 GRAP (http://www.grap.coop)
-#    @author Sylvain LE GAL (https://twitter.com/legalsylvain)
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# © 2013-2016 GRAP (Sylvain LE GAL https://twitter.com/legalsylvain)
+# © 2015-2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'French Departments (Départements)',
     'summary': 'Populate Database with French Departments (Départements)',
     'version': '9.0.0.1.1',
     'category': 'French Localization',
-    'description': """
-Populate Database with French Departments (Départements)
-========================================================
-
-Feature:
---------
-    * Create a new model res_country_department, sub division of the """
-    """res_country_state;
-    * Populate the table res_country_department with the french departments;
-
-Technical informations:
------------------------
-    * Use 3166-2:FR codifications (more detail"""
-    """http://fr.wikipedia.org/wiki/ISO_3166-2:FR);
-
-Copyright, Authors and Licence:
--------------------------------
-    * Copyright: 2013-2014, Groupement Régional Alimentaire de Proximité
-    * Copyright: 2014, Akretion (http://www.akretion.com/)
-    * Author: Sylvain LE GAL (https://twitter.com/legalsylvain)
-    * Author: Alexis de Lattre <alexis.delattre@akretion.com>
-    * Licence: AGPL-3 (http://www.gnu.org/licenses/)""",
     'author': "GRAP,Akretion,Odoo Community Association (OCA)",
     'website': 'http://www.grap.coop',
     'license': 'AGPL-3',
