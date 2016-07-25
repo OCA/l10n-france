3c35313d51fbf9eeb3220cfc5048902fc6221bbf
Alexis de Lattre <alexis.delattre@akretion.com>
2016-07-25 12:04:02 +0200

    Add module l10n_fr_department_oversea
    Convert YAML test to unittest
    Convert api.one to api.multi
    Remove country_id field in data/res_country_department_data.yml because it is a related field

---
+++ l10n_fr_department_oversea/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# © 2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'French Overseas Departments (DOM)',
+    'summary': 'Populate Database with overseas French Departments (Départements)',
+    'version': '9.0.1.0.0',
+    'category': 'French Localization',
+    'author': "Akretion,Odoo Community Association (OCA)",
+    'website': 'http://www.akretion.com',
+    'license': 'AGPL-3',
+    'depends': ['l10n_fr_department'],
+    'data': [
+        'data/res_country_state.xml',
+        'data/res_country_department.xml',
+    ],
+    'post_init_hook': 'set_oversea_department_on_partner',
+    'images': [],
+    'installable': True,
+}
---
+++ l10n_fr_department_oversea/tests/test_department_oversea.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+# © 2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp.tests.common import TransactionCase
+
+
+class TestFrDepartmentOversea(TransactionCase):
+
+    def test_fr_department_oversea(self):
+        rpo = self.env['res.partner']
+        partner1 = rpo.create({
+            'name': u'Monsieur Payet',
+            'street': u'11 rue du Stade',
+            'street2': u'Montgaillard',
+            'zip': '97400',
+            'city': u'Saint Denis',
+            'country_id': self.env.ref('base.re').id,
+        })
+        self.assertEquals(
+            partner1.department_id,
+            self.env.ref(
+                'l10n_fr_department_oversea.res_country_department_reunion'))
+        # I also want it to work if you select France as country
+        partner2 = rpo.create({
+            'name': u'Monsieur Hoarau',
+            'street': u'13 rue du Stade',
+            'street2': u'Montgaillard',
+            'zip': '97400',
+            'city': u'Saint Denis',
+            'country_id': self.env.ref('base.fr').id,
+        })
+        self.assertEquals(
+            partner2.department_id,
+            self.env.ref(
+                'l10n_fr_department_oversea.res_country_department_reunion'))
