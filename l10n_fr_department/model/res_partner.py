52c14de9178f4d60217051ca5912ce116fa633c4
Alexis de Lattre <alexis.delattre@akretion.com>
2016-07-25 12:04:02 +0200

    Add module l10n_fr_department_oversea
    Convert YAML test to unittest
    Convert api.one to api.multi
    Remove country_id field in data/res_country_department_data.yml because it is a related field

--- l10n_fr_department/model/res_partner.py
+++ l10n_fr_department/model/res_partner.py
@@ -1,24 +1,6 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    l10n FR Departments module for Odoo
-#    Copyright (C) 2014-2015 Akretion (http://www.akretion.com)
-#    @author Alexis de Lattre <alexis.delattre@akretion.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# © 2014-2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from openerp import models, fields, api
 
@@ -26,29 +8,31 @@ from openerp import models, fields, api
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
-    @api.one
+    @api.multi
     @api.depends('zip', 'country_id', 'country_id.code')
     # If a department code changes, it will have to be manually recomputed
     def _compute_department(self):
-        '''This method is designed to be inherited'''
-        dpt_id = False
-        zip = self.zip
-        if (
-                self.country_id and
-                self.country_id.code in (
-                'FR', 'GP', 'MQ', 'GF', 'RE', 'YT') and
-                zip and
-                len(zip) == 5):
-            code = zip[0:2]
-            if code == '97':
-                code = zip[0:3]
-            dpts = self.env['res.country.department'].search([
-                ('code', '=', code),
-                ('country_id', '=', self.country_id.id),
-                ])
-            if len(dpts) == 1:
-                dpt_id = dpts[0].id
-        self.department_id = dpt_id
+        rcdo = self.env['res.country.department']
+        fr_country_ids = self.env['res.country'].search([
+            ('code', 'in', ('FR', 'GP', 'MQ', 'GF', 'RE', 'YT'))]).ids
+        for partner in self:
+            dpt_id = False
+            zip = partner.zip
+            if (
+                    partner.country_id and
+                    partner.country_id.id in fr_country_ids and
+                    zip and
+                    len(zip) == 5):
+                code = zip[0:2]
+                if code == '97':
+                    code = zip[0:3]
+                dpts = rcdo.search([
+                    ('code', '=', code),
+                    ('country_id', 'in', fr_country_ids),
+                    ])
+                if len(dpts) == 1:
+                    dpt_id = dpts[0].id
+            partner.department_id = dpt_id
 
     department_id = fields.Many2one(
         'res.country.department', compute='_compute_department',
