343b3f67446f81b9aa63c92f6076698c99ef34be
Alexis de Lattre <alexis.delattre@akretion.com>
2016-09-26 10:59:36 +0200

    Display department with name AND code i.e. 'Rhône (69)'
    Add group by in partner search view

--- l10n_fr_department/model/res_country_department.py
+++ l10n_fr_department/model/res_country_department.py
@@ -22,12 +22,13 @@
 #
 ##############################################################################
 
-from openerp import models, fields, _
+from openerp import models, fields, api, _
 
 
 class ResCountryDepartment(models.Model):
     _description = "Department"
     _name = 'res.country.department'
+    _rec_name = 'display_name'
     _order = 'country_id, code'
 
     state_id = fields.Many2one(
@@ -43,8 +44,19 @@ class ResCountryDepartment(models.Model):
         string='Departement Code', size=3, required=True,
         help="""The department code."""
         """(ISO 3166-2 Codification)""")
+    display_name = fields.Char(
+        compute='compute_display_name', string='Display Name', readonly=True,
+        store=True)
 
     _sql_constraints = [
         ('code_uniq', 'unique (code)',
             _("""You cannot have two departments with the same code!""")),
     ]
+
+    @api.one
+    @api.depends('name', 'code')
+    def compute_display_name(self):
+        dname = self.name
+        if self.code:
+            dname = '%s (%s)' % (dname, self.code)
+        self.display_name = dname
