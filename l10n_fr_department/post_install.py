83841ee01a2d48977d303bef1b23d35579db74d9
Alexis de Lattre <alexis.delattre@akretion.com>
2016-10-07 00:14:27 +0200

    Port l10n_fr_department, l10n_fr_department_oversea and l10n_fr_state to v10

--- l10n_fr_department/post_install.py
+++ l10n_fr_department/post_install.py
@@ -2,26 +2,24 @@
 # © 2014-2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import SUPERUSER_ID
+from odoo import api, SUPERUSER_ID
 
 
-def set_department_on_partner(cr, pool):
+def set_department_on_partner(cr, registry):
     """This post_install script is required because, when the module
     is installed, Odoo creates the column in the DB and compute the field
     and THEN it loads the file data/res_country_department_data.yml...
     So, when it computes the field on module installation, the
     departments are not available in the DB, so the department_id field
     on res.partner stays null. This post_install script fixes this."""
-    rpo = pool['res.partner']
-    fr_country_ids = pool['res.country'].search(
-        cr, SUPERUSER_ID,
-        [('code', 'in', ('FR', 'GP', 'MQ', 'GF', 'RE', 'YT'))])
-    partner_ids = rpo.search(
-        cr, SUPERUSER_ID,
-        [
-            '|', ('active', '=', False), ('active', '=', True),
-            ('country_id', 'in', fr_country_ids),
-        ])
-    partners = rpo.browse(cr, SUPERUSER_ID, partner_ids)
-    partners._compute_department()
+    with api.Environment.manage():
+        env = api.Environment(cr, SUPERUSER_ID, {})
+        fr_countries = env['res.country'].search(
+            [('code', 'in', ('FR', 'GP', 'MQ', 'GF', 'RE', 'YT'))])
+        partners = env['res.partner'].search(
+            [
+                '|', ('active', '=', False), ('active', '=', True),
+                ('country_id', 'in', fr_countries.ids),
+            ])
+        partners._compute_department()
     return
