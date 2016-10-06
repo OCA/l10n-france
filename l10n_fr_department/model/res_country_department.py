950bb6c4335344d337b5d679777873eb7521a1a3
Alexis de Lattre <alexis.delattre@akretion.com>
2016-10-07 00:14:27 +0200

    Port l10n_fr_department, l10n_fr_department_oversea and l10n_fr_state to v10

--- l10n_fr_department/model/res_country_department.py
+++ l10n_fr_department/model/res_country_department.py
@@ -1,28 +1,11 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    l10n FR Departments module for Odoo
-#    Copyright (C) 2013-2014 GRAP (http://www.grap.coop)
-#    Copyright (C) 2015 Akretion (http://www.akretion.com)
-#    @author Sylvain LE GAL (https://twitter.com/legalsylvain)
-#    @author Alexis de Lattre (alexis.delattre@akretion.com)
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# © 2013-2016 GRAP (http://www.grap.coop)
+# © 2015-2016 Akretion (http://www.akretion.com)
+# @author Sylvain LE GAL (https://twitter.com/legalsylvain)
+# @author Alexis de Lattre (alexis.delattre@akretion.com)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import models, fields, api, _
+from odoo import models, fields, api, _
 
 
 class ResCountryDepartment(models.Model):
@@ -50,7 +33,7 @@ class ResCountryDepartment(models.Model):
 
     _sql_constraints = [
         ('code_uniq', 'unique (code)',
-            _("""You cannot have two departments with the same code!""")),
+            _("You cannot have two departments with the same code!")),
     ]
 
     @api.one
