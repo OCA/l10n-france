2040d9cf1665c19423e21a0dded28b74d0a9ddac
Pedro M. Baeza <pedro.baeza@gmail.com>
2016-10-06 14:54:33 +0200

    [MIG] Make modules uninstallable

--- l10n_fr_base_location_geonames_import/__openerp__.py
+++ l10n_fr_base_location_geonames_import/__openerp__.py
@@ -29,5 +29,5 @@ This module has been written by Alexis de Lattre from Akretion
     'website': 'http://www.akretion.com',
     'depends': ['base_location_geonames_import'],
     'external_dependencies': {'python': ['unidecode']},
-    'installable': True,
+    'installable': False,
 }
