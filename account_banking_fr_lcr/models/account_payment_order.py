00193ed23bcacec9a0e2ad9487c3a4bf4f32e967
Alexis de Lattre <alexis.delattre@akretion.com>
2016-10-08 00:25:23 +0200

    Port modules account_banking_fr_lcr and l10n_fr_siret to v10
    Prepare the migration of l10n_fr_naf_ape to v10

--- account_banking_fr_lcr/models/account_payment_order.py
+++ account_banking_fr_lcr/models/account_payment_order.py
@@ -2,8 +2,8 @@
 # © 2014-2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import models, fields, api, _
-from openerp.exceptions import UserError
+from odoo import models, fields, api, _
+from odoo.exceptions import UserError
 from unidecode import unidecode
 
 LCR_DATE_FORMAT = '%d%m%y'
