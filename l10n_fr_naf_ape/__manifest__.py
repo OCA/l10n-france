c5862795dd7f35c98e42f282db46c5cf6e69ed6e
Alexis de Lattre <alexis.delattre@akretion.com>
2016-10-08 00:25:23 +0200

    Port modules account_banking_fr_lcr and l10n_fr_siret to v10
    Prepare the migration of l10n_fr_naf_ape to v10

--- l10n_fr_naf_ape/__manifest__.py
+++ l10n_fr_naf_ape/__manifest__.py
@@ -1,27 +1,10 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Odoo, Open Source Management Solution
-#    Copyright (C) 2011 Numérigraphe SARL.
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# © 2011 Numérigraphe SARL
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'French NAF partner categories and APE code',
-    'version': '9.0.1.0.0',
+    'version': '10.0.1.0.0',
     'author': u'Numérigraphe SARL,Odoo Community Association (OCA)',
     'category': 'French Localization',
     'depends': [
@@ -29,7 +12,7 @@
     ],
     'data': [
         'data/res.partner.category.csv',
-        'partner_view.xml',
+        'views/partner.xml',
     ],
     'license': 'AGPL-3',
     'installable': False,
