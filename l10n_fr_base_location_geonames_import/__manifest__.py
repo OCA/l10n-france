db5c935b54ea81d0824fe9afb0e384ae9d2dd893
Alexis de Lattre <alexis.delattre@akretion.com>
2016-10-24 10:04:47 +0200

    Port l10n_fr_base_location_geonames_import
    Add README.rst

--- l10n_fr_base_location_geonames_import/__manifest__.py
+++ l10n_fr_base_location_geonames_import/__manifest__.py
@@ -4,30 +4,14 @@
 
 {
     'name': 'French Localization for Base Location Geonames Import',
-    'version': '9.0.1.0.0',
+    'version': '10.0.1.0.0',
     'category': 'Extra Tools',
     'license': 'AGPL-3',
     'summary': 'France-specific tuning for import of better zip entries '
     'from Geonames',
-    'description': """
-French Localization Base Location Geonames Import
-=================================================
-
-This module adds some France-specific tuning for the
-wizard that imports better zip entries from Geonames
-(http://download.geonames.org/export/zip/). This wizard is provided
-by the module base_location_geonames_import from the projet
-*partner-contact-management*. This tuning aims at complying with
-France's postal standards published by *La Poste*. This tuning will
-be applied for France and France-related territories where *La Poste*
-postal standards apply.
-
-This module has been written by Alexis de Lattre from Akretion
-<alexis.delattre@akretion.com>.
-    """,
     'author': "Akretion,Odoo Community Association (OCA)",
     'website': 'http://www.akretion.com',
     'depends': ['base_location_geonames_import'],
     'external_dependencies': {'python': ['unidecode']},
-    'installable': False,
+    'installable': True,
 }
