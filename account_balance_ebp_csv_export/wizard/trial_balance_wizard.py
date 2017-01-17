0eaa1df9dd08892301bbed65e6630b63d6623867
Alexis de Lattre <alexis.delattre@akretion.com>
2017-01-17 17:52:55 +0100

    Pylint fixes

--- account_balance_ebp_csv_export/wizard/trial_balance_wizard.py
+++ account_balance_ebp_csv_export/wizard/trial_balance_wizard.py
@@ -8,7 +8,12 @@ from openerp import models, api
 from openerp.report import interface
 from openerp.addons.account_financial_report_webkit.report.\
     trial_balance import TrialBalanceWebkit
-import unicodecsv
+import logging
+logger = logging.getLogger(__name__)
+try:
+    import unicodecsv
+except ImportError:
+    logger.debug('Cannot import unicodecsv')
 
 try:
     from cStringIO import StringIO
