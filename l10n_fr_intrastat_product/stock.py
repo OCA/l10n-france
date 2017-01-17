013a8375d032648de6ad4ed08401cadaed2086ae
Alexis de Lattre <alexis.delattre@akretion.com>
2017-01-17 17:52:55 +0100

    Pylint fixes

--- l10n_fr_intrastat_product/stock.py
+++ l10n_fr_intrastat_product/stock.py
@@ -1,4 +1,4 @@
-# -*- encoding: utf-8 -*-
+# -*- coding: utf-8 -*-
 ##############################################################################
 #
 #    l10n FR Report intrastat product module for Odoo
