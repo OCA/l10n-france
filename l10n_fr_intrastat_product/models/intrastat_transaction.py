175a877a93149400bd22e2c657a345cd8d1b5527
Alexis de Lattre <alexis.delattre@akretion.com>
2017-03-08 11:09:27 +0100

    Port DEB to v10

--- l10n_fr_intrastat_product/models/intrastat_transaction.py
+++ l10n_fr_intrastat_product/models/intrastat_transaction.py
@@ -1,27 +1,10 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    L10n FR intrastat product module for Odoo
-#    Copyright (C) 2010-2015 Akretion (http://www.akretion.com/)
-#    @author Alexis de Lattre <alexis.delattre@akretion.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# © 2010-2017 Akretion (http://www.akretion.com/)
+# @author Alexis de Lattre <alexis.delattre@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import models, fields, api, _
-from openerp.exceptions import ValidationError
+from odoo import models, fields, api, _
+from odoo.exceptions import ValidationError
 
 # If you modify the line below, please also update intrastat_type_view.xml
 # (form view of report.intrastat.type, field transaction_code
@@ -36,7 +19,7 @@ class IntrastatTransaction(models.Model):
         ('out_refund', 'Customer Refund'),
         ('in_invoice', 'Supplier Invoice'),
         ('none', 'None'),
-        ], string='Possible on', select=True, required=True)
+        ], string='Possible on', index=True, required=True)
     # procedure_code = fields.Selection([ => code
     fr_transaction_code = fields.Selection([
         ('', '-'),
@@ -83,35 +66,34 @@ class IntrastatTransaction(models.Model):
         'same code and the same procedure code.'
         )]
 
-    @api.one
     @api.constrains('code', 'fr_transaction_code')
     def _code_check(self):
-        if (
-                self.company_id.country_id and
-                self.company_id.country_id == self.env.ref('base.fr')):
+        for trans in self:
             if (
-                    self.code not in fiscal_only_tuple and
-                    not self.fr_transaction_code):
-                raise ValidationError(
-                    _('You must enter a value for the transaction code.'))
-            if self.code in fiscal_only_tuple and self.fr_transaction_code:
-                raise ValidationError(_(
-                    "You should not set a transaction code when the "
-                    "Code (i.e. Procedure Code) is '25', '26' or '31'."))
+                    trans.company_id.country_id and
+                    trans.company_id.country_id == self.env.ref('base.fr')):
+                if (
+                        trans.code not in fiscal_only_tuple and
+                        not trans.fr_transaction_code):
+                    raise ValidationError(
+                        _('You must enter a value for the transaction code.'))
+                if trans.code in fiscal_only_tuple and trans.fr_transaction_code:
+                    raise ValidationError(_(
+                        "You should not set a transaction code when the "
+                        "Code (i.e. Procedure Code) is '25', '26' or '31'."))
 
     @api.onchange('code')
     def procedure_code_on_change(self):
         if self.code in fiscal_only_tuple:
             self.fr_transaction_code = False
 
-    @api.one
     @api.depends('code', 'description', 'fr_transaction_code')
     def _compute_display_name(self):
-        display_name = self.code
-        if self.fr_transaction_code:
-            display_name += '/%s' % self.fr_transaction_code
-        if self.description:
-            display_name += ' ' + self.description
-        self.display_name = len(display_name) > 55 \
-            and display_name[:55] + '...' \
-            or display_name
+        for trans in self:
+            display_name = trans.code
+            if trans.fr_transaction_code:
+                display_name += '/%s' % trans.fr_transaction_code
+            if trans.description:
+                display_name += ' ' + trans.description
+            trans.display_name = len(display_name) > 55 \
+                and display_name[:55] + '...' or display_name
