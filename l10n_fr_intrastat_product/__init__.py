e54bece37db2cdad07483d8a0745bb5dca419b54
Alexis de Lattre <alexis.delattre@akretion.com>
2017-03-08 11:09:27 +0100

    Port DEB to v10

--- l10n_fr_intrastat_product/__init__.py
+++ l10n_fr_intrastat_product/__init__.py
@@ -1,9 +1,3 @@
 # -*- coding: utf-8 -*-
 
-from . import intrastat_transaction
-from . import partner
-from . import product
-from . import stock
-from . import company
-#from . import purchase  # TODO : see if we move it to intrastat_product or NOT
-from . import intrastat_product
+from . import models
