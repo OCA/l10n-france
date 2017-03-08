2bd1121113355d18489d402fd0fa08868456ff84
Alexis de Lattre <alexis.delattre@akretion.com>
2017-03-08 11:09:27 +0100

    Port DEB to v10

---
+++ l10n_fr_intrastat_product/models/account_config_settings.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# © 2017 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import models, fields
+
+
+class AccountConfigSettings(models.TransientModel):
+    _inherit = 'account.config.settings'
+
+    fr_intrastat_accreditation = fields.Char(
+        related='company_id.fr_intrastat_accreditation')
