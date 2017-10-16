2cb9355a6d51b344845c9aa537a61a03e61446ff
Alexis de Lattre <alexis.delattre@akretion.com>
2017-10-16 20:32:22 +0200

    [MIG] Migrate l10n_fr_mis_reports to v10

---
+++ l10n_fr_mis_reports/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# © 2015-2017 Akretion (http://www.akretion.com)
+# @author Alexis de Lattre <alexis.delattre@akretion.com>
+
+{
+    'name': 'MIS reports for France',
+    'version': '10.0.1.0.0',
+    'category': 'Accounting & Finance',
+    'license': 'AGPL-3',
+    'summary': 'MIS Report templates for the French P&L and Balance Sheets',
+    'author': 'Akretion,Odoo Community Association (OCA)',
+    'website': 'http://www.akretion.com',
+    'depends': ['mis_builder', 'l10n_fr'],
+    'data': [
+        'data/mis_report_styles.xml',
+        'data/mis_report_pl.xml',
+        'data/mis_report_pl_simplified.xml',
+        'data/mis_report_bs.xml',
+        'data/mis_report_bs_simplified.xml',
+        ],
+    'installable': True,
+}
