b68faba29978c6fdb2bc53c54255c4e2dde18a91
Voodoo <voodoo@fake.com>
2017-12-19 11:45:27 +0000

    wip

--- l10n_fr_certification_pos/models/pos_config.py
+++ l10n_fr_certification_pos/models/pos_config.py
@@ -15,10 +15,10 @@ class PosConfig(models.Model):
 
     _secured_model_name = 'pos.order'
 
-    _SELECTION_PREVENT_PRINT_LEGACY = [
-        ('no', 'Allow Printing'),
-        ('warning', 'Mark a Warning'),
-        ('block', 'Prevent Printing'),
+    _SELECTION_HASH_PRINT_LEGACY = [
+        ('no', 'Do not Print Hash'),
+        ('warning', 'Print Hash or Mark a Warning'),
+        ('block', 'Print Hash or Prevent Printing'),
     ]
 
     l10n_fr_prevent_print = fields.Char(
@@ -30,15 +30,16 @@ class PosConfig(models.Model):
         " and the value of the key 'l10n_fr_certification_mode' in the server"
         " configuration file.")
 
-    l10n_fr_prevent_print_legacy = fields.Selection(
-        string='Prevent Uncertified Bill', default='no',
-        selection=_SELECTION_PREVENT_PRINT_LEGACY, help="Indicate what is the"
-        " behaviour of the Point of Sale, if the server is unreachable\n"
-        " * 'Allow Printing': Normal behaviour\n"
-        " * 'Mark a Warning': A warning text will be printed on the bill\n"
-        " * 'Prevent Printing': The downgraded mode is disabled and the"
-        " cashier will not have the possibility to give the bill to the"
-        " customer, if there is a network problem. This is the most secure"
+    l10n_fr_hash_print_legacy = fields.Selection(
+        string='Prevent Uncertified Bill', required=True, default='no',
+        selection=_SELECTION_HASH_PRINT_LEGACY, help="Indicate what is the"
+        " behaviour of the Point of Sale\n"
+        " * 'Do not Print Hash': Normal behaviour\n"
+        " * 'Print Hash or Mark a Warning': A warning text will be printed"
+        " on the bill if the server is unreachable\n"
+        " * 'Print Hash or Prevent Printing': The downgraded mode is disabled"
+        " and the cashier will not have the possibility to give the bill to"
+        " the customer, if there is a network problem. This is the most secure"
         " mode from a point of view of certification but you have to ensure"
         " that network connection is reliable.\n\n"
         " Note : This setting has no effect, if server configuration file"
