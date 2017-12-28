6794d580fb00defec483a8aac81f0c0d20d59b11
Alexis de Lattre <alexis.delattre@akretion.com>
2017-12-29 00:44:38 +0100

    Add support for Chorus API

---
+++ l10n_fr_chorus_ubl/models/company.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# © 2017 Akretion (http://www.akretion.com)
+# @author: Alexis de Lattre <alexis.delattre@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import fields, models
+
+
+class ResCompany(models.Model):
+    _inherit = 'res.company'
+
+    fr_chorus_invoice_format = fields.Selection(
+        selection_add=[('xml_ubl', 'UBL XML')])
