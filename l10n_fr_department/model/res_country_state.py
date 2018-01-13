4a836037facbced18c02899ece61c47a0b911cd7
Nicolas JEUDY <nicolas@panda-chi.io>
2018-01-13 17:37:10 +0100

    l10n_fr_department: migrate to 11.0

--- l10n_fr_department/model/res_country_state.py
+++ l10n_fr_department/model/res_country_state.py
@@ -1,9 +1,8 @@
-# -*- coding: utf-8 -*-
 # © 2013-2016 GRAP (http://www.grap.coop)
 # © 2015-2016 Akretion (http://www.akretion.com)
 # @author Sylvain LE GAL (https://twitter.com/legalsylvain)
 # @author Alexis de Lattre (alexis.delattre@akretion.com)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo import models, fields
 
