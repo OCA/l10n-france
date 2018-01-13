f50781e95bcf0804cd73f51e42dc6876b24f4165
Nicolas JEUDY <nicolas@panda-chi.io>
2018-01-13 17:37:10 +0100

    l10n_fr_department: migrate to 11.0

--- l10n_fr_department/model/res_partner.py
+++ l10n_fr_department/model/res_partner.py
@@ -1,6 +1,5 @@
-# -*- coding: utf-8 -*-
 # © 2014-2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo import models, fields, api
 
@@ -29,7 +28,7 @@ class ResPartner(models.Model):
                 dpts = rcdo.search([
                     ('code', '=', code),
                     ('country_id', 'in', fr_country_ids),
-                    ])
+                ])
                 if len(dpts) == 1:
                     dpt_id = dpts[0].id
             partner.department_id = dpt_id
