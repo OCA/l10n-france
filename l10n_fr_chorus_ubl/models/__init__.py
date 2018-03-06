7771d29a003e65e7805f09d50003d8f7c90b9882
Alexis de Lattre <alexis.delattre@akretion.com>
2018-03-07 00:43:15 +0100

    Add wizard to send multiple invoices to Chorus
    New object chorus.flow. Move several features from invoices to chorus.flow.
    IMPORTANT: you need to update the entries for the RGS key and certificate in your Odoo server configuration file, see l10n_fr_chorus_account/README.rst

--- l10n_fr_chorus_ubl/models/__init__.py
+++ l10n_fr_chorus_ubl/models/__init__.py
@@ -3,3 +3,4 @@
 from . import ubl
 from . import company
 from . import account_invoice
+from . import chorus_flow
--- l10n_fr_chorus_ubl/models/account_invoice.py
+++ l10n_fr_chorus_ubl/models/account_invoice.py
@@ -3,14 +3,14 @@
 # @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import fields, models, api
+from odoo import models
+import tarfile
+import time
+from io import BytesIO
 
 
 class AccountInvoice(models.Model):
     _inherit = 'account.invoice'
-
-    chorus_format = fields.Selection(
-        selection_add=[('xml_ubl', 'UBL XML')])
 
     def generate_ubl_xml_string(self, version='2.1'):
         self.ensure_one()
@@ -27,21 +27,38 @@ class AccountInvoice(models.Model):
             cdr_dict[u'Marché public'] = self.agreement_id.code
         return cdr_dict
 
-    @api.model
-    def chorus_invoiceformat2syntax(self):
-        res = super(AccountInvoice, self).chorus_invoiceformat2syntax()
-        res['xml_ubl'] = 'IN_DP_E1_UBL_INVOICE'
-        return res
-
     def prepare_chorus_deposer_flux_payload(self):
-        if self.company_id.fr_chorus_invoice_format == 'xml_ubl':
-            xml_string = self.generate_ubl_xml_string()
-            # Seems they don't want '/' in filenames
-            filename =\
-                'UBL_chorus_facture_%s.xml' % self.number.replace('/', '-')
-            syntaxe_flux = self.chorus_invoiceformat2syntax()['xml_ubl']
+        # move more logic into the l10n_fr_chorus_account module ?
+        # I'll wait for the Factur-X API to be published
+        if self[0].company_id.fr_chorus_invoice_format == 'xml_ubl':
+            syntaxe_flux = self.env['chorus.flow'].syntax_odoo2chorus()[
+                'xml_ubl']
+            if len(self) == 1:
+                chorus_file_content = self.generate_ubl_xml_string()
+                # Seems they don't want '/' in filenames
+                filename =\
+                    'UBL_chorus_facture_%s.xml' % self.number.replace('/', '-')
+            else:
+                # Generate tarball
+                filename = 'UBL_chorus_lot_factures.tar.gz'
+                tarfileobj = BytesIO()
+                with tarfile.open(fileobj=tarfileobj, mode='w:gz') as tar:
+                    for inv in self:
+                        xml_string = inv.generate_ubl_xml_string()
+                        xmlfileio = BytesIO(xml_string)
+                        xmlfilename =\
+                            'UBL_chorus_facture_%s.xml' % inv.number.replace(
+                                '/', '-')
+                        tarinfo = tarfile.TarInfo(name=xmlfilename)
+                        tarinfo.size = len(xml_string)
+                        tarinfo.mtime = int(time.time())
+                        tar.addfile(tarinfo=tarinfo, fileobj=xmlfileio)
+                    tar.close()
+                tarfileobj.seek(0)
+                chorus_file_content = tarfileobj.read()
+
             payload = {
-                'fichierFlux': xml_string.encode('base64'),
+                'fichierFlux': chorus_file_content.encode('base64'),
                 'nomFichier': filename,
                 'syntaxeFlux': syntaxe_flux,
                 'avecSignature': False,
