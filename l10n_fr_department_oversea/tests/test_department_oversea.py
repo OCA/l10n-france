524ad843149f221de8d0af10beedc96bfa83e7c1
Danh Võ Phương <danh@trobz.com>
2018-04-23 15:29:26 +0700

    l10n_fr_department_oversea: migrate to 11.0

--- l10n_fr_department_oversea/tests/test_department_oversea.py
+++ l10n_fr_department_oversea/tests/test_department_oversea.py
@@ -1,36 +1,33 @@
-# -*- coding: utf-8 -*-
 # © 2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 from odoo.tests.common import TransactionCase
 
 
 class TestFrDepartmentOversea(TransactionCase):
-
     def test_fr_department_oversea(self):
         rpo = self.env['res.partner']
         partner1 = rpo.create({
-            'name': u'Monsieur Payet',
-            'street': u'11 rue du Stade',
-            'street2': u'Montgaillard',
+            'name': 'Monsieur Payet',
+            'street': '11 rue du Stade',
+            'street2': 'Montgaillard',
             'zip': '97400',
-            'city': u'Saint Denis',
+            'city': 'Saint Denis',
             'country_id': self.env.ref('base.re').id,
         })
-        self.assertEquals(
+        self.assertEqual(
             partner1.department_id,
             self.env.ref(
                 'l10n_fr_department_oversea.res_country_department_reunion'))
         # I also want it to work if you select France as country
         partner2 = rpo.create({
-            'name': u'Monsieur Hoarau',
-            'street': u'13 rue du Stade',
-            'street2': u'Montgaillard',
+            'name': 'Monsieur Hoarau',
+            'street': '13 rue du Stade',
+            'street2': 'Montgaillard',
             'zip': '97400',
-            'city': u'Saint Denis',
+            'city': 'Saint Denis',
             'country_id': self.env.ref('base.fr').id,
         })
-        self.assertEquals(
+        self.assertEqual(
             partner2.department_id,
             self.env.ref(
                 'l10n_fr_department_oversea.res_country_department_reunion'))
