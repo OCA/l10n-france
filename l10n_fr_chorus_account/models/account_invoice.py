511039cbac3f8352a9f6c7e1ce75e349d18508bc
Alexis de Lattre <alexis.delattre@akretion.com>
2018-05-14 23:26:49 +0200

    Improve constrains on Services for Chorus invoices
    Add tracking on some Chorus field

--- l10n_fr_chorus_account/models/account_invoice.py
+++ l10n_fr_chorus_account/models/account_invoice.py
@@ -19,9 +19,11 @@ class AccountInvoice(models.Model):
     _inherit = ['account.invoice', 'chorus.api']
 
     chorus_flow_id = fields.Many2one(
-        'chorus.flow', string='Chorus Flow', readonly=True, copy=False)
+        'chorus.flow', string='Chorus Flow', readonly=True, copy=False,
+        track_visibility='onchange')
     chorus_identifier = fields.Integer(
-        string='Chorus Invoice Indentifier', readonly=True, copy=False)
+        string='Chorus Invoice Indentifier', readonly=True, copy=False,
+        track_visibility='onchange')
     chorus_status = fields.Char(
         string='Chorus Invoice Status', readonly=True, copy=False,
         track_visibility='onchange')
@@ -49,55 +51,38 @@ class AccountInvoice(models.Model):
                 if (
                         cpartner.fr_chorus_required in
                         ('service', 'service_and_engagement') and
-                        not inv.partner_id.parent_id):
+                        not (inv.partner_id.parent_id and
+                             inv.partner_id.name and
+                             inv.partner_id.fr_chorus_service_code)):
                     raise UserError(_(
                         "Partner '%s' is configured as "
                         "Service Code required for Chorus, so you must "
-                        "select a contact as customer for the invoice.")
-                        % cpartner.name)
-                if (
-                        cpartner.fr_chorus_required in
-                        ('service', 'service_and_engagement') and
-                        not inv.partner_id.fr_chorus_service_code):
-                    raise UserError(_(
-                        "Missing 'Chorus Service Code' on contact '%s' of "
-                        "customer '%s' which is configured as "
-                        "Service Code required for Chorus.")
-                        % (inv.partner_id.name, cpartner.name))
+                        "select a contact as customer for the invoice and "
+                        "this contact should have a name and a Chorus "
+                        "Service Code.") % cpartner.name)
                 if (
                         cpartner.fr_chorus_required in
                         ('engagement', 'service_and_engagement') and
                         not inv.name):
                     raise UserError(_(
-                        "The field 'Reference/Description' should contain "
-                        "the engagement number because customer '%s' is "
-                        "configured as Engagement required for Chorus.")
-                        % cpartner.name)
-                if (
-                        cpartner.fr_chorus_required ==
-                        'service_or_engagement' and (
-                        not inv.name or
-                        not inv.partner_id.parent_id)):
-                    raise UserError(_(
                         "Partner '%s' is configured as "
-                        "Service or Engagement required for Chorus but "
-                        "there is no engagement number in the field "
-                        "'Reference/Description' and the customer "
-                        "is not a contact (Service Codes can only be "
-                        "configured on contacts)") % cpartner.name)
+                        "Engagement required for Chorus, so the "
+                        "field 'Reference/Description' must contain "
+                        "an engagement number.") % cpartner.name)
                 if (
-                        cpartner.fr_chorus_required ==
-                        'service_or_engagement' and (
-                        not inv.name or
-                        not inv.partner_id.fr_chorus_service_code)):
-                    raise UserError(_(
-                        "Partner '%s' is configured as "
-                        "Service or Engagement required for Chorus but "
-                        "there is no engagement number in the field "
-                        "'Reference/Description' and the "
-                        "'Chorus Service Code' is not configured "
-                        "on contact '%s'") % (
-                        cpartner.name, inv.partner_id.name))
+                        cpartner.fr_chorus_required == 'service_or_engagement'
+                        and (not inv.name and not (
+                        inv.partner_id.parent_id and
+                        inv.partner_id.name and
+                        inv.partner_id.fr_chorus_service_code))):
+                        raise UserError(_(
+                            "Partner '%s' is configured as "
+                            "'Service or Engagement' required for Chorus but "
+                            "there is no engagement number in the field "
+                            "'Reference/Description' and the customer of the "
+                            "invoice is not correctly configured as a service "
+                            "(should be a contact with a Chorus service code "
+                            "and a name).") % cpartner.name)
                 if not self.payment_mode_id:
                     raise UserError(_(
                         "Missing Payment Mode. This "
