6cfca8993572becbd0815919cfc6154a74ac8284
Alexis de Lattre <alexis.delattre@akretion.com>
2018-07-12 14:25:55 +0200

    Add module l10n_fr_account_invoice_import_factur-x
    Improve module l10n_fr_business_document_import
    PEP8 fix in l10n_fr_chorus_account

---
+++ l10n_fr_account_invoice_import_factur-x/wizard/account_invoice_import.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Akretion France
+# @author: Alexis de Lattre <alexis.delattre@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import models
+
+
+class AccountInvoiceImport(models.TransientModel):
+    _inherit = 'account.invoice.import'
+
+    def prepare_facturx_xpath_dict(self):
+        xpathd = super(AccountInvoiceImport, self).prepare_facturx_xpath_dict()
+        xpathd['partner']['siret'] = [
+            "//ram:ApplicableHeaderTradeAgreement"
+            "/ram:SellerTradeParty"
+            "/ram:SpecifiedLegalOrganization"
+            "/ram:ID[@schemeID='0002']"]
+        return xpathd
