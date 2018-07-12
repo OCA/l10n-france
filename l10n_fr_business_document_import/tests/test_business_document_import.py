9b6fdddd184dee9e88e5cbb582840b9cdc1b7ab4
Alexis de Lattre <alexis.delattre@akretion.com>
2018-07-12 14:25:55 +0200

    Add module l10n_fr_account_invoice_import_factur-x
    Improve module l10n_fr_business_document_import
    PEP8 fix in l10n_fr_chorus_account

--- l10n_fr_business_document_import/tests/test_business_document_import.py
+++ l10n_fr_business_document_import/tests/test_business_document_import.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-# © 2016-2017 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# Copyright 2016-2018 Akretion France
+# @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo.tests.common import TransactionCase
@@ -7,14 +8,28 @@ from odoo.tests.common import TransactionCase
 
 class TestL10nFRBusinessDocumentImport(TransactionCase):
 
-    def test_match_partner_siren(self):
+    def test_match_partner_siren_siret(self):
         partner1 = self.env['res.partner'].create({
-            'name': 'France Telecom',
+            'name': u'France Telecom (siège)',
             'supplier': True,
             'is_company': True,
             'siren': '380129866',
+            'nic': '46850',
+            })
+        partner2 = self.env['res.partner'].create({
+            'name': 'France Telecom Lyon',
+            'supplier': True,
+            'is_company': True,
+            'siren': '380129866',
+            'nic': '00010',
             })
         bdio = self.env['business.document.import']
         partner_dict = {'siren': '380 129 866'}
         res = bdio._match_partner(partner_dict, [])
-        self.assertEquals(res, partner1)
+        self.assertIn(res, [partner1, partner2])
+        partner_dict = {'siret': '380 129 866 46850'}
+        res = bdio._match_partner(partner_dict, [])
+        self.assertEqual(res, partner1)
+        partner_dict = {'siret': '380 129 866 00035'}
+        res = bdio._match_partner(partner_dict, [])
+        self.assertIn(res, [partner1, partner2])
