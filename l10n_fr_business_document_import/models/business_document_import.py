28148606fdb5403b65085315f6beeed287736706
Alexis de Lattre <alexis.delattre@akretion.com>
2018-07-12 14:25:55 +0200

    Add module l10n_fr_account_invoice_import_factur-x
    Improve module l10n_fr_business_document_import
    PEP8 fix in l10n_fr_chorus_account

--- l10n_fr_business_document_import/models/business_document_import.py
+++ l10n_fr_business_document_import/models/business_document_import.py
@@ -11,15 +11,29 @@ class BusinessDocumentImport(models.AbstractModel):
     @api.model
     def _match_partner(
             self, partner_dict, chatter_msg, partner_type='supplier'):
+        if partner_type == 'supplier':
+            domain = [('supplier', '=', True)]
+        elif partner_type == 'customer':
+            domain = [('customer', '=', True)]
+        else:
+            domain = []
+
+        if partner_dict.get('siret'):
+            siret = partner_dict['siret'].replace(' ', '')
+            if len(siret) == 14 and siret.isdigit():
+                partners = self.env['res.partner'].search(
+                    domain + [
+                        ('parent_id', '=', False),
+                        ('siret', '=', siret),
+                        ])
+                if partners:
+                    return partners[0]
+                # fallback on siren search
+                elif not partner_dict.get('siren'):
+                    partner_dict['siren'] = siret[:9]
         if partner_dict.get('siren'):
             siren = partner_dict['siren'].replace(' ', '')
-            if len(siren) == 9:
-                if partner_type == 'supplier':
-                    domain = [('supplier', '=', True)]
-                elif partner_type == 'customer':
-                    domain = [('customer', '=', True)]
-                else:
-                    domain = []
+            if len(siren) == 9 and siren.isdigit():
                 partners = self.env['res.partner'].search(
                     domain + [
                         ('parent_id', '=', False),
