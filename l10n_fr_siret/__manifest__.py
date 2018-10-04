e28486e722332a663355f1f471ed1fd65c412e6a
Alexis de Lattre <alexis.delattre@akretion.com>
2018-10-04 10:57:14 +0200

    [MIG] l10n_fr_siret from v11 to v12

--- l10n_fr_siret/__manifest__.py
+++ l10n_fr_siret/__manifest__.py
@@ -1,16 +1,15 @@
-# © 2011 Numérigraphe SARL.
-# © 2014-2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# Copyright 2011 Numérigraphe SARL.
+# Copyright 2014-2018 Akretion France
+# @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'French company identity numbers SIRET/SIREN/NIC',
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
     "category": 'French Localization',
     'author': u'Numérigraphe,Akretion,Odoo Community Association (OCA)',
     'license': 'AGPL-3',
-    'depends': ['l10n_fr', 'account'],
-    # account is required only for the inherit of the partner form view
-    # l10n_fr is required because we re-define the siret field on res.company
+    'depends': ['l10n_fr'],
     'data': [
         'views/partner.xml',
         'views/company.xml',
