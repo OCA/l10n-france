28384e1ea32dce5effe5cd78cda28a034072f919
Alexis de Lattre <alexis.delattre@akretion.com>
2018-10-04 10:57:14 +0200

    [MIG] l10n_fr_siret from v11 to v12

--- l10n_fr_siret/models/company.py
+++ l10n_fr_siret/models/company.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
-# © 2011 Numérigraphe SARL.
-# © 2014-2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# Copyright 2011 Numérigraphe SARL.
+# Copyright 2014-2018 Akretion France
+# @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import models, fields
@@ -10,14 +11,16 @@ class ResCompany(models.Model):
     """Replace the company's fields for SIRET/RC with the partner's"""
     _inherit = 'res.company'
 
-    # siret field is defined in l10n_fr module
-    siret = fields.Char(
-        string='SIRET', related='partner_id.siret', store=True)
+    # siret field is defined in l10n_fr module on res.partner
+    # with an unstored related field on res.company
+    siret = fields.Char(store=True, compute_sudo=True)
     siren = fields.Char(
-        string='SIREN', related='partner_id.siren', store=True)
+        string='SIREN', related='partner_id.siren',
+        store=True, compute_sudo=True)
     nic = fields.Char(
-        string='NIC', related='partner_id.nic', store=True)
-    # company_registry field is definied in base module
+        string='NIC', related='partner_id.nic',
+        store=True, compute_sudo=True)
+    # company_registry field is definied in base module on res.company
     company_registry = fields.Char(
         string='Company Registry', related='partner_id.company_registry',
-        store=True)
+        store=True, compute_sudo=True)
