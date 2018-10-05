dc475cb21cfb87caa687c7d674acd53e2b7db000
Alexis de Lattre <alexis.delattre@akretion.com>
2018-10-05 11:15:11 +0200

    [MIG] l10n_fr_department, l10n_fr_department_oversea, l10n_fr_state from v11 to v12

--- l10n_fr_department/model/res_country_department.py
+++ l10n_fr_department/model/res_country_department.py
@@ -1,16 +1,15 @@
-# © 2013-2016 GRAP (http://www.grap.coop)
-# © 2015-2016 Akretion (http://www.akretion.com)
+# Copyright 2013-2018 GRAP (http://www.grap.coop)
+# Copyright 2015-2016 Akretion France (http://www.akretion.com)
 # @author Sylvain LE GAL (https://twitter.com/legalsylvain)
 # @author Alexis de Lattre (alexis.delattre@akretion.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields, api
+from odoo import api, fields, models
 
 
 class ResCountryDepartment(models.Model):
     _description = "Department"
     _name = 'res.country.department'
-    _rec_name = 'display_name'
     _order = 'country_id, code'
 
     state_id = fields.Many2one(
@@ -25,9 +24,6 @@ class ResCountryDepartment(models.Model):
     code = fields.Char(
         string='Department Code', size=3, required=True,
         help="The department code (ISO 3166-2 codification)")
-    display_name = fields.Char(
-        compute='_compute_display_name_field', string='Display Name',
-        readonly=True, store=True)
 
     _sql_constraints = [(
         'code_uniq',
@@ -35,11 +31,12 @@ class ResCountryDepartment(models.Model):
         "You cannot have two departments with the same code!"
     )]
 
-    @api.multi
     @api.depends('name', 'code')
-    def _compute_display_name_field(self):
+    def name_get(self):
+        res = []
         for rec in self:
             dname = rec.name
             if rec.code:
                 dname = '%s (%s)' % (dname, rec.code)
-            rec.display_name = dname
+            res.append((rec.id, dname))
+        return res
