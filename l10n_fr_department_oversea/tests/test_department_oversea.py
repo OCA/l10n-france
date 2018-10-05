0b53846e6472660537fe46d400c57df86d1d1381
Alexis de Lattre <alexis.delattre@akretion.com>
2018-10-05 11:15:11 +0200

    [MIG] l10n_fr_department, l10n_fr_department_oversea, l10n_fr_state from v11 to v12

--- l10n_fr_department_oversea/tests/test_department_oversea.py
+++ l10n_fr_department_oversea/tests/test_department_oversea.py
@@ -1,4 +1,5 @@
-# © 2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# Copyright 2016-2018 Akretion France
+# @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 from odoo.tests.common import TransactionCase
 
