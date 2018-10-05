4719a6bc2d584ab91cfb3684546e9c02687ffabe
Alexis de Lattre <alexis.delattre@akretion.com>
2018-10-05 11:15:11 +0200

    [MIG] l10n_fr_department, l10n_fr_department_oversea, l10n_fr_state from v11 to v12

--- l10n_fr_department/__manifest__.py
+++ l10n_fr_department/__manifest__.py
@@ -1,11 +1,12 @@
-# © 2013-2016 GRAP (Sylvain LE GAL https://twitter.com/legalsylvain)
-# © 2015-2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# Copyright 2013-2018 GRAP (Sylvain LE GAL https://twitter.com/legalsylvain)
+# Copyright 2015-2018 Akretion France
+# @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
     'name': 'French Departments (Départements)',
     'summary': 'Populate Database with French Departments (Départements)',
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
     'category': 'French Localization',
     'author': 'GRAP, '
               'Akretion, '
@@ -19,7 +20,7 @@
     ],
     'data': [
         'security/ir.model.access.csv',
-        'data/res_country_department_data.yml',
+        'data/res_country_department.xml',
         'view/res_country_department.xml',
         'view/res_partner.xml',
     ],
