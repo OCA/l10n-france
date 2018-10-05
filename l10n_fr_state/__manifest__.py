9458fab91300693f2a4a0d71b2b25d2e920fac7f
Alexis de Lattre <alexis.delattre@akretion.com>
2018-10-05 11:15:11 +0200

    [MIG] l10n_fr_department, l10n_fr_department_oversea, l10n_fr_state from v11 to v12

--- l10n_fr_state/__manifest__.py
+++ l10n_fr_state/__manifest__.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
-# © 2013-2016 GRAP (Sylvain LE GAL https://twitter.com/legalsylvain)
-# © 2015-2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# Copyright 2013-2018 GRAP (Sylvain LE GAL https://twitter.com/legalsylvain)
+# Copyright 2015-2018 Akretion France
+# @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'French States (Régions)',
     'summary': 'Populate Database with French States (Régions)',
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
     'category': 'French Localization',
     'author': 'GRAP, '
               'Akretion, '
@@ -16,7 +17,7 @@
     'license': 'AGPL-3',
     'depends': ['base'],
     'pre_init_hook': 'create_fr_state_xmlid',
-    'data': ['data/res_country_state_data.yml'],
+    'data': ['data/res_country_state.xml'],
     'images': ['static/src/img/screenshots/1.png'],
     'installable': True,
 }
