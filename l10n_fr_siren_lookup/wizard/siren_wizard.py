4e31ede4a5c14964dec6109d722dbb4544160806
remi-filament <30716308+remi-filament@users.noreply.github.com>
2018-12-04 15:31:05 +0100

    Rework following comments on PR

---
+++ l10n_fr_siren_lookup/wizard/siren_wizard.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+
+# © 2018 Le Filament (<http://www.le-filament.com>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+import json
+import requests
+
+from odoo import api, fields, models
+
+URL = "https://data.opendatasoft.com/api/records/1.0/search/?dataset=sirene%40public&q={request}&rows=100"
+
+class SirenWizard(models.TransientModel):
+    _name = 'siren.wizard'
+    _description = 'Get values from companies'
+
+
+    ## Default functions
+    @api.model
+    def _default_name(self):
+        return self.env['res.partner'].browse(self.env.context.get('active_id')).name
+
+    @api.model
+    def _default_partner(self):
+        return self.env.context.get('active_id')
+
+    ## Fields
+    name = fields.Char(string='Company', default=_default_name)
+    line_ids = fields.One2many('siren.wizard.line', 'wizard_id', string="Results",)
+    partner_id = fields.Integer('Partner', default=_default_partner)
+
+
+    ## Action
+    @api.model
+    def _prepare_partner_from_data(self, data):
+        return {
+            'street': data.get('l4_normalisee', False),
+            'zip': data.get('codpos', False),
+            'city': data.get('libcom', False),
+            'siren': data.get('siren', False),
+            'siret': data.get('siret', False),
+            'category': data.get('categorie', False),
+            'creation_date': data.get('dcret', False),
+            'ape': data.get('apen700', False),
+            'ape_label': data.get('libapet', False),
+            'legal_type': data.get('libnj', False),
+            'staff': data.get('efetcent', 0),
+        }
+
+    def get_lines(self):
+        # Get request
+        r = requests.get(URL.format(request=self.name))
+        # Serialization request to JSON
+        companies = r.json()
+        # Unlink all company lines
+        self.line_ids.unlink()
+        # Fill new company lines
+        for company in companies['records']:
+            new_company = self.line_ids.create({
+                    'wizard_id': self.id,
+                    'name': company['fields']['l1_normalisee'],
+                })
+            new_company.update(self._prepare_partner_from_data(company['fields']))
+        return { "type": "ir.actions.do_nothing", }
