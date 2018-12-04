766647878bcf5df6c614a51e64ff9783e4e1a7d8
remi-filament <30716308+remi-filament@users.noreply.github.com>
2018-12-04 17:52:05 +0100

    Fix Flake8 reported issues

--- l10n_fr_siren_lookup/models/res_partner.py
+++ l10n_fr_siren_lookup/models/res_partner.py
@@ -3,13 +3,13 @@
 # © 2018 Le Filament (<http://www.le-filament.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
-    ## Fields
+    # Fields
     legal_type = fields.Char("Legal Type")
     siren = fields.Char("SIREN")
     siret = fields.Char("SIRET")
