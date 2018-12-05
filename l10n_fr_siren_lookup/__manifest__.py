c7b23c127b20dae77e64a6f39d0d4265d323606c
remi-filament <30716308+remi-filament@users.noreply.github.com>
2018-12-05 10:40:04 +0100

    Align manifest file with OCA template

--- l10n_fr_siren_lookup/__manifest__.py
+++ l10n_fr_siren_lookup/__manifest__.py
@@ -1,22 +1,22 @@
 # -*- coding: utf-8 -*-
 # © 2018 Le Filament (<https://le-filament.com>)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
-    'name': 'SIREN Lookup',
-    'summary': "Lookup partner company in French SIREN database",
-    'version': '10.0.1.0.0',
-    'license': 'AGPL-3',
-    'author': 'Le Filament, Odoo Community Association (OCA)',
-    'category': 'Partner',
-    'depends': [
-        'base'
+    "name": "SIREN Lookup",
+    "summary": "Lookup partner company in French SIREN database",
+    "version": "10.0.1.0.0",
+    "category": "Partner",
+    "website": "https://github.com/OCA/l10n-france",
+    "author": "Le Filament, Odoo Community Association (OCA)",
+    "maintainers": ["remi-filament"],
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "base",
     ],
-    'maintainers': [
-        'remi-filament',
-    ],
-    'website': 'http://www.le-filament.com',
-    'data': [
-        'wizard/siren_wizard.xml',
-        'views/res_partner.xml',
+    "data": [
+        "wizard/siren_wizard.xml",
+        "views/res_partner.xml",
     ],
 }
