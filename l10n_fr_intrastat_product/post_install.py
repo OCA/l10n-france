3d1c8529b79076326c8dd5d7f533346c5c7ad729
Alexis de Lattre <alexis.delattre@akretion.com>
2018-12-06 19:36:40 +0100

    Auto-configure account.tax.template
    New post-install script that uses the information of account.tax.template
    Finalise port to v11

--- l10n_fr_intrastat_product/post_install.py
+++ l10n_fr_intrastat_product/post_install.py
@@ -1,7 +1,12 @@
-# © 2017 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# Copyright 2017-2018 Akretion France
+# @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import api, SUPERUSER_ID
+from odoo.tools import file_open
+from lxml import etree
+import logging
+logger = logging.getLogger(__name__)
 
 
 def set_fr_company_intrastat(cr, registry):
@@ -11,16 +16,22 @@ def set_fr_company_intrastat(cr, registry):
     So, when it computes the field on module installation, the
     departments are not available in the DB, so the department_id field
     on res.partner stays null. This post_install script fixes this."""
+    f = file_open(
+        'l10n_fr_intrastat_product/data/account_tax_template.xml', 'rb')
+    xml_root = etree.parse(f)
+    data = {}
+    for record in xml_root.xpath('//record'):
+        xmlid = record.attrib['id']
+        data[xmlid] = {}
+        for xfield in record.xpath('field'):
+            xfield_dict = xfield.attrib
+            data[xmlid][xfield_dict['name']] = xfield_dict.get('ref')
+    logger.debug('set_unece_on_taxes data=%s', data)
     with api.Environment.manage():
         env = api.Environment(cr, SUPERUSER_ID, {})
+        imdo = env['ir.model.data']
+        ato = env['account.tax']
         fr_id = env.ref('base.fr').id
-        fr_vat_tax_codes = [
-            '20.0', '8.5', '10.0', '5.5', '2.1',
-            '20.0-TTC', '8.5-TTC', '10.0-TTC', '5.5-TTC', '2.1-TTC',
-            'ACH-20.0', 'ACH-8.5', 'ACH-10.0', 'ACH-5.5', 'ACH-2.1',
-            'ACH-20.0-TTC', 'ACH-8.5-TTC', 'ACH-10.0-TTC', 'ACH-5.5-TTC',
-            'ACH-2.1-TTC',
-            'IMMO-20.0', 'IMMO-8.5', 'IMMO-10.0', 'IMMO-5.5', 'IMMO-2.1']
         companies = env['res.company'].search([
             ('partner_id.country_id', '=', fr_id)])
         out_inv_trans_id = env.ref(
@@ -36,7 +47,22 @@ def set_fr_company_intrastat(cr, registry):
                 'intrastat_transaction_in_invoice': in_inv_trans_id,
                 'intrastat_accessory_costs': True,
                 })
-            fr_vat_taxes = env['account.tax'].search([
-                ('description', 'in', fr_vat_tax_codes)])
-            fr_vat_taxes.write({'exclude_from_intrastat_if_present': True})
+            taxes = ato.search([('company_id', '=', company.id)])
+            for tax in taxes:
+                xmlid_obj = imdo.search([
+                    ('model', '=', 'account.tax'),
+                    ('module', '=', 'l10n_fr'),
+                    ('res_id', '=', tax.id)], limit=1)
+                if (
+                        xmlid_obj and xmlid_obj.name and
+                        len(xmlid_obj.name.split('_')) > 1):
+                    # Remove the 'companyID_' prefix from XMLID of tax
+                    xmlid_ori_end = '_'.join(xmlid_obj.name.split('_')[1:])
+                    xmlid_ori = 'l10n_fr.%s' % xmlid_ori_end
+                    if data.get(xmlid_ori):
+                        logger.debug(
+                            'set_fr_company_intrastat writing '
+                            'exclude_from_intrastat_if_present=True on '
+                            'tax ID %d', tax.id)
+                        tax.write({'exclude_from_intrastat_if_present': True})
     return
