d45979d2ff69547cc116ea6955636e5caa13fb6c
Alexis de Lattre <alexis.delattre@akretion.com>
2019-04-08 19:59:20 +0200

    MIG account_bank_statement_import_fr_cfonb to v12

--- account_bank_statement_import_fr_cfonb/__manifest__.py
+++ account_bank_statement_import_fr_cfonb/__manifest__.py
@@ -1,15 +1,14 @@
-# -*- coding: utf-8 -*-
-# © 2014-2016 Akretion (http://www.akretion.com)
+# Copyright 2014-2019 Akretion France (http://www.akretion.com)
 # @author Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Import French CFONB Bank Statements',
-    'version': '10.0.1.0.0',
+    'version': '12.0.1.0.0',
     'license': 'AGPL-3',
     'author': "Akretion,Odoo Community Association (OCA)",
-    'website': 'http://www.akretion.com',
-    'summary': 'Import French CFONB files as Bank Statements in Odoo',
+    'website': 'https://github.com/OCA/l10n-france',
+    'summary': 'Import CFONB bank statements files in Odoo',
     'depends': ['account_bank_statement_import'],
     'data': ['views/account_bank_statement_import.xml'],
     'installable': True,
