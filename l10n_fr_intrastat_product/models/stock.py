bb9c86a0a4b2e12fdb46f6584c31cf05b7f895ec
Alexis de Lattre <alexis.delattre@akretion.com>
2019-05-28 19:22:43 +0200

    [MIG] l10n_fr_intrastat_product to v12

--- l10n_fr_intrastat_product/models/stock.py
+++ l10n_fr_intrastat_product/models/stock.py
@@ -1,15 +1,14 @@
-# © 2010-2017 Akretion (http://www.akretion.com)
+# Copyright 2010-2019 Akretion France (http://www.akretion.com)
 # @author Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, api, _
+from odoo import models, _
 from odoo.exceptions import UserError
 
 
 class StockWarehouse(models.Model):
     _inherit = 'stock.warehouse'
 
-    @api.multi
     def get_fr_department(self):
         self.ensure_one()
         if not self.partner_id:
@@ -20,17 +19,15 @@ class StockWarehouse(models.Model):
 class StockLocation(models.Model):
     _inherit = 'stock.location'
 
-    @api.multi
     def get_fr_department(self):
         '''I don't think it's a good idea to use the get_intrastat_region()
         of intrastat_product because it doesn't return the same object.
         That's why there is a small code duplication in this method'''
         self.ensure_one()
         locations = self.search(
-            [('parent_left', '<=', self.parent_left),
-             ('parent_right', '>=', self.parent_right)])
+            [('id', 'parent_of', self.id)])
         warehouses = self.env['stock.warehouse'].search(
-            [('lot_stock_id', 'in', [x.id for x in locations])])
+            [('lot_stock_id', 'in', locations.ids)])
         if warehouses:
             return warehouses[0].get_fr_department()
         return None
