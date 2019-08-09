c58cba82d4e5c2e010c3a065d2d6bfedaefef10c
Alexis de Lattre <alexis.delattre@akretion.com>
2019-08-09 19:33:44 +0200

    [MIG] account_banking_fr_lcr from v10 to v12

--- account_banking_fr_lcr/__manifest__.py
+++ account_banking_fr_lcr/__manifest__.py
@@ -1,15 +1,15 @@
-# -*- coding: utf-8 -*-
-# © 2014-2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# Copyright 2014-2019 Akretion France (http://www.akretion.com/)
+# @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 
 {
     'name': 'French Letter of Change',
     'summary': 'Create French LCR CFONB files',
-    'version': '10.0.1.0.0',
+    'version': '12.0.1.0.0',
     'license': 'AGPL-3',
     'author': "Akretion,Odoo Community Association (OCA)",
-    'website': 'http://www.akretion.com',
+    'website': 'https://github.com/OCA/l10n-france',
     'category': 'French localisation',
     'depends': ['account_payment_order', 'document'],
     'external_dependencies': {
--- account_banking_fr_lcr/models/__init__.py
+++ account_banking_fr_lcr/models/__init__.py
@@ -1,4 +1,2 @@
-# -*- coding: utf-8 -*-
-
 from . import account_payment_order
 from . import account_move_line
