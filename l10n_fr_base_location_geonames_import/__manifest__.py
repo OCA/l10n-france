a2159ad3fcf48b6f2280f9f71dba1a5546a1b841
remi-filament <30716308+remi-filament@users.noreply.github.com>
2019-10-24 18:21:56 +0200

    Migration of l10n_fr_base_geonames_import to v12

--- l10n_fr_base_location_geonames_import/__manifest__.py
+++ l10n_fr_base_location_geonames_import/__manifest__.py
@@ -1,15 +1,15 @@
-# -*- coding: utf-8 -*-
+# © 2019 Le Filament (https://le-filament.com).
 # © 2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'French Localization for Base Location Geonames Import',
-    'version': '10.0.1.0.0',
+    'version': '12.0.1.0.0',
     'category': 'Extra Tools',
     'license': 'AGPL-3',
-    'summary': 'France-specific tuning for import of better zip entries '
-    'from Geonames',
-    'author': "Akretion,Odoo Community Association (OCA)",
+    'summary': 'France-specific tuning for import of better zip entries from '
+               'Geonames',
+    'author': "Le Filament,Akretion,Odoo Community Association (OCA)",
     'website': 'http://www.akretion.com',
     'depends': ['base_location_geonames_import'],
     'external_dependencies': {'python': ['unidecode']},
---
+++ l10n_fr_base_location_geonames_import/readme/USAGE.rst
@@ -0,0 +1,2 @@
+This module does not introduce any extra menu/button on the interface.
+It overloads the function called by the wizard allowing to import location from geonames.
