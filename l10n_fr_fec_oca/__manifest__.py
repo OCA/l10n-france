3c2f4146bab3e1294af17e323986391de8e8b203
Alexis de Lattre <alexis.delattre@akretion.com>
2019-11-27 00:03:10 +0100

    Improve FEC generation: encoding, field delimiter, partner export are now configurable
    Fix filename when VAT number contrains spaces
    PEP8

--- l10n_fr_fec_oca/__manifest__.py
+++ l10n_fr_fec_oca/__manifest__.py
@@ -1,15 +1,20 @@
-#-*- coding:utf-8 -*-
-
-# Copyright (C) 2013-2015 Akretion (http://www.akretion.com)
+# Copyright 2013-2019 Akretion France (http://www.akretion.com/)
+# Copyright 2016-2019 Odoo SA (https://www.odoo.com/fr_FR/)
+# @author: Alexis de Lattre <alexis.delattre@akretion.com>
 
 {
     'name': 'France - FEC',
     'category': 'Accounting',
+    'license': 'LGPL-3',
     'summary': "Fichier d'Échange Informatisé (FEC) for France",
     'author': "Akretion,Odoo Community Association (OCA)",
-    'depends': ['l10n_fr', 'account'],
+    'website': 'https://github.com/OCA/l10n-france',
+    'depends': ['l10n_fr', 'account', 'date_range'],
+    'external_dependencies': {
+        'python': ['unicodecsv', 'unidecode'],
+        },
     'data': [
-        'wizard/account_fr_fec_view.xml',
+        'wizard/account_fr_fec_oca_view.xml',
     ],
-    'auto_install': True,
+    'installable': True,
 }
