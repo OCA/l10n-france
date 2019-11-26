f0194d51d716bd096d56f3f585c185a51e393bb1
Alexis de Lattre <alexis.delattre@akretion.com>
2019-11-26 16:21:12 +0100

    Check-in of the l10n_fr_fec module from github.com/odoo/odoo 12.0 branch under the name l10n_fr_fec_oca
    This module was initially developped by Alexis de Lattre in OCA/l10n-france for v8, then moved to odoo core and received several contribution/improvements by Odoo SA employees and it also received fixes and improvements by community members.

---
+++ l10n_fr_fec_oca/__manifest__.py
@@ -0,0 +1,15 @@
+#-*- coding:utf-8 -*-
+
+# Copyright (C) 2013-2015 Akretion (http://www.akretion.com)
+
+{
+    'name': 'France - FEC',
+    'category': 'Accounting',
+    'summary': "Fichier d'Échange Informatisé (FEC) for France",
+    'author': "Akretion,Odoo Community Association (OCA)",
+    'depends': ['l10n_fr', 'account'],
+    'data': [
+        'wizard/account_fr_fec_view.xml',
+    ],
+    'auto_install': True,
+}
