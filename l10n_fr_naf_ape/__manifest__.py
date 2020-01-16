3b9800d6ca8a960636ea2336b9cc7a13ae476088
Manuel Claeys Bouuaert <manuel.claeys.b@gmail.com>
2020-01-16 11:31:06 +0100

    [MIG] bump version, remove utf comment, update readme

--- l10n_fr_naf_ape/__manifest__.py
+++ l10n_fr_naf_ape/__manifest__.py
@@ -1,11 +1,10 @@
-# -*- coding: utf-8 -*-
 # © 2011 Numérigraphe SARL
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'French NAF partner categories and APE code',
-    'version': '10.0.1.0.0',
-    'author': u'Numérigraphe SARL,Odoo Community Association (OCA)',
+    'version': '11.0.1.0.0',
+    'author': 'Numérigraphe SARL,Odoo Community Association (OCA)',
     'category': 'French Localization',
     'depends': [
         'l10n_eu_nace'
