5d5917efb8b0e567fb812b83c9697885f9b6ddd7
Alexis de Lattre <alexis.delattre@akretion.com>
2020-01-31 00:31:46 +0100

    l10n_fr_das2: add support for all different kinds of expense of the official report

--- l10n_fr_das2/models/account_account.py
+++ l10n_fr_das2/models/account_account.py
@@ -10,7 +10,23 @@ from odoo.exceptions import ValidationError
 class AccountAccount(models.Model):
     _inherit = 'account.account'
 
-    fr_das2 = fields.Boolean(string='DAS2')
+    fr_das2 = fields.Selection('get_das2_types', string='DAS2')
+
+    @api.model
+    def get_das2_types(self):
+        res = [
+            ('fee', u'Honoraires et vacations'),
+            ('commission', u'Commissions'),
+            ('brokerage', u'Courtages'),
+            ('discount', u'Ristournes'),
+            ('attendance_fee', u'Jetons de présence'),
+            ('copyright_royalties', u"Droits d'auteur"),
+            ('licence_royalties', u"Droits d'inventeur"),
+            ('other_income', u'Autre rémunérations'),
+            ('allowance', u'Indemnités et remboursements'),
+            ('withholding_tax', u'Retenue à la source'),
+            ]
+        return res
 
     @api.constrains('fr_das2', 'user_type_id')
     def das2_check(self):
@@ -26,4 +42,8 @@ class AccountAccount(models.Model):
 class AccountAccountTemplate(models.Model):
     _inherit = 'account.account.template'
 
-    fr_das2 = fields.Boolean(string='DAS2')
+    fr_das2 = fields.Selection('get_das2_types', string='DAS2')
+
+    @api.model
+    def get_das2_types(self):
+        return self.env['account.account'].get_das2_types()
