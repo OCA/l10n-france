ac2a74c954873206022c033ca15157e605b454e5
Alexis de Lattre <alexis.delattre@akretion.com>
2020-02-11 22:42:13 +0100

    [MIG] l10n_fr_account_invoice_facturx to v12 (from v10)
    Rename the module from l10n_fr_account_invoice_factur-x to l10n_fr_account_invoice_facturx (dash removed)

---
+++ l10n_fr_account_invoice_facturx/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2017-2020 Akretion France
+# @author: Alexis de Lattre <alexis.delattre@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Factur-X Invoices for France',
+    'version': '12.0.1.0.0',
+    'category': 'Localisation',
+    'license': 'AGPL-3',
+    'summary': "France-specific module to generate Factur-X invoices",
+    'author': 'Akretion,Odoo Community Association (OCA)',
+    'website': 'https://github.com/OCA/l10n-france',
+    'depends': [
+        'account_invoice_facturx',
+        'l10n_fr_siret',
+        ],
+    'auto_install': True,
+    'installable': True,
+}
