8d0aedc3040bafcf1fd06ad396226cce757b053c
Krunal <krunal267@gmail.com>
2020-02-14 16:59:58 +0530

    [MIG] Migration to 12.0: l10n_fr_fec_group_sale_purchase

---
+++ l10n_fr_fec_group_sale_purchase/__openerp__.py
@@ -0,0 +1,16 @@
+# -*- encoding: utf-8 -*-
+{
+    'name': 'France - FEC Group Sale Purchase',
+    'version': '9.0.1.0.0',
+    'category': 'Localization',
+    'summary': "Fichier d'Échange Informatisé (FEC) for France",
+    'author': "Druidoo",
+    'website': 'www.druidoo.fr',
+    'depends': [
+        'l10n_fr_fec',
+    ],
+    'data': [
+        'wizard/fec_view.xml',
+    ],
+    'installable': True,
+}
