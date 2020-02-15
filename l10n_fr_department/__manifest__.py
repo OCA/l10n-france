e7ec4ac4a390ddb22d53c1acad742d0237c04c88
zuher83 <zu83@outlook.fr>
2020-02-15 16:41:22 +0100

    [MIG] l10n_fr_department: migration 12.0 to 13.0

--- l10n_fr_department/__manifest__.py
+++ l10n_fr_department/__manifest__.py
@@ -6,7 +6,7 @@
 {
     'name': 'French Departments (Départements)',
     'summary': 'Populate Database with French Departments (Départements)',
-    'version': '12.0.1.0.0',
+    'version': '13.0.1.0.0',
     'category': 'French Localization',
     'author': 'GRAP, '
               'Akretion, '
