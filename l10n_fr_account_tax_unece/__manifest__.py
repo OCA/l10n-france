41c6edea6b8ec1e6c88fc8b7b854de7c5619db53
Alexis de Lattre <alexis.delattre@akretion.com>
2020-02-28 00:09:41 +0100

    [MIG] l10n_fr_account_tax_unece from v12 to v13

--- l10n_fr_account_tax_unece/__manifest__.py
+++ l10n_fr_account_tax_unece/__manifest__.py
@@ -1,18 +1,19 @@
-# Copyright 2017-2019 Akretion France (http://www.akretion.com/)
+# Copyright 2017-2020 Akretion France (http://www.akretion.com/)
 # @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': u'L10n FR Account Tax UNECE',
-    'summary': "Auto-configure UNECE params on French taxes",
-    'version': '12.0.1.0.0',
-    'category': 'French Localization',
-    'author': "Akretion,Odoo Community Association (OCA)",
-    'website': 'https://github.com/OCA/l10n-france',
-    'license': 'AGPL-3',
-    'depends': ['l10n_fr', 'account_tax_unece'],
-    'data': ['data/account_tax_template.xml'],
-    'post_init_hook': 'set_unece_on_taxes',
-    'installable': True,
-    'auto_installable': True,
+    "name": "L10n FR Account Tax UNECE",
+    "summary": "Auto-configure UNECE params on French taxes",
+    "version": "13.0.1.0.0",
+    "category": "French Localization",
+    "author": "Akretion,Odoo Community Association (OCA)",
+    "maintainers": ["alexis-via"],
+    "website": "https://github.com/OCA/l10n-france",
+    "license": "AGPL-3",
+    "depends": ["l10n_fr", "account_tax_unece"],
+    "data": ["data/account_tax_template.xml"],
+    "post_init_hook": "set_unece_on_taxes",
+    "installable": True,
+    "auto_installable": True,
 }
