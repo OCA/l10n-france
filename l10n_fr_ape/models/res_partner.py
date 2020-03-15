fafab2c104ece8f1ea670c9a7aace2c01f09e20a
MonsieurB <monsieurb@saaslys.com>
2020-03-15 16:13:27 +0100

    nothing

--- l10n_fr_ape/models/res_partner.py
+++ l10n_fr_ape/models/res_partner.py
@@ -1,11 +1,10 @@
 # Copyright 2020 MonsieurB <monsieurb@saaslys.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class ResPartnerIndustry(models.Model):
     _inherit = "res.partner.industry"
 
     code = fields.Char(string='Code')
-
