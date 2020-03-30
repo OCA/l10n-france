0f2d451368aad8611dcc72c3e465e0a8417ebd87
Alexis de Lattre <alexis.delattre@akretion.com>
2020-03-30 23:57:38 +0200

    [MIG] l10n_fr_cog and l10n_fr_das2 from v10 to v12
    l10n_fr_cog: update country list
    l10n_fr_das2:
    - add demo data,
    - add constraint on lines,
    - improve chatter messages,
    - fix readonly status of 'job' field on lines,
    - handle 'job' field in partner change on lines.

--- l10n_fr_cog/__manifest__.py
+++ l10n_fr_cog/__manifest__.py
@@ -1,12 +1,11 @@
-# -*- coding: utf-8 -*-
 # Copyright 2020 Akretion France (http://www.akretion.com/)
 # @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': u'Code Officiel Géographique',
-    'summary': u'Add Code Officiel Géographique (COG) on countries',
-    'version': '10.0.1.0.0',
+    'name': 'Code Officiel Géographique',
+    'summary': 'Add Code Officiel Géographique (COG) on countries',
+    'version': '12.0.1.0.0',
     'category': 'French Localization',
     'author': "Akretion,Odoo Community Association (OCA)",
     'website': 'https://github.com/OCA/l10n-france',
