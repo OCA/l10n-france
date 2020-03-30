1ac4cfc37acd2a5e5f0149b5ab1f01627dc23052
Alexis de Lattre <alexis.delattre@akretion.com>
2020-03-30 23:57:38 +0200

    [MIG] l10n_fr_cog and l10n_fr_das2 from v10 to v12
    l10n_fr_cog: update country list
    l10n_fr_das2:
    - add demo data,
    - add constraint on lines,
    - improve chatter messages,
    - fix readonly status of 'job' field on lines,
    - handle 'job' field in partner change on lines.

--- l10n_fr_das2/models/partner.py
+++ l10n_fr_das2/models/partner.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2020 Akretion France (http://www.akretion.com/)
 # @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
@@ -10,15 +9,15 @@ class ResPartner(models.Model):
     _inherit = 'res.partner'
 
     fr_das2_type = fields.Selection([
-        ('fee', u'Honoraires et vacations'),
-        ('commission', u'Commissions'),
-        ('brokerage', u'Courtages'),
-        ('discount', u'Ristournes'),
-        ('attendance_fee', u'Jetons de présence'),
-        ('copyright_royalties', u"Droits d'auteur"),
-        ('licence_royalties', u"Droits d'inventeur"),
-        ('other_income', u'Autre rémunérations'),
-        ('allowance', u'Indemnités et remboursements'),
+        ('fee', 'Honoraires et vacations'),
+        ('commission', 'Commissions'),
+        ('brokerage', 'Courtages'),
+        ('discount', 'Ristournes'),
+        ('attendance_fee', 'Jetons de présence'),
+        ('copyright_royalties', "Droits d'auteur"),
+        ('licence_royalties', "Droits d'inventeur"),
+        ('other_income', 'Autre rémunérations'),
+        ('allowance', 'Indemnités et remboursements'),
         ], string='DAS2 Type', track_visibility='onchange')
     fr_das2_job = fields.Char(
         string='DAS2 Job', size=30,
