011ac925e756fc23aa2c79424818009fcd7ba3fa
Alexis de Lattre <alexis.delattre@akretion.com>
2020-03-30 23:57:38 +0200

    [MIG] l10n_fr_cog and l10n_fr_das2 from v10 to v12
    l10n_fr_cog: update country list
    l10n_fr_das2:
    - add demo data,
    - add constraint on lines,
    - improve chatter messages,
    - fix readonly status of 'job' field on lines,
    - handle 'job' field in partner change on lines.

--- l10n_fr_das2/__manifest__.py
+++ l10n_fr_das2/__manifest__.py
@@ -1,12 +1,11 @@
-# -*- coding: utf-8 -*-
 # Copyright 2020 Akretion France (http://www.akretion.com/)
 # @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'DAS2',
-    'version': '10.0.1.0.0',
-    'category': 'Accounting',
+    'version': '12.0.1.0.0',
+    'category': 'Invoicing Management',
     'license': 'AGPL-3',
     'summary': 'DAS2 (France)',
     'author': 'Akretion,Odoo Community Association (OCA)',
@@ -23,8 +22,9 @@
         'security/ir.model.access.csv',
         'views/l10n_fr_das2.xml',
         'views/partner.xml',
-        'views/account_config_settings.xml',
+        'views/config_settings.xml',
     ],
+    'demo': ['demo/demo.xml'],
     'installable': True,
     'application': True,
 }
