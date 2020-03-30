8c632bc44609e5f16b604e798304a06097edadbf
Alexis de Lattre <alexis.delattre@akretion.com>
2020-03-30 23:57:38 +0200

    [MIG] l10n_fr_cog and l10n_fr_das2 from v10 to v12
    l10n_fr_cog: update country list
    l10n_fr_das2:
    - add demo data,
    - add constraint on lines,
    - improve chatter messages,
    - fix readonly status of 'job' field on lines,
    - handle 'job' field in partner change on lines.

--- l10n_fr_cog/__init__.py
+++ l10n_fr_cog/__init__.py
@@ -1,4 +1,2 @@
-# -*- coding: utf-8 -*-
-
 from .post_install import set_fr_cog
 from . import models
--- l10n_fr_cog/models/country.py
+++ l10n_fr_cog/models/country.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2020 Akretion France (http://www.akretion.com/)
 # @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
@@ -10,5 +9,5 @@ class ResCountry(models.Model):
     _inherit = 'res.country'
 
     fr_cog = fields.Integer(
-        string=u'Code Officiel Géographique',
-        help=u"Code Officiel Géographique, by INSEE")
+        string='Code Officiel Géographique',
+        help="Code Officiel Géographique, by INSEE")
