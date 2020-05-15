0213cae054ea3dc4a32c70bc24de8ffd125fd7ac
Alexis de Lattre <alexis.delattre@akretion.com>
2020-05-15 10:15:36 +0200

    cfonb: migrate to v13

--- account_bank_statement_import_fr_cfonb/__manifest__.py
+++ account_bank_statement_import_fr_cfonb/__manifest__.py
@@ -4,9 +4,10 @@
 
 {
     'name': 'Import French CFONB Bank Statements',
-    'version': '12.0.1.0.0',
+    'version': '13.0.1.0.0',
     'license': 'AGPL-3',
     'author': "Akretion,Odoo Community Association (OCA)",
+    'maintainers': ['alexis-via'],
     'website': 'https://github.com/OCA/l10n-france',
     'summary': 'Import CFONB bank statements files in Odoo',
     'depends': ['account_bank_statement_import'],
--- account_bank_statement_import_fr_cfonb/readme/USAGE.rst
+++ account_bank_statement_import_fr_cfonb/readme/USAGE.rst
@@ -1,1 +1,1 @@
-In the dashboard of the *Accounting* menu, click on the button *Import Statement* located on the journal corresponding to the bank account of the statement file you are importing.
+In the dashboard of the *Invoicing* menu, click on the button *Import Statements* located on the journal corresponding to the bank account of the statement file you are importing.
