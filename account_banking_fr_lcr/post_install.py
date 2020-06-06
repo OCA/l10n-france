1443d0e674dd4b6a109b0c353b7f08aa6af6c85b
Alexis de Lattre <alexis.delattre@akretion.com>
2020-06-06 23:33:49 +0200

    black, isort and other stuff on LCR module

--- account_banking_fr_lcr/post_install.py
+++ account_banking_fr_lcr/post_install.py
@@ -2,24 +2,24 @@
 # @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, SUPERUSER_ID
+from odoo import SUPERUSER_ID, api
 
 
 def update_bank_journals(cr, registry):
     with api.Environment.manage():
         env = api.Environment(cr, SUPERUSER_ID, {})
-        ajo = env['account.journal']
-        journals = ajo.search([('type', '=', 'bank')])
-        lcr = env.ref('account_banking_fr_lcr.fr_lcr')
+        ajo = env["account.journal"]
+        journals = ajo.search([("type", "=", "bank")])
+        lcr = env.ref("account_banking_fr_lcr.fr_lcr")
         if lcr:
-            journals.write({'inbound_payment_method_ids': [(4, lcr.id)]})
+            journals.write({"inbound_payment_method_ids": [(4, lcr.id)]})
             # This module doesn't depend on account_payment_unece
             # so we test the attribute 'unece_id' on the payment method
             # to know if the module is installed or not
-            if hasattr(lcr, 'unece_id'):
+            if hasattr(lcr, "unece_id"):
                 lcr_unece = env.ref(
-                    'account_payment_unece.payment_means_24',
-                    raise_if_not_found=False)
+                    "account_payment_unece.payment_means_24", raise_if_not_found=False
+                )
                 if lcr_unece:
-                    lcr.write({'unece_id': lcr_unece.id})
+                    lcr.write({"unece_id": lcr_unece.id})
     return
