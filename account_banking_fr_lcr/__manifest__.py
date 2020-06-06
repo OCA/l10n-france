55e6f6dc4b11a5552306ea778cf4ce5cc4018452
Alexis de Lattre <alexis.delattre@akretion.com>
2020-06-06 23:30:58 +0200

    [MIG] LCR module to v13

--- account_banking_fr_lcr/__manifest__.py
+++ account_banking_fr_lcr/__manifest__.py
@@ -1,4 +1,4 @@
-# Copyright 2014-2019 Akretion France (http://www.akretion.com/)
+# Copyright 2014-2020 Akretion France (http://www.akretion.com/)
 # @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
@@ -6,12 +6,13 @@
 {
     'name': 'French Letter of Change',
     'summary': 'Create French LCR CFONB files',
-    'version': '12.0.1.0.0',
+    'version': '13.0.1.0.0',
     'license': 'AGPL-3',
     'author': "Akretion,Odoo Community Association (OCA)",
+    'maintainers': ['alexis-via'],
     'website': 'https://github.com/OCA/l10n-france',
     'category': 'French localisation',
-    'depends': ['account_payment_order', 'document'],
+    'depends': ['account_payment_order'],
     'external_dependencies': {
         'python': ['unidecode'],
         },
