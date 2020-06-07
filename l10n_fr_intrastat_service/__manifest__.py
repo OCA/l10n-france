3660b7f57ebcc637b17cb46a576b756437cf5d40
Alexis de Lattre <alexis.delattre@akretion.com>
2020-06-07 20:34:45 +0200

    [MIG] l10n_fr_intrastat_service to v13

--- l10n_fr_intrastat_service/__manifest__.py
+++ l10n_fr_intrastat_service/__manifest__.py
@@ -1,14 +1,15 @@
-# Copyright 2010-2019 Akretion France (http://www.akretion.com)
+# Copyright 2010-2020 Akretion France (http://www.akretion.com)
 # @author Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'France Intrastat Service (DES)',
-    'version': '12.0.1.0.0',
+    'version': '13.0.1.0.0',
     'category': 'Localisation/Report Intrastat',
     'license': 'AGPL-3',
     'summary': 'Module for Intrastat service reporting (DES) for France',
     'author': 'Akretion,Odoo Community Association (OCA)',
+    'maintainers': ['alexis-via'],
     'website': 'https://github.com/OCA/l10n-france',
     'depends': ['intrastat_base'],
     'data': [
