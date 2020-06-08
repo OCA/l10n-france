bae6c1a3dc15001715523253324bd97db79ce17c
Alexis de Lattre <alexis.delattre@akretion.com>
2020-06-08 22:56:25 +0200

    [MIG] DEB to v13

--- l10n_fr_intrastat_product/__manifest__.py
+++ l10n_fr_intrastat_product/__manifest__.py
@@ -1,14 +1,15 @@
-# Copyright 2010-2019 Akretion France (http://www.akretion.com)
+# Copyright 2010-2020 Akretion France (http://www.akretion.com)
 # @author Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'DEB',
-    'version': '12.0.1.0.1',
+    'version': '13.0.1.0.0',
     'category': 'Localisation/Report Intrastat',
     'license': 'AGPL-3',
     'summary': "DEB (Déclaration d'Échange de Biens) for France",
     'author': 'Akretion,Odoo Community Association (OCA)',
+    'maintainers': ['alexis-via'],
     'website': 'https://github.com/OCA/l10n-france',
     'depends': [
         'l10n_fr',
