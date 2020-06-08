c1413be3819aff33736e91785fdeaac070d60375
Alexis de Lattre <alexis.delattre@akretion.com>
2020-06-08 22:58:48 +0200

    DEB: black, isort, ...

--- l10n_fr_intrastat_product/models/stock.py
+++ l10n_fr_intrastat_product/models/stock.py
@@ -2,32 +2,32 @@
 # @author Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, _
+from odoo import _, models
 from odoo.exceptions import UserError
 
 
 class StockWarehouse(models.Model):
-    _inherit = 'stock.warehouse'
+    _inherit = "stock.warehouse"
 
     def get_fr_department(self):
         self.ensure_one()
         if not self.partner_id:
-            raise UserError(_('Missing partner on warehouse %s') % self.name)
+            raise UserError(_("Missing partner on warehouse %s") % self.name)
         return self.partner_id.department_id
 
 
 class StockLocation(models.Model):
-    _inherit = 'stock.location'
+    _inherit = "stock.location"
 
     def get_fr_department(self):
-        '''I don't think it's a good idea to use the get_intrastat_region()
+        """I don't think it's a good idea to use the get_intrastat_region()
         of intrastat_product because it doesn't return the same object.
-        That's why there is a small code duplication in this method'''
+        That's why there is a small code duplication in this method"""
         self.ensure_one()
-        locations = self.search(
-            [('id', 'parent_of', self.id)])
-        warehouses = self.env['stock.warehouse'].search(
-            [('lot_stock_id', 'in', locations.ids)])
+        locations = self.search([("id", "parent_of", self.id)])
+        warehouses = self.env["stock.warehouse"].search(
+            [("lot_stock_id", "in", locations.ids)]
+        )
         if warehouses:
             return warehouses[0].get_fr_department()
         return None
