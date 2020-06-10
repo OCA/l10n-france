cff7db8c25ba090853239d6d5c669180929a0141
Alexis de Lattre <alexis.delattre@akretion.com>
2020-06-10 23:25:33 +0200

    Black, isort on chorus modules

--- l10n_fr_chorus_account/models/config_settings.py
+++ l10n_fr_chorus_account/models/config_settings.py
@@ -6,24 +6,32 @@ from odoo import fields, models
 
 
 class ResConfigSettings(models.TransientModel):
-    _inherit = 'res.config.settings'
+    _inherit = "res.config.settings"
 
     group_chorus_api = fields.Boolean(
-        string='Use Chorus Pro API',
-        implied_group='l10n_fr_chorus_account.group_chorus_api',
+        string="Use Chorus Pro API",
+        implied_group="l10n_fr_chorus_account.group_chorus_api",
         help="If you select 'Use Chorus Pro API', it will add all users to the "
-        "Chorus API group.")
+        "Chorus API group.",
+    )
     fr_chorus_api_login = fields.Char(
-        related='company_id.fr_chorus_api_login', readonly=False)
+        related="company_id.fr_chorus_api_login", readonly=False
+    )
     fr_chorus_api_password = fields.Char(
-        related='company_id.fr_chorus_api_password', readonly=False)
+        related="company_id.fr_chorus_api_password", readonly=False
+    )
     fr_chorus_qualif = fields.Boolean(
-        related='company_id.fr_chorus_qualif', readonly=False)
+        related="company_id.fr_chorus_qualif", readonly=False
+    )
     fr_chorus_invoice_format = fields.Selection(
-        related='company_id.fr_chorus_invoice_format', readonly=False)
+        related="company_id.fr_chorus_invoice_format", readonly=False
+    )
     fr_chorus_check_commitment_number = fields.Boolean(
-        related='company_id.fr_chorus_check_commitment_number', readonly=False)
+        related="company_id.fr_chorus_check_commitment_number", readonly=False
+    )
     fr_chorus_pwd_expiry_date = fields.Date(
-        related='company_id.fr_chorus_pwd_expiry_date', readonly=False)
+        related="company_id.fr_chorus_pwd_expiry_date", readonly=False
+    )
     fr_chorus_expiry_remind_user_ids = fields.Many2many(
-        related='company_id.fr_chorus_expiry_remind_user_ids', readonly=False)
+        related="company_id.fr_chorus_expiry_remind_user_ids", readonly=False
+    )
