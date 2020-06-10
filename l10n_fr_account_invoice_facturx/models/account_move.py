41238557883b789ae7fa4117c297b5e57fa9dabc
Alexis de Lattre <alexis.delattre@akretion.com>
2020-06-10 23:25:33 +0200

    Black, isort on chorus modules

--- l10n_fr_account_invoice_facturx/models/account_move.py
+++ l10n_fr_account_invoice_facturx/models/account_move.py
@@ -6,12 +6,12 @@ from odoo import api, models
 
 
 class AccountMove(models.Model):
-    _inherit = 'account.move'
+    _inherit = "account.move"
 
     @api.model
     def _cii_get_party_identification(self, commercial_partner):
         res = super()._cii_get_party_identification(commercial_partner)
         # partner.siret has a value even if partner.nic == False
         if commercial_partner.siren and commercial_partner.nic:
-            res['0002'] = commercial_partner.siret
+            res["0002"] = commercial_partner.siret
         return res
