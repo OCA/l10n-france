e395ebd0f66dae235a1f86a1e2fd29149a511d5a
Alexis de Lattre <alexis.delattre@akretion.com>
2020-06-10 23:25:33 +0200

    Black, isort on chorus modules

--- l10n_fr_account_invoice_facturx/__manifest__.py
+++ l10n_fr_account_invoice_facturx/__manifest__.py
@@ -3,17 +3,14 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Factur-X Invoices for France',
-    'version': '13.0.1.0.0',
-    'category': 'Localisation',
-    'license': 'AGPL-3',
-    'summary': "France-specific module to generate Factur-X invoices",
-    'author': 'Akretion,Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/l10n-france',
-    'depends': [
-        'account_invoice_facturx',
-        'l10n_fr_siret',
-        ],
-    'auto_install': True,
-    'installable': True,
+    "name": "Factur-X Invoices for France",
+    "version": "13.0.1.0.0",
+    "category": "Localisation",
+    "license": "AGPL-3",
+    "summary": "France-specific module to generate Factur-X invoices",
+    "author": "Akretion,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-france",
+    "depends": ["account_invoice_facturx", "l10n_fr_siret",],
+    "auto_install": True,
+    "installable": True,
 }
