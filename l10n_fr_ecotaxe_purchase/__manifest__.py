53f86899a270253bd8e6cef9d941344070f31543
Mourad <mourad.elhadj.mimoune@akretion.com>
2020-07-24 11:07:15 +0200

    [FIX] split l10n_fr_ecotaxe in 3 modules (add l10n_fr_ecotaxe_sale, l10n_fr_ecotaxe_purchase)

---
+++ l10n_fr_ecotaxe_purchase/__manifest__.py
@@ -0,0 +1,15 @@
+# © 2014-2020 Akretion (http://www.akretion.com)
+#   @author Mourad EL HADJ MIMOUNE <mourad.elhadj.mimoune@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "France Purchase Ecotaxe",
+    "summary": "Purchase Ecotaxe used in French localisation contexte",
+    "version": "12.0.0.1.0",
+    "author": "Akretion,Odoo Community Association (OCA)",
+    "website": "http://www.akretion.com",
+    "category": "Localization/Account Taxes",
+    "license": "AGPL-3",
+    "depends": ["purchase"],
+    "data": ["views/purchase_view.xml"],
+    "installable": True,
+}
