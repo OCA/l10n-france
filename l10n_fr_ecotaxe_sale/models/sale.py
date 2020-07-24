bdf3acd877ca4ab153d71d2fd39e89806b5eb354
Mourad <mourad.elhadj.mimoune@akretion.com>
2020-07-24 11:07:15 +0200

    [FIX] split l10n_fr_ecotaxe in 3 modules (add l10n_fr_ecotaxe_sale, l10n_fr_ecotaxe_purchase)

---
+++ l10n_fr_ecotaxe_sale/models/sale.py
@@ -0,0 +1,46 @@
+# © 2015 -2020 Akretion (http://www.akretion.com)
+#   @author Mourad EL HADJ MIMOUNE <mourad.elhadj.mimoune@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import api, fields, models
+
+
+class SaleOrderLine(models.Model):
+
+    _inherit = "sale.order.line"
+
+    subtotal_ecotaxe = fields.Float(
+        store=True, compute="_compute_ecotaxe", oldname="amount_ecotaxe"
+    )
+    unit_ecotaxe_amount = fields.Float(
+        string="ecotaxe Unit.", store=True, compute="_compute_ecotaxe",
+    )
+
+    @api.multi
+    @api.depends("product_id", "product_uom_qty")
+    def _compute_ecotaxe(self):
+        for line in self:
+            line.unit_ecotaxe_amount = line.product_id.ecotaxe_amount
+
+            if line.pricelist_id:
+                cur = line.pricelist_id.currency_id
+                line.unit_ecotaxe_amount = cur.round(line.unit_ecotaxe_amount)
+            line.subtotal_ecotaxe = line.unit_ecotaxe_amount * line.product_uom_qty
+
+
+class SaleOrder(models.Model):
+
+    _inherit = "sale.order"
+
+    amount_ecotaxe = fields.Float(
+        string="Included Ecotaxe", store=True, compute="_compute_ecotaxe"
+    )
+
+    @api.multi
+    @api.depends("order_line.subtotal_ecotaxe")
+    def _compute_ecotaxe(self):
+        for order in self:
+            val_ecotaxe = 0.0
+            for line in order.order_line_ids:
+                val_ecotaxe += line.subtotal_ecotaxe
+            order.amount_ecotaxe = val_ecotaxe
