0efadf55ed35f2e6ae4b82a1b552d2ac2ab0bcf8
Mourad <mourad.elhadj.mimoune@akretion.com>
2020-07-24 11:07:15 +0200

    [FIX] split l10n_fr_ecotaxe in 3 modules (add l10n_fr_ecotaxe_sale, l10n_fr_ecotaxe_purchase)

--- l10n_fr_ecotaxe/__manifest__.py
+++ l10n_fr_ecotaxe/__manifest__.py
@@ -1,27 +1,22 @@
-# -*- coding: utf-8 -*-
-# © 2014-2016 Akretion (http://www.akretion.com)
+# © 2014-2020 Akretion (http://www.akretion.com)
 #   @author Mourad EL HADJ MIMOUNE <mourad.elhadj.mimoune@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    'name': 'France Custom Ecotaxe',
-    'summary': 'Use Ecotaxe in French localisation contexte',
-    'version': '12.0.0.1.0',
-    'author': "Akretion,Odoo Community Association (OCA)",
-    'website': 'http://www.akretion.com',
-    'category': 'Localization/Account Taxes',
-    'license': 'AGPL-3',
-    'depends': [
-        'account',
+    "name": "France Custom Ecotaxe",
+    "summary": "Use Ecotaxe in French localisation contexte",
+    "version": "12.0.0.1.0",
+    "author": "Akretion,Odoo Community Association (OCA)",
+    "website": "http://www.akretion.com",
+    "category": "Localization/Account Taxes",
+    "license": "AGPL-3",
+    "depends": ["account"],
+    "data": [
+        "data/account_data.xml",
+        "security/ir_rule.xml",
+        "security/ir.model.access.csv",
+        "views/product_view.xml",
+        "views/account_invoice_view.xml",
+        "views/account_ecotaxe_classification_view.xml",
     ],
-    'data': [
-        'data/account_data.xml',
-        'security/ir_rule.xml',
-        'security/ir_model_access.yml',
-        'views/product_view.xml',
-        # 'views/sale_view.xml',
-        # 'views/purchase_view.xml',
-        'views/account_ecotaxe_classification_view.xml',
-
-    ],
-    'installable': True,
+    "installable": True,
 }
