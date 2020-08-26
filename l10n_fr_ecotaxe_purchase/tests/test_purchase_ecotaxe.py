313bd511f1d1a0df27bc8d8308582d8e3667bb97
Mourad <mourad.elhadj.mimoune@akretion.com>
2020-08-26 16:00:35 +0200

    [FIX] add sale & purchase ecotaxe test + format code

--- l10n_fr_ecotaxe_purchase/tests/test_purchase_ecotaxe.py
+++ l10n_fr_ecotaxe_purchase/tests/test_purchase_ecotaxe.py
@@ -2,31 +2,19 @@
 # @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
+from odoo import fields
 from odoo.tests import SavepointCase
 
 
-class TestInvoiceEcotaxe(SavepointCase):
+class TestPurchaseEcotaxe(SavepointCase):
 
     @classmethod
     def setUpClass(cls):
-        super(TestInvoiceEcotaxe, cls).setUpClass()
+        super(TestPurchaseEcotaxe, cls).setUpClass()
         cls.partner = cls.env["res.partner"].create({"name": "Test"})
-        cls.account_type = cls.env["account.account.type"].create({
-            "name": "Test",
-            "type": "receivable",
-        })
-        cls.account = cls.env["account.account"].create({
-            "name": "Test account",
-            "code": "TEST",
-            "user_type_id": cls.account_type.id,
-            "reconcile": True,
-        })
-        cls.invoice = cls.env["account.invoice"].create({
-            "name": "Test Customer Invoice",
-            "journal_id": cls.env["account.journal"].search(
-                [("type", "=", "sale")])[0].id,
+        cls.purchase = cls.env["purchase.order"].create({
+            "name": "Test Customer purchase",
             "partner_id": cls.partner.id,
-            "account_id": cls.account.id,
         })
         cls.product = cls.env["product.template"].create({
             "name": "Product Test",
@@ -34,14 +22,15 @@ class TestInvoiceEcotaxe(SavepointCase):
             "weight": 100.00,
         })
 
-        cls.invoice_line = cls.env["account.invoice.line"]
-        cls.invoice_line1 = cls.invoice_line.create({
-            "invoice_id": cls.invoice.id,
+        cls.purchase_line = cls.env["purchase.order.line"]
+        cls.purchase_line1 = cls.purchase_line.create({
+            "order_id": cls.purchase.id,
             "name": "Line 1",
             "price_unit": 100,
             "product_id": cls.product.product_variant_ids[:1].id,
-            "account_id": cls.account.id,
-            "quantity": 1,
+            "product_qty": 1,
+            "date_planned": fields.Datetime.now(),
+            "product_uom": cls.product.uom_id.id,
         })
         cls.ecotaxe_classification = cls.env["account.ecotaxe.classification"]
         cls.ecotaxe_classification1 = cls.ecotaxe_classification.create({
@@ -63,6 +52,7 @@ class TestInvoiceEcotaxe(SavepointCase):
             "list_price": 2000.00,
             "weight": 400.00,
             "ecotaxe_classification_id": cls.ecotaxe_classification2.id,
+            "sale_line_warn": "no-message",
         })
 
     def test_01_manual_fixed_ecotaxe(self):
@@ -70,44 +60,41 @@ class TestInvoiceEcotaxe(SavepointCase):
         self.product.manual_fixed_ecotaxe = 1.5
         self.product._compute_ecotaxe()
         self.assertEqual(self.product.ecotaxe_amount, 1.5)
-        self.invoice_line1.quantity = 4
-        self.invoice._onchange_invoice_line_ids()
-        self.assertEqual(self.invoice_line1.unit_ecotaxe_amount, 1.5)
-        self.assertEqual(self.invoice_line1.subtotal_ecotaxe, 6.0)
-        self.assertEqual(self.invoice.amount_total, 400.0)
-        self.assertEqual(self.invoice.amount_ecotaxe, 6.0)
+        self.purchase_line1.product_qty = 4
+        self.assertEqual(self.purchase_line1.unit_ecotaxe_amount, 1.5)
+        self.assertEqual(self.purchase_line1.subtotal_ecotaxe, 6.0)
+        self.assertEqual(self.purchase.amount_total, 400.0)
+        self.assertEqual(self.purchase.amount_ecotaxe, 6.0)
 
     def test_02_classification_weight_based_ecotaxe(self):
         """ Tests multiple lines with weight based ecotaxes """
         self.product.ecotaxe_classification_id = self.ecotaxe_classification2
         self.product._compute_ecotaxe()
         self.assertEqual(self.product.ecotaxe_amount, 4)
-        self.invoice_line1.quantity = 3
-        self.invoice._onchange_invoice_line_ids()
-        self.assertEqual(self.invoice_line1.unit_ecotaxe_amount, 4)
-        self.assertEqual(self.invoice_line1.subtotal_ecotaxe, 12)
-        self.assertEqual(self.invoice.amount_total, 300.0)
-        self.assertEqual(self.invoice.amount_ecotaxe, 12)
+        self.purchase_line1.product_qty = 3
+        self.assertEqual(self.purchase_line1.unit_ecotaxe_amount, 4)
+        self.assertEqual(self.purchase_line1.subtotal_ecotaxe, 12)
+        self.assertEqual(self.purchase.amount_total, 300.0)
+        self.assertEqual(self.purchase.amount_ecotaxe, 12)
 
     def test_03_classification_ecotaxe(self):
         """ Tests multiple lines with mixed ecotaxes """
         self.product.ecotaxe_classification_id = self.ecotaxe_classification1
         self.product._compute_ecotaxe()
         self.assertEqual(self.product.ecotaxe_amount, 2.4)
-        self.invoice_line1.quantity = 3
-        self.invoice._onchange_invoice_line_ids()
-        self.invoice_line2 = self.invoice_line.create({
-            "invoice_id": self.invoice.id,
+        self.purchase_line1.product_qty = 3
+        self.purchase_line2 = self.purchase_line.create({
+            "order_id": self.purchase.id,
             "name": "Line 1",
             "price_unit": 2000,
             "product_id": self.product2.product_variant_ids[:1].id,
-            "account_id": self.account.id,
-            "quantity": 2,
+            "product_qty": 2,
+            "date_planned": fields.Datetime.now(),
+            "product_uom": self.product2.uom_id.id,
         })
-        self.invoice._onchange_invoice_line_ids()
-        self.assertEqual(self.invoice_line1.unit_ecotaxe_amount, 2.4)
-        self.assertEqual(self.invoice_line1.subtotal_ecotaxe, 7.2)
-        self.assertEqual(self.invoice_line2.unit_ecotaxe_amount, 16)
-        self.assertEqual(self.invoice_line2.subtotal_ecotaxe, 32)
-        self.assertEqual(self.invoice.amount_total, 4300.0)
-        self.assertEqual(self.invoice.amount_ecotaxe, 39.2)
+        self.assertEqual(self.purchase_line1.unit_ecotaxe_amount, 2.4)
+        self.assertEqual(self.purchase_line1.subtotal_ecotaxe, 7.2)
+        self.assertEqual(self.purchase_line2.unit_ecotaxe_amount, 16)
+        self.assertEqual(self.purchase_line2.subtotal_ecotaxe, 32)
+        self.assertEqual(self.purchase.amount_total, 4300.0)
+        self.assertEqual(self.purchase.amount_ecotaxe, 39.2)
--- l10n_fr_ecotaxe_sale/tests/test_sale_ecotaxe.py
+++ l10n_fr_ecotaxe_sale/tests/test_sale_ecotaxe.py
@@ -5,28 +5,15 @@
 from odoo.tests import SavepointCase
 
 
-class TestInvoiceEcotaxe(SavepointCase):
+class TestSaleEcotaxe(SavepointCase):
 
     @classmethod
     def setUpClass(cls):
-        super(TestInvoiceEcotaxe, cls).setUpClass()
+        super(TestSaleEcotaxe, cls).setUpClass()
         cls.partner = cls.env["res.partner"].create({"name": "Test"})
-        cls.account_type = cls.env["account.account.type"].create({
-            "name": "Test",
-            "type": "receivable",
-        })
-        cls.account = cls.env["account.account"].create({
-            "name": "Test account",
-            "code": "TEST",
-            "user_type_id": cls.account_type.id,
-            "reconcile": True,
-        })
-        cls.invoice = cls.env["account.invoice"].create({
-            "name": "Test Customer Invoice",
-            "journal_id": cls.env["account.journal"].search(
-                [("type", "=", "sale")])[0].id,
+        cls.sale = cls.env["sale.order"].create({
+            "name": "Test Customer sale",
             "partner_id": cls.partner.id,
-            "account_id": cls.account.id,
         })
         cls.product = cls.env["product.template"].create({
             "name": "Product Test",
@@ -34,14 +21,14 @@ class TestInvoiceEcotaxe(SavepointCase):
             "weight": 100.00,
         })
 
-        cls.invoice_line = cls.env["account.invoice.line"]
-        cls.invoice_line1 = cls.invoice_line.create({
-            "invoice_id": cls.invoice.id,
+        cls.sale_line = cls.env["sale.order.line"]
+        cls.sale_line1 = cls.sale_line.create({
+            "order_id": cls.sale.id,
             "name": "Line 1",
             "price_unit": 100,
             "product_id": cls.product.product_variant_ids[:1].id,
-            "account_id": cls.account.id,
-            "quantity": 1,
+            "product_uom_qty": 1,
+            'tax_id': [],
         })
         cls.ecotaxe_classification = cls.env["account.ecotaxe.classification"]
         cls.ecotaxe_classification1 = cls.ecotaxe_classification.create({
@@ -70,44 +57,41 @@ class TestInvoiceEcotaxe(SavepointCase):
         self.product.manual_fixed_ecotaxe = 1.5
         self.product._compute_ecotaxe()
         self.assertEqual(self.product.ecotaxe_amount, 1.5)
-        self.invoice_line1.quantity = 4
-        self.invoice._onchange_invoice_line_ids()
-        self.assertEqual(self.invoice_line1.unit_ecotaxe_amount, 1.5)
-        self.assertEqual(self.invoice_line1.subtotal_ecotaxe, 6.0)
-        self.assertEqual(self.invoice.amount_total, 400.0)
-        self.assertEqual(self.invoice.amount_ecotaxe, 6.0)
+        self.sale_line1.product_uom_qty = 4
+        self.assertEqual(self.sale_line1.unit_ecotaxe_amount, 1.5)
+        self.assertEqual(self.sale_line1.subtotal_ecotaxe, 6.0)
+        self.assertEqual(self.sale.amount_total, 400.0)
+        self.assertEqual(self.sale.amount_ecotaxe, 6.0)
 
     def test_02_classification_weight_based_ecotaxe(self):
         """ Tests multiple lines with weight based ecotaxes """
         self.product.ecotaxe_classification_id = self.ecotaxe_classification2
         self.product._compute_ecotaxe()
         self.assertEqual(self.product.ecotaxe_amount, 4)
-        self.invoice_line1.quantity = 3
-        self.invoice._onchange_invoice_line_ids()
-        self.assertEqual(self.invoice_line1.unit_ecotaxe_amount, 4)
-        self.assertEqual(self.invoice_line1.subtotal_ecotaxe, 12)
-        self.assertEqual(self.invoice.amount_total, 300.0)
-        self.assertEqual(self.invoice.amount_ecotaxe, 12)
+        self.sale_line1.product_uom_qty = 3
+        self.assertEqual(self.sale_line1.unit_ecotaxe_amount, 4)
+        self.assertEqual(self.sale_line1.subtotal_ecotaxe, 12)
+        self.assertEqual(self.sale.amount_untaxed, 300.0)
+        self.assertEqual(self.sale.amount_total, 300.0)
+        self.assertEqual(self.sale.amount_ecotaxe, 12)
 
     def test_03_classification_ecotaxe(self):
         """ Tests multiple lines with mixed ecotaxes """
         self.product.ecotaxe_classification_id = self.ecotaxe_classification1
         self.product._compute_ecotaxe()
         self.assertEqual(self.product.ecotaxe_amount, 2.4)
-        self.invoice_line1.quantity = 3
-        self.invoice._onchange_invoice_line_ids()
-        self.invoice_line2 = self.invoice_line.create({
-            "invoice_id": self.invoice.id,
+        self.sale_line1.product_uom_qty = 3
+        self.sale_line2 = self.sale_line.create({
+            "order_id": self.sale.id,
             "name": "Line 1",
             "price_unit": 2000,
             "product_id": self.product2.product_variant_ids[:1].id,
-            "account_id": self.account.id,
-            "quantity": 2,
+            "product_uom_qty": 2,
+            'tax_id': [],
         })
-        self.invoice._onchange_invoice_line_ids()
-        self.assertEqual(self.invoice_line1.unit_ecotaxe_amount, 2.4)
-        self.assertEqual(self.invoice_line1.subtotal_ecotaxe, 7.2)
-        self.assertEqual(self.invoice_line2.unit_ecotaxe_amount, 16)
-        self.assertEqual(self.invoice_line2.subtotal_ecotaxe, 32)
-        self.assertEqual(self.invoice.amount_total, 4300.0)
-        self.assertEqual(self.invoice.amount_ecotaxe, 39.2)
+        self.assertEqual(self.sale_line1.unit_ecotaxe_amount, 2.4)
+        self.assertEqual(self.sale_line1.subtotal_ecotaxe, 7.2)
+        self.assertEqual(self.sale_line2.unit_ecotaxe_amount, 16)
+        self.assertEqual(self.sale_line2.subtotal_ecotaxe, 32)
+        self.assertEqual(self.sale.amount_total, 4300.0)
+        self.assertEqual(self.sale.amount_ecotaxe, 39.2)
