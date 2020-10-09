5dd5f352c2ec6dd1496d66af4c1e6ec64f3a9973
Alexis de Lattre <alexis.delattre@akretion.com>
2020-10-10 00:17:35 +0200

    l10n_fr_hr_check_ssnid: black, isort and all that stuff

--- l10n_fr_hr_check_ssnid/__manifest__.py
+++ l10n_fr_hr_check_ssnid/__manifest__.py
@@ -3,15 +3,15 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'French Localization - Check Social Security Number',
-    'version': '14.0.1.0.0',
-    'category': 'Human Resources',
-    'license': 'AGPL-3',
-    'summary': 'Check validity of Social Security Numbers in French companies',
-    'author': 'Akretion,Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/l10n-france/',
-    'depends': ['hr'],
-    'external_dependencies': {'python': ['stdnum']},
-    'data': ['views/hr_employee.xml'],
-    'installable': True,
+    "name": "French Localization - Check Social Security Number",
+    "version": "14.0.1.0.0",
+    "category": "Human Resources",
+    "license": "AGPL-3",
+    "summary": "Check validity of Social Security Numbers in French companies",
+    "author": "Akretion,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-france/",
+    "depends": ["hr"],
+    "external_dependencies": {"python": ["stdnum"]},
+    "data": ["views/hr_employee.xml"],
+    "installable": True,
 }
