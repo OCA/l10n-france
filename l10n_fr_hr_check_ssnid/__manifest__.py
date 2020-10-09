3aae64c4bb7d0b10233bdc97d5bd26bf6325210c
Alexis de Lattre <alexis.delattre@akretion.com>
2020-10-10 00:15:59 +0200

    [MIG] l10n_fr_hr_check_ssnid from v10 to v14
    Convert README to new sub-dir format

--- l10n_fr_hr_check_ssnid/__manifest__.py
+++ l10n_fr_hr_check_ssnid/__manifest__.py
@@ -1,15 +1,15 @@
-# -*- coding: utf-8 -*-
-# Copyright 2018 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# Copyright 2018-2020 Akretion France (http://www.akretion.com/)
+# @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'French Localization - Check Social Security Number',
-    'version': '10.0.1.0.0',
+    'version': '14.0.1.0.0',
     'category': 'Human Resources',
     'license': 'AGPL-3',
     'summary': 'Check validity of Social Security Numbers in French companies',
     'author': 'Akretion,Odoo Community Association (OCA)',
-    'website': 'http://www.akretion.com',
+    'website': 'https://github.com/OCA/l10n-france/',
     'depends': ['hr'],
     'external_dependencies': {'python': ['stdnum']},
     'data': ['views/hr_employee.xml'],
---
+++ l10n_fr_hr_check_ssnid/readme/INSTALL.rst
@@ -0,0 +1,5 @@
+This module require the Python `stdnum <https://arthurdejong.org/python-stdnum/>`_ library. To install it, run:
+
+.. code::
+
+  pip3 install python-stdnum
