daaaded7787dc4cdfbed4966ca2f538ed064fdc6
Alexis de Lattre <alexis.delattre@akretion.com>
2020-10-09 23:50:27 +0200

    [MIG] l10n_fr_department* and l10n_fr_state to v14
    Fix bug #88 about cities whose postal code doesn't match their department

--- l10n_fr_state/__manifest__.py
+++ l10n_fr_state/__manifest__.py
@@ -1,23 +1,22 @@
-# -*- coding: utf-8 -*-
-# Copyright 2013-2018 GRAP (Sylvain LE GAL https://twitter.com/legalsylvain)
-# Copyright 2015-2018 Akretion France
+# Copyright 2013-2020 GRAP (Sylvain LE GAL https://twitter.com/legalsylvain)
+# Copyright 2015-2020 Akretion France
 # @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'French States (Régions)',
-    'summary': 'Populate Database with French States (Régions)',
-    'version': '13.0.1.0.0',
-    'category': 'French Localization',
-    'author': 'GRAP, '
-              'Akretion, '
-              'Nicolas JEUDY, '
-              'Odoo Community Association (OCA)',
-    'website': 'http://www.grap.coop',
-    'license': 'AGPL-3',
-    'depends': ['base'],
-    'pre_init_hook': 'create_fr_state_xmlid',
-    'data': ['data/res_country_state.xml'],
-    'images': ['static/src/img/screenshots/1.png'],
-    'installable': True,
+    "name": "French States (Régions)",
+    "summary": "Populate Database with French States (Régions)",
+    "version": "14.0.1.0.0",
+    "category": "French Localization",
+    "author": "GRAP, "
+    "Akretion, "
+    "Nicolas JEUDY, "
+    "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-france",
+    "license": "AGPL-3",
+    "depends": ["base"],
+    "pre_init_hook": "create_fr_state_xmlid",
+    "data": ["data/res_country_state.xml"],
+    "images": ["static/src/img/screenshots/1.png"],
+    "installable": True,
 }
