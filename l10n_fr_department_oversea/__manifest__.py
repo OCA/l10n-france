05c26d69032e178fa500e94bb2829c4956569c1c
Alexis de Lattre <alexis.delattre@akretion.com>
2020-10-09 23:50:27 +0200

    [MIG] l10n_fr_department* and l10n_fr_state to v14
    Fix bug #88 about cities whose postal code doesn't match their department

--- l10n_fr_department_oversea/__manifest__.py
+++ l10n_fr_department_oversea/__manifest__.py
@@ -1,21 +1,21 @@
-# © 2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# Copyright 2016-2020 Akretion France (http://www.akretion.com/)
+# @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'French Overseas Departments (DOM)',
-    'summary': 'Populate Database with overseas French '
-               "Departments (Départements d'outre-mer)",
-    'version': '13.0.1.0.0',
-    'category': 'French Localization',
-    'author': 'Akretion,Odoo Community Association (OCA)',
-    'website': 'http://www.akretion.com',
-    'license': 'AGPL-3',
-    'depends': ['l10n_fr_department'],
-    'data': [
-        'data/res_country_state.xml',
-        'data/res_country_department.xml',
+    "name": "French Overseas Departments (DOM)",
+    "summary": "Populate Database with overseas French "
+    "Departments (Départements d'outre-mer)",
+    "version": "14.0.1.0.0",
+    "category": "French Localization",
+    "author": "Akretion,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-france",
+    "license": "AGPL-3",
+    "depends": ["l10n_fr_department"],
+    "data": [
+        "data/res_country_state.xml",
+        "data/res_country_department.xml",
     ],
-    'post_init_hook': 'set_oversea_department_on_partner',
-    'images': [],
-    'installable': True,
+    "post_init_hook": "set_oversea_department_on_partner",
+    "installable": True,
 }
