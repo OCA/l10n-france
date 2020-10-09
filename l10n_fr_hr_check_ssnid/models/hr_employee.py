c12fe04cddd610ae59543fb85f9cc0e7bf6ad624
Alexis de Lattre <alexis.delattre@akretion.com>
2020-10-10 00:17:35 +0200

    l10n_fr_hr_check_ssnid: black, isort and all that stuff

--- l10n_fr_hr_check_ssnid/models/hr_employee.py
+++ l10n_fr_hr_check_ssnid/models/hr_employee.py
@@ -2,28 +2,30 @@
 # @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, models, _
-from odoo.exceptions import ValidationError
 import logging
+
+from odoo import _, api, models
+from odoo.exceptions import ValidationError
+
 logger = logging.getLogger(__name__)
 
 try:
-    from stdnum.fr.nir import validate, InvalidFormat,\
-        InvalidLength, InvalidChecksum
+    from stdnum.fr.nir import InvalidChecksum, InvalidFormat, InvalidLength, validate
 except ImportError:
-    logger.debug('Cannot import stdnum')
+    logger.debug("Cannot import stdnum")
 
 
 class HrEmployee(models.Model):
-    _inherit = 'hr.employee'
+    _inherit = "hr.employee"
 
-    @api.constrains('ssnid')
+    @api.constrains("ssnid")
     def france_ssnid_constrain(self):
         for empl in self:
-            if empl.company_id.country_id.code == 'FR' and empl.ssnid:
+            if empl.company_id.country_id.code == "FR" and empl.ssnid:
                 try:
                     validate(empl.ssnid)
                 except (InvalidFormat, InvalidLength, InvalidChecksum) as e:
-                    raise ValidationError(_(
-                        "The French Social Security Number '%s' is invalid. "
-                        "(%s)") % (empl.ssnid, e))
+                    raise ValidationError(
+                        _("The French Social Security Number '%s' is invalid. " "(%s)")
+                        % (empl.ssnid, e)
+                    )
