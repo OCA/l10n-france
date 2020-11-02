81bec02db9a7dd80a6a015db48415f2103ae59d2
Raphaël Valyi <rvalyi@akretion.com>
2020-11-02 06:02:19 +0000

    [IMP] l10n_fr_siret: black, isort, prettier

--- l10n_fr_siret/models/company.py
+++ l10n_fr_siret/models/company.py
@@ -1,24 +1,22 @@
-# -*- coding: utf-8 -*-
 # Copyright 2011 Numérigraphe SARL.
 # Copyright 2014-2018 Akretion France
 # @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields
+from odoo import fields, models
 
 
 class ResCompany(models.Model):
     """Replace the company's fields for SIRET/RC with the partner's"""
-    _inherit = 'res.company'
+
+    _inherit = "res.company"
 
     # siret field is defined in l10n_fr module on res.partner
     # with an unstored related field on res.company
     siret = fields.Char(store=True)
-    siren = fields.Char(
-        string='SIREN', related='partner_id.siren', store=True)
-    nic = fields.Char(
-        string='NIC', related='partner_id.nic', store=True)
+    siren = fields.Char(string="SIREN", related="partner_id.siren", store=True)
+    nic = fields.Char(string="NIC", related="partner_id.nic", store=True)
     # company_registry field is definied in base module on res.company
     company_registry = fields.Char(
-        string='Company Registry', related='partner_id.company_registry',
-        store=True)
+        string="Company Registry", related="partner_id.company_registry", store=True
+    )
