b6402b94259cc8cb2cab27f24e5875edb4c57ff7
Raphaël Valyi <rvalyi@akretion.com>
2020-11-02 06:02:19 +0000

    [IMP] l10n_fr_siret: black, isort, prettier

--- l10n_fr_siret/__manifest__.py
+++ l10n_fr_siret/__manifest__.py
@@ -4,16 +4,16 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'French company identity numbers SIRET/SIREN/NIC',
-    'version': '13.0.1.0.0',
-    "category": 'French Localization',
-    'author': 'Numérigraphe,Akretion,Odoo Community Association (OCA)',
-    'license': 'AGPL-3',
-    'depends': ['l10n_fr'],
-    'data': [
-        'views/partner.xml',
-        'views/company.xml',
+    "name": "French company identity numbers SIRET/SIREN/NIC",
+    "version": "13.0.1.0.0",
+    "category": "French Localization",
+    "author": "Numérigraphe,Akretion,Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "depends": ["l10n_fr"],
+    "data": [
+        "views/partner.xml",
+        "views/company.xml",
     ],
-    'demo': ['demo/partner_demo.xml'],
-    'installable': True,
+    "demo": ["demo/partner_demo.xml"],
+    "installable": True,
 }
