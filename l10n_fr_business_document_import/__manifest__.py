6bb4d0457557e15ee3739230954a5ee8135144a5
Florian da Costa <florian.dacosta@akretion.com>
2020-11-02 12:45:29 +0100

    Migrate l10n_fr_business_document_import to v12

--- l10n_fr_business_document_import/__manifest__.py
+++ l10n_fr_business_document_import/__manifest__.py
@@ -1,15 +1,14 @@
-# -*- coding: utf-8 -*-
 # © 2015-2017 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'L10n FR Business Document Import',
-    'version': '10.0.1.0.0',
+    'version': '12.0.1.0.0',
     'category': 'French Localization',
     'license': 'AGPL-3',
     'summary': 'Adapt the module base_business_document_import for France',
     'author': 'Akretion,Odoo Community Association (OCA)',
-    'website': 'http://www.akretion.com',
+    'website': 'https://github.com/OCA/l10n-france',
     'depends': ['l10n_fr_siret', 'base_business_document_import'],
     'installable': True,
     'auto_install': True,
