2755bde22bcf64f789af5512ae6cc084e6ec32e3
Kevin Khao <kevinkhao@gmail.com>
2020-11-30 18:36:23 +0100

    [IMP] Misc. feedback processing

---
+++ l10n_fr_hr_rup/models/hr_contract.py
@@ -0,0 +1,20 @@
+# Copyright (C) 2020 Akretion (http://www.akretion.com/)
+
+from odoo import models, fields, api
+
+
+class HrContract(models.Model):
+    _inherit = "hr.contract"
+    _order = "date_start,name"
+
+    pcs_id = fields.Many2one("hr.employee.pcs", "PCS")
+    employer_address_id = fields.Many2one("res.partner", "Addresse de l'employeur")
+    qualification = fields.Char("Qualification")
+    work_location = fields.Char("Localisation du bureau")
+
+    @api.onchange('employee_id')
+    def _onchange_employee_id(self):
+        result = super()._onchange_employee_id()
+        self.pcs_id = self.employee_id.pcs_id
+        self.qualification = self.employee_id.qualification
+        self.work_location = self.employee_id.work_location
---
+++ l10n_fr_hr_rup/readme/USAGE.rst
@@ -0,0 +1,1 @@
+Select contracts in list view and print the report to get a PDF.
