c51a89d2b9b573ed4ee07a9ce07ab1f813c18f05
Alexis de Lattre <alexis.delattre@akretion.com>
2020-12-01 18:42:41 +0100

    [MIG] l10n_fr_fec_oca to v14

--- l10n_fr_fec_oca/__manifest__.py
+++ l10n_fr_fec_oca/__manifest__.py
@@ -1,20 +1,23 @@
-# Copyright 2013-2019 Akretion France (http://www.akretion.com/)
-# Copyright 2016-2019 Odoo SA (https://www.odoo.com/fr_FR/)
+# Copyright 2013-2020 Akretion France (http://www.akretion.com/)
+# @author: Alexis de Lattre <alexis.delattre@akretion.com>
+# Copyright 2016-2020 Odoo SA (https://www.odoo.com/fr_FR/)
 # @author: Alexis de Lattre <alexis.delattre@akretion.com>
 
 {
     "name": "France - FEC",
     "category": "Accounting",
-    "version": "12.0.1.0.0",
+    "version": "14.0.1.0.0",
     "license": "LGPL-3",
     "summary": "Fichier d'Échange Informatisé (FEC) for France",
     "author": "Akretion,Odoo Community Association (OCA)",
+    "maintainers": ["alexis-via"],
     "website": "https://github.com/OCA/l10n-france",
     "depends": ["l10n_fr", "account", "date_range"],
     "external_dependencies": {
         "python": ["unicodecsv", "unidecode"],
-        },
+    },
     "data": [
+        "security/ir.model.access.csv",
         "wizard/account_fr_fec_oca_view.xml",
     ],
     "installable": True,
