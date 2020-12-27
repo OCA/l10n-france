c4796b0940ba2f6be0883eedc932f6f22577571e
Alexis de Lattre <alexis.delattre@akretion.com>
2020-12-27 01:08:21 +0100

    [MIG] account_banking_fr_lcr to v14.0

--- account_banking_fr_lcr/__manifest__.py
+++ account_banking_fr_lcr/__manifest__.py
@@ -6,7 +6,7 @@
 {
     "name": "French Letter of Change",
     "summary": "Create French LCR CFONB files",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "license": "AGPL-3",
     "author": "Akretion,Odoo Community Association (OCA)",
     "maintainers": ["alexis-via"],
