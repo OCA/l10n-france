011f981cc5923f7ec5ae2b598e7b63da8f9fe78a
Alexis de Lattre <alexis.delattre@akretion.com>
2021-01-05 23:40:26 +0100

    DES: use the new field intrastat_fiscal_position

--- l10n_fr_intrastat_service/__manifest__.py
+++ l10n_fr_intrastat_service/__manifest__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    "name": "France Intrastat Service (DES)",
+    "name": "DES",
     "version": "14.0.1.0.0",
     "category": "Localisation/Report Intrastat",
     "license": "AGPL-3",
