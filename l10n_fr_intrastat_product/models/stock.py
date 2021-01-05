ba7c12d9fd4ec042c4fc5c13a253d3718ccf43d5
Alexis de Lattre <alexis.delattre@akretion.com>
2021-01-06 00:41:53 +0100

    [MIG] l10n_fr_intrastat_product to v14

--- l10n_fr_intrastat_product/models/stock.py
+++ l10n_fr_intrastat_product/models/stock.py
@@ -1,4 +1,4 @@
-# Copyright 2010-2019 Akretion France (http://www.akretion.com)
+# Copyright 2010-2020 Akretion France (http://www.akretion.com)
 # @author Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
@@ -12,7 +12,7 @@ class StockWarehouse(models.Model):
     def get_fr_department(self):
         self.ensure_one()
         if not self.partner_id:
-            raise UserError(_("Missing partner on warehouse %s") % self.name)
+            raise UserError(_("Missing partner on warehouse '%s'.") % self.display_name)
         return self.partner_id.department_id
 
 
@@ -24,10 +24,9 @@ class StockLocation(models.Model):
         of intrastat_product because it doesn't return the same object.
         That's why there is a small code duplication in this method"""
         self.ensure_one()
-        locations = self.search([("id", "parent_of", self.id)])
-        warehouses = self.env["stock.warehouse"].search(
-            [("lot_stock_id", "in", locations.ids)]
+        warehouse = self.env["stock.warehouse"].search(
+            [("lot_stock_id", "parent_of", self.ids)], limit=1
         )
-        if warehouses:
-            return warehouses[0].get_fr_department()
+        if warehouse:
+            return warehouse.get_fr_department()
         return None
--- l10n_fr_intrastat_product/post_install.py
+++ l10n_fr_intrastat_product/post_install.py
@@ -1,38 +1,19 @@
-# Copyright 2017-2019 Akretion France
+# Copyright 2017-2020 Akretion France
 # @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 import logging
 
-from lxml import etree
-
 from odoo import SUPERUSER_ID, api
-from odoo.tools import file_open
 
 logger = logging.getLogger(__name__)
 
 
 def set_fr_company_intrastat(cr, registry):
-    """This post_install script is required because, when the module
-    is installed, Odoo creates the column in the DB and compute the field
-    and THEN it loads the file data/res_country_department_data.yml...
-    So, when it computes the field on module installation, the
-    departments are not available in the DB, so the department_id field
-    on res.partner stays null. This post_install script fixes this."""
-    f = file_open("l10n_fr_intrastat_product/data/account_tax_template.xml", "rb")
-    xml_root = etree.parse(f)
-    data = {}
-    for record in xml_root.xpath("//record"):
-        xmlid = record.attrib["id"]
-        data[xmlid] = {}
-        for xfield in record.xpath("field"):
-            xfield_dict = xfield.attrib
-            data[xmlid][xfield_dict["name"]] = xfield_dict.get("ref")
-    logger.debug("set_unece_on_taxes data=%s", data)
     with api.Environment.manage():
         env = api.Environment(cr, SUPERUSER_ID, {})
         imdo = env["ir.model.data"]
-        ato = env["account.tax"]
+        afpo = env["account.fiscal.position"]
         fr_id = env.ref("base.fr").id
         companies = env["res.company"].search([("partner_id.country_id", "=", fr_id)])
         out_inv_trans_id = env.ref(
@@ -53,26 +34,22 @@ def set_fr_company_intrastat(cr, registry):
                     "intrastat_accessory_costs": True,
                 }
             )
-            taxes = ato.search([("company_id", "=", company.id)])
-            for tax in taxes:
-                xmlid_obj = imdo.search(
+            fps = afpo.search([("company_id", "=", company.id)])
+            for fp in fps:
+                xmlid_rec = imdo.search(
                     [
-                        ("model", "=", "account.tax"),
+                        ("model", "=", "account.fiscal.position"),
                         ("module", "=", "l10n_fr"),
-                        ("res_id", "=", tax.id),
+                        ("res_id", "=", fp.id),
+                        ("name", "=like", "%_fiscal_position_template_intraeub2b"),
                     ],
                     limit=1,
                 )
-                if xmlid_obj and xmlid_obj.name and len(xmlid_obj.name.split("_")) > 1:
-                    # Remove the 'companyID_' prefix from XMLID of tax
-                    xmlid_ori_end = "_".join(xmlid_obj.name.split("_")[1:])
-                    xmlid_ori = "l10n_fr.%s" % xmlid_ori_end
-                    if data.get(xmlid_ori):
-                        logger.debug(
-                            "set_fr_company_intrastat writing "
-                            "exclude_from_intrastat_if_present=True on "
-                            "tax ID %d",
-                            tax.id,
-                        )
-                        tax.write({"exclude_from_intrastat_if_present": True})
+                if xmlid_rec:
+                    logger.debug(
+                        "set_fr_company_intrastat writing intrastat=True "
+                        "on fiscal position ID %d",
+                        fp.id,
+                    )
+                    fp.write({"intrastat": True})
     return
