62262bf0172e63e77b1ea98ca6fff3bf05b914f4
Alexis de Lattre <alexis.delattre@akretion.com>
2021-02-18 22:28:45 +0100

    Black, isort, etc on l10n_fr_cog and l10n_fr_das2

--- l10n_fr_das2/__manifest__.py
+++ l10n_fr_das2/__manifest__.py
@@ -3,28 +3,29 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'DAS2',
-    'version': '14.0.1.0.0',
-    'category': 'Invoicing Management',
-    'license': 'AGPL-3',
-    'summary': 'DAS2 (France)',
-    'author': 'Akretion,Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/l10n-france',
-    'depends': [
-        'l10n_fr_siret',
-        'l10n_fr_cog',
-        ],
-    'external_dependencies': {
-        'python': ['unidecode', 'python-stdnum'],
-        },
-    'data': [
-        'security/das2_security.xml',
-        'security/ir.model.access.csv',
-        'views/l10n_fr_das2.xml',
-        'views/res_partner.xml',
-        'views/res_config_settings.xml',
+    "name": "DAS2",
+    "version": "14.0.1.0.0",
+    "category": "Invoicing Management",
+    "license": "AGPL-3",
+    "summary": "DAS2 (France)",
+    "author": "Akretion,Odoo Community Association (OCA)",
+    "maintainers": ["alexis-via"],
+    "website": "https://github.com/OCA/l10n-france",
+    "depends": [
+        "l10n_fr_siret",
+        "l10n_fr_cog",
     ],
-    'demo': ['demo/demo.xml'],
-    'installable': True,
-    'application': True,
+    "external_dependencies": {
+        "python": ["unidecode", "python-stdnum"],
+    },
+    "data": [
+        "security/das2_security.xml",
+        "security/ir.model.access.csv",
+        "views/l10n_fr_das2.xml",
+        "views/res_partner.xml",
+        "views/res_config_settings.xml",
+    ],
+    "demo": ["demo/demo.xml"],
+    "installable": True,
+    "application": True,
 }
--- l10n_fr_das2/models/res_company.py
+++ l10n_fr_das2/models/res_company.py
@@ -6,13 +6,16 @@ from odoo import fields, models
 
 
 class ResCompany(models.Model):
-    _inherit = 'res.company'
+    _inherit = "res.company"
 
     fr_das2_partner_declare_threshold = fields.Integer(
-        string='DAS2 Partner Declaration Threshold',
-        default=1200)
+        string="DAS2 Partner Declaration Threshold", default=1200
+    )
 
-    _sql_constraints = [(
-        'fr_das2_partner_declare_threshold_positive',
-        'CHECK(fr_das2_partner_declare_threshold >= 0)',
-        'The DAS2 partner declaration threshold must be positive!')]
+    _sql_constraints = [
+        (
+            "fr_das2_partner_declare_threshold_positive",
+            "CHECK(fr_das2_partner_declare_threshold >= 0)",
+            "The DAS2 partner declaration threshold must be positive!",
+        )
+    ]
