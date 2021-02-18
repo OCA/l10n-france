64ae1b35dab7dba8fb928705805b1dfe237c9c31
Alexis de Lattre <alexis.delattre@akretion.com>
2021-02-18 22:23:34 +0100

    [MIG] l10n_fr_das2 and l10n_fr_cog to v14
    Add warning message at the top of DAS2 form (and not only in chatter)
    Use python-stdnum to validate SIRET

--- l10n_fr_cog/__manifest__.py
+++ l10n_fr_cog/__manifest__.py
@@ -1,11 +1,11 @@
-# Copyright 2020 Akretion France (http://www.akretion.com/)
+# Copyright 2020-2021 Akretion France (http://www.akretion.com/)
 # @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Code Officiel Géographique',
     'summary': 'Add Code Officiel Géographique (COG) on countries',
-    'version': '12.0.1.0.0',
+    'version': '14.0.1.0.0',
     'category': 'French Localization',
     'author': "Akretion,Odoo Community Association (OCA)",
     'website': 'https://github.com/OCA/l10n-france',
