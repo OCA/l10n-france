f439cc0fe5e11f0f2dfbf320e4183b3dc06c3cf5
Alexis de Lattre <alexis.delattre@akretion.com>
2021-02-18 22:23:34 +0100

    [MIG] l10n_fr_das2 and l10n_fr_cog to v14
    Add warning message at the top of DAS2 form (and not only in chatter)
    Use python-stdnum to validate SIRET

--- l10n_fr_das2/__manifest__.py
+++ l10n_fr_das2/__manifest__.py
@@ -1,10 +1,10 @@
-# Copyright 2020 Akretion France (http://www.akretion.com/)
+# Copyright 2020-2021 Akretion France (http://www.akretion.com/)
 # @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'DAS2',
-    'version': '12.0.1.0.1',
+    'version': '14.0.1.0.0',
     'category': 'Invoicing Management',
     'license': 'AGPL-3',
     'summary': 'DAS2 (France)',
@@ -15,14 +15,14 @@
         'l10n_fr_cog',
         ],
     'external_dependencies': {
-        'python': ['unidecode'],
+        'python': ['unidecode', 'python-stdnum'],
         },
     'data': [
         'security/das2_security.xml',
         'security/ir.model.access.csv',
         'views/l10n_fr_das2.xml',
-        'views/partner.xml',
-        'views/config_settings.xml',
+        'views/res_partner.xml',
+        'views/res_config_settings.xml',
     ],
     'demo': ['demo/demo.xml'],
     'installable': True,
