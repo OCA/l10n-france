bdfc7b50a431fb5b15d35268664305b4879c3f59
Alexis de Lattre <alexis.delattre@akretion.com>
2021-03-18 19:12:13 +0100

    l10n_fr_business_document_import: black, isort...

--- l10n_fr_business_document_import/tests/test_business_document_import.py
+++ l10n_fr_business_document_import/tests/test_business_document_import.py
@@ -6,29 +6,32 @@ from odoo.tests.common import TransactionCase
 
 
 class TestL10nFRBusinessDocumentImport(TransactionCase):
-
     def test_match_partner_siren_siret(self):
-        partner1 = self.env['res.partner'].create({
-            'name': u'France Telecom (siège)',
-            'supplier_rank': 1,
-            'is_company': True,
-            'siren': '380129866',
-            'nic': '46850',
-            })
-        partner2 = self.env['res.partner'].create({
-            'name': 'France Telecom Lyon',
-            'supplier_rank': 1,
-            'is_company': True,
-            'siren': '380129866',
-            'nic': '00014',
-            })
-        bdio = self.env['business.document.import']
-        partner_dict = {'siren': '380 129 866'}
+        partner1 = self.env["res.partner"].create(
+            {
+                "name": u"France Telecom (siège)",
+                "supplier_rank": 1,
+                "is_company": True,
+                "siren": "380129866",
+                "nic": "46850",
+            }
+        )
+        partner2 = self.env["res.partner"].create(
+            {
+                "name": "France Telecom Lyon",
+                "supplier_rank": 1,
+                "is_company": True,
+                "siren": "380129866",
+                "nic": "00014",
+            }
+        )
+        bdio = self.env["business.document.import"]
+        partner_dict = {"siren": "380 129 866"}
         res = bdio._match_partner(partner_dict, [])
         self.assertIn(res, [partner1, partner2])
-        partner_dict = {'siret': '380 129 866 46850'}
+        partner_dict = {"siret": "380 129 866 46850"}
         res = bdio._match_partner(partner_dict, [])
         self.assertEqual(res, partner1)
-        partner_dict = {'siret': '380 129 866 00022'}
+        partner_dict = {"siret": "380 129 866 00022"}
         res = bdio._match_partner(partner_dict, [])
         self.assertIn(res, [partner1, partner2])
