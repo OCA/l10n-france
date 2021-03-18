716ccd35c13e48e23eac0402cca515faafc40201
Alexis de Lattre <alexis.delattre@akretion.com>
2021-03-18 19:12:13 +0100

    l10n_fr_business_document_import: black, isort...

--- l10n_fr_business_document_import/__manifest__.py
+++ l10n_fr_business_document_import/__manifest__.py
@@ -3,15 +3,15 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'L10n FR Business Document Import',
-    'version': '14.0.1.0.0',
-    'category': 'French Localization',
-    'license': 'AGPL-3',
-    'summary': 'Adapt the module base_business_document_import for France',
-    'author': 'Akretion,Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/l10n-france',
-    'depends': ['l10n_fr_siret', 'base_business_document_import'],
-    'external_dependencies': {'python': ['python-stdnum']},
-    'installable': True,
-    'auto_install': True,
+    "name": "L10n FR Business Document Import",
+    "version": "14.0.1.0.0",
+    "category": "French Localization",
+    "license": "AGPL-3",
+    "summary": "Adapt the module base_business_document_import for France",
+    "author": "Akretion,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-france",
+    "depends": ["l10n_fr_siret", "base_business_document_import"],
+    "external_dependencies": {"python": ["python-stdnum"]},
+    "installable": True,
+    "auto_install": True,
 }
