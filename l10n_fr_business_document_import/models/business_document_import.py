1d44f0f38461bcd32269cccffffaedf1bc029651
Alexis de Lattre <alexis.delattre@akretion.com>
2021-03-18 19:09:53 +0100

    [MIG] l10n_fr_business_document_import to v14

--- l10n_fr_business_document_import/models/business_document_import.py
+++ l10n_fr_business_document_import/models/business_document_import.py
@@ -1,36 +1,26 @@
-# Copyright 2015-2018 Akretion France (http://www.akretion.com/)
+# Copyright 2015-2021 Akretion France (http://www.akretion.com/)
 # @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import api, models, _
+from stdnum.fr.siren import validate as siren_validate
+from stdnum.fr.siret import validate as siret_validate
 
 
 class BusinessDocumentImport(models.AbstractModel):
     _inherit = 'business.document.import'
 
     @api.model
-    def _match_partner(
-            self, partner_dict, chatter_msg, partner_type='supplier'):
-        company_id = self._context.get('force_company') or\
-            self.env.user.company_id.id
-        domain = [
-            '|', ('company_id', '=', False),
-            ('company_id', '=', company_id)]
-        if partner_type == 'supplier':
-            domain += [('supplier', '=', True)]
-        elif partner_type == 'customer':
-            domain += [('customer', '=', True)]
-
+    def _hook_match_partner(
+            self, partner_dict, chatter_msg, domain, order):
+        rpo = self.env['res.partner']
         if partner_dict.get('siret'):
             siret = partner_dict['siret'].replace(' ', '')
-            if len(siret) == 14 and siret.isdigit():
-                partners = self.env['res.partner'].search(
-                    domain + [
-                        ('parent_id', '=', False),
-                        ('siret', '=', siret),
-                        ])
-                if partners:
-                    return partners[0]
+            if siret_validate(siret):
+                partner = rpo.search(
+                    domain + [('siret', '=', siret)], order=order, limit=1)
+                if partner:
+                    return partner
                 # fallback on siren search
                 elif not partner_dict.get('siren'):
                     partner_dict['siren'] = siret[:9]
@@ -40,16 +30,24 @@ class BusinessDocumentImport(models.AbstractModel):
                 siren = str(partner_dict['siren'])
             else:
                 siren = partner_dict['siren'].replace(' ', '')
-            if len(siren) == 9 and siren.isdigit():
-                partners = self.env['res.partner'].search(
+            if siren_validate(siren):
+                partner = rpo.search(
                     domain + [
                         ('parent_id', '=', False),
                         ('siren', '=', siren),
-                        ])
-                if partners:
-                    return partners[0]
-        return super()._match_partner(
-            partner_dict, chatter_msg, partner_type=partner_type)
+                        ], limit=1, order=order)
+                if partner:
+                    return partner
+        return super()._hook_match_partner(
+            partner_dict, chatter_msg, domain, order)
+
+    @api.model
+    def user_error_wrap(self, method, data_dict, error_msg):
+        if method == "_match_partner" and error_msg and data_dict:
+            error_msg += 'SIREN: %s\nSIRET: %s\n' % (
+                data_dict.get('siren'),
+                data_dict.get('siret'))
+        return super().user_error_wrap(method, data_dict, error_msg)
 
     @api.model
     def _check_company(self, company_dict, chatter_msg):
@@ -59,25 +57,26 @@ class BusinessDocumentImport(models.AbstractModel):
         if self._context.get('force_company'):
             company = rco.browse(self._context['force_company'])
         else:
-            company = self.env.user.company_id
+            company = self.env.company
+        siren = False
         if company_dict.get('siret'):
-            parsed_company_siret = company_dict['siret'].replace(' ', '')
-            if (
-                    len(parsed_company_siret) == 14 and
-                    parsed_company_siret.isdigit()):
-                parsed_company_siren = parsed_company_siret[:9]
-                if company.siren:
-                    if company.siren != parsed_company_siren:
-                        raise self.user_error_wrap(_(
-                            "The SIREN of the customer written in the "
-                            "business document (%s) doesn't match the SIREN "
-                            "of the company '%s' (%s) in which you are "
-                            "trying to import this document.") % (
-                                parsed_company_siren, company.display_name,
-                                company.siren))
-                else:
-                    chatter_msg.append(_(
-                        "Missing SIRET on company '%s'")
-                        % company.display_name)
-        return super()._check_company(
-            company_dict, chatter_msg)
+            siret = company_dict['siret'].replace(' ', '')
+            siren = siret[:9]
+        if company_dict.get('siren'):
+            siren = company_dict['siren'].replace(' ', '')
+        if siren and siren_validate(siren):
+            if company.siren:
+                if company.siren != siren:
+                    raise self.user_error_wrap(
+                        "_check_company",
+                        company_dict,
+                        _("The SIREN of the customer written in the "
+                        "business document (%s) doesn't match the SIREN "
+                        "of the company '%s' (%s) in which you are "
+                        "trying to import this document.") % (
+                            siren, company.display_name, company.siren))
+            else:
+                chatter_msg.append(_(
+                    "Missing SIRET on company '%s'")
+                    % company.display_name)
+        return super()._check_company(company_dict, chatter_msg)
