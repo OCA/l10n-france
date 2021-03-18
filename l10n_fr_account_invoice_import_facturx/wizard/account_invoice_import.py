10ec326ac6fd3831eb60bbdad870bfaea38cc98f
Alexis de Lattre <alexis.delattre@akretion.com>
2021-03-18 19:40:05 +0100

    l10n_fr_account_invoice_import_facturx: black, isort

--- l10n_fr_account_invoice_import_facturx/wizard/account_invoice_import.py
+++ l10n_fr_account_invoice_import_facturx/wizard/account_invoice_import.py
@@ -6,29 +6,31 @@ from odoo import models
 
 
 class AccountInvoiceImport(models.TransientModel):
-    _inherit = 'account.invoice.import'
+    _inherit = "account.invoice.import"
 
     def prepare_facturx_xpath_dict(self):
         xpathd = super().prepare_facturx_xpath_dict()
-        xpathd['partner']['siret'] = [
+        xpathd["partner"]["siret"] = [
             "//ram:ApplicableHeaderTradeAgreement"
             "/ram:SellerTradeParty"
             "/ram:SpecifiedLegalOrganization"
-            "/ram:ID[@schemeID='0002']"]
-        xpathd['company']['siret'] = [
+            "/ram:ID[@schemeID='0002']"
+        ]
+        xpathd["company"]["siret"] = [
             "//ram:ApplicableHeaderTradeAgreement"
             "/ram:BuyerTradeParty"
             "/ram:SpecifiedLegalOrganization"
-            "/ram:ID[@schemeID='0002']"]
+            "/ram:ID[@schemeID='0002']"
+        ]
         return xpathd
 
     # If one day we have a module l10n_fr_account_invoice_import
     # we could move the inherit of _prepare_new_partner_context() there
     def _prepare_new_partner_context(self, parsed_inv):
         context = super()._prepare_new_partner_context(parsed_inv)
-        if parsed_inv['partner'].get('siren'):
-            context['default_siren'] = parsed_inv['partner']['siren']
-        elif parsed_inv['partner'].get('siret'):
-            context['default_siren'] = parsed_inv['partner']['siret'][:9]
-            context['default_nic'] = parsed_inv['partner']['siret'][9:14]
+        if parsed_inv["partner"].get("siren"):
+            context["default_siren"] = parsed_inv["partner"]["siren"]
+        elif parsed_inv["partner"].get("siret"):
+            context["default_siren"] = parsed_inv["partner"]["siret"][:9]
+            context["default_nic"] = parsed_inv["partner"]["siret"][9:14]
         return context
