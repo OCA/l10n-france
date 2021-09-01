415dbd6bdffad22cc28744031ae44ebcd8014d28
Alexis de Lattre <alexis.delattre@akretion.com>
2021-09-01 22:51:51 +0200

    Restore tests and remove Alpha status
    Unfuck the "Donald Trump" pull request: this is the real/serious work made
    by the anti-Donald Trump squad.

--- account_banking_fr_lcr/__manifest__.py
+++ account_banking_fr_lcr/__manifest__.py
@@ -10,7 +10,6 @@
     "license": "AGPL-3",
     "author": "Akretion,Odoo Community Association (OCA)",
     "maintainers": ["alexis-via"],
-    "development_status": "Alpha",
     "website": "https://github.com/OCA/l10n-france",
     "category": "French localisation",
     "depends": ["account_payment_order"],
