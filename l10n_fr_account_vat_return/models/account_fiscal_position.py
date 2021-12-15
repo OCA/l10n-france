19c43e347affa442d426c72e01c550a9e587c4c3
Alexis de Lattre <alexis.delattre@akretion.com>
2021-12-15 23:17:16 +0100

    l10n_fr_account_vat_return: add support for account.fiscal.position.template

--- l10n_fr_account_vat_return/models/account_fiscal_position.py
+++ l10n_fr_account_vat_return/models/account_fiscal_position.py
@@ -18,3 +18,26 @@ class AccountFiscalPosition(models.Model):
         string="Type",
         help="This field is used by the French VAT return module",
     )
+
+
+class AccountFiscalPositionTemplate(models.Model):
+    _inherit = "account.fiscal.position.template"
+
+    fr_vat_type = fields.Selection(
+        [
+            ("france", "France"),
+            ("intracom_b2b", "Intra-EU B2B"),
+            ("intracom_b2c", "Intra-EU B2C over 10k€ limit"),
+            ("extracom", "Extra-EU"),
+        ],
+        string="Type",
+    )
+
+
+class AccountChartTemplate(models.Model):
+    _inherit = "account.chart.template"
+
+    def _get_fp_vals(self, company, position):
+        vals = super()._get_fp_vals(company, position)
+        vals["fr_vat_type"] = position.fr_vat_type
+        return vals
