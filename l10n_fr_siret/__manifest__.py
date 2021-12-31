e1f6f87fe21ec5a2bb03dc06a7751a8710b20778
Alexis de Lattre <alexis.delattre@akretion.com>
2021-12-31 16:20:28 +0100

    [MIG] l10n_fr_siret from v14 to v15

--- l10n_fr_siret/__manifest__.py
+++ l10n_fr_siret/__manifest__.py
@@ -1,11 +1,11 @@
-# Copyright 2011-2020 Numérigraphe SARL.
-# Copyright 2014-2020 Akretion France
+# Copyright 2011-2021 Numérigraphe SARL.
+# Copyright 2014-2021 Akretion France
 # @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "French company identity numbers SIRET/SIREN/NIC",
-    "version": "14.0.1.3.1",
+    "version": "15.0.1.0.0",
     "category": "French Localization",
     "author": "Numérigraphe,Akretion,Odoo Community Association (OCA)",
     "maintainers": ["alexis-via"],
