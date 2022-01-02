e40741c141ef79ec1ca6e2988b185824b67a5cd5
Alexis de Lattre <alexis.delattre@akretion.com>
2022-01-02 23:09:01 +0100

    [MIG] l10n_fr_department and l10n_fr_department_oversea from v14 to v15

--- l10n_fr_department/model/res_partner.py
+++ l10n_fr_department/model/res_partner.py
@@ -1,4 +1,4 @@
-# Copyright 2014-2020 Akretion France (http://www.akretion.com/)
+# Copyright 2014-2022 Akretion France (http://www.akretion.com/)
 # author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
