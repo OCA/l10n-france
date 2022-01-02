84151731e03b07c652a9f977af7be36da02cb59e
Alexis de Lattre <alexis.delattre@akretion.com>
2022-01-02 23:09:01 +0100

    [MIG] l10n_fr_department and l10n_fr_department_oversea from v14 to v15

--- l10n_fr_department/post_install.py
+++ l10n_fr_department/post_install.py
@@ -1,4 +1,4 @@
-# Copyright 2014-2020 Akretion France (http://www.akretion.com/)
+# Copyright 2014-2021 Akretion France (http://www.akretion.com/)
 # @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
@@ -12,15 +12,14 @@ def set_department_on_partner(cr, registry):
     So, when it computes the field on module installation, the
     departments are not available in the DB, so the department_id field
     on res.partner stays null. This post_install script fixes this."""
-    with api.Environment.manage():
-        env = api.Environment(cr, SUPERUSER_ID, {})
-        fr_countries = env["res.country"].search(
-            [("code", "in", ("FR", "GP", "MQ", "GF", "RE", "YT"))]
-        )
-        partners = (
-            env["res.partner"]
-            .with_context(active_test=False)
-            .search([("country_id", "in", fr_countries.ids)])
-        )
-        partners._compute_department()
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    fr_countries = env["res.country"].search(
+        [("code", "in", ("FR", "GP", "MQ", "GF", "RE", "YT"))]
+    )
+    partners = (
+        env["res.partner"]
+        .with_context(active_test=False)
+        .search([("country_id", "in", fr_countries.ids)])
+    )
+    partners._compute_department()
     return
