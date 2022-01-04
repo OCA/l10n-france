39943806a35e23349c3d401a4cd1931b423016a4
SilvioC2C <silvio.gregorini@camptocamp.com>
2022-01-04 15:54:16 +0100

    [15.0][ADD] l10n_fr_ecotaxe_product: manage ecotax on products
    Allows managing ecotax functionalities directly on products instead of templates.
    This way, a variant can have an ecotax configuration that is different from its template's configuration.

---
+++ l10n_fr_ecotaxe_product/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2021 Camptocamp
+#   @author Silvio Gregorini <silvio.gregorini@camptocamp.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "France Custom Ecotaxe - Manage on Products",
+    "summary": "Move Ecotax management from templates to products",
+    "version": "15.0.1.0.0",
+    "author": "Camptocamp, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-france",
+    "category": "Localization/Account Taxes",
+    "license": "AGPL-3",
+    "depends": ["l10n_fr_ecotaxe"],
+    "installable": True,
+}
---
+++ l10n_fr_ecotaxe_product/tests/__init__.py
@@ -0,0 +1,6 @@
+# Copyright 2021 Camptocamp
+#   @author Silvio Gregorini <silvio.gregorini@camptocamp.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from . import test_invoice_ecotaxe
+from . import test_product_ecotaxe
