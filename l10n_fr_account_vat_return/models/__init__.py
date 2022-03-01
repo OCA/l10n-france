111159feef38401cb3725d44c04bf3f306c3f4d3
Alexis de Lattre <alexis.delattre@akretion.com>
2022-03-01 16:48:59 +0100

    l10n_fr_account_vat_return: disable native VAT on payment reconcile mess

--- l10n_fr_account_vat_return/models/__init__.py
+++ l10n_fr_account_vat_return/models/__init__.py
@@ -6,3 +6,4 @@ from . import account_fiscal_position
 from . import l10n_fr_account_vat_box
 from . import l10n_fr_account_vat_return
 from . import account_move
+from . import account_partial_reconcile
