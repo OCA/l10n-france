24700107d9078c9fdf36c02466a260188ae0f0d1
Alexis de Lattre <alexis.delattre@akretion.com>
2022-06-15 00:29:30 +0200

    Replace boolean for supplier VAT on payment on res.partner and account.move by a new type of fiscal position
    Migration scripts provided

--- l10n_fr_account_vat_return/models/account_fiscal_position.py
+++ l10n_fr_account_vat_return/models/account_fiscal_position.py
@@ -2,37 +2,42 @@
 # @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class AccountFiscalPosition(models.Model):
     _inherit = "account.fiscal.position"
 
     fr_vat_type = fields.Selection(
-        [
+        "_get_fr_vat_type_sel",
+        string="Type",
+        help="This field is used by the French VAT return module",
+    )
+
+    @api.model
+    def _get_fr_vat_type_sel(self):
+        sel = [
             ("france", "France"),
+            ("france_vendor_vat_on_payment", "France Vendor VAT on Payment"),
             ("intracom_b2b", "Intra-EU B2B"),
             ("intracom_b2c", "Intra-EU B2C over 10k€ limit"),
             ("extracom", "Extra-EU"),
             ("france_exo", "France Exonerated"),
-        ],
-        string="Type",
-        help="This field is used by the French VAT return module",
-    )
+        ]
+        return sel
 
 
 class AccountFiscalPositionTemplate(models.Model):
     _inherit = "account.fiscal.position.template"
 
     fr_vat_type = fields.Selection(
-        [
-            ("france", "France"),
-            ("intracom_b2b", "Intra-EU B2B"),
-            ("intracom_b2c", "Intra-EU B2C over 10k€ limit"),
-            ("extracom", "Extra-EU"),
-        ],
+        "_get_fr_vat_type_sel",
         string="Type",
     )
+
+    @api.model
+    def _get_fr_vat_type_sel(self):
+        return self.env["account.fiscal.position"]._get_fr_vat_type_sel()
 
 
 class AccountChartTemplate(models.Model):
