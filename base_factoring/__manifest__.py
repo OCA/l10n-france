cda54e5681ad5336e847f17a4b46c8001f52a360
David Beal <david.beal@akretion.com>
2022-07-01 11:28:30 +0200

    [ADD] 15.0 module base_factoring

---
+++ base_factoring/__manifest__.py
@@ -0,0 +1,24 @@
+# © 2022 David BEAL @ Akretion
+# © 2022 Alexis DE LATTRE @ Akretion
+
+{
+    "name": "Base Factoring",
+    "version": "15.0.1.0.0",
+    "category": "Accounting",
+    "license": "AGPL-3",
+    "maintainers": [
+        "bealdav",
+        "alexis-via",
+    ],
+    "depends": [
+        "account",
+    ],
+    "data": [
+        "views/account.xml",
+        "views/partner.xml",
+        "views/company.xml",
+        "views/config.xml",
+    ],
+    "demo": [
+    ],
+}
