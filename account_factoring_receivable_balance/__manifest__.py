c045e142247802de615a64044ae739dc72642f92
David Beal <david.beal@akretion.com>
2022-07-12 16:50:52 +0200

    FIX acc_facto_balance: remove subrog_id from domain

--- account_factoring_receivable_balance/__manifest__.py
+++ account_factoring_receivable_balance/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Account Factoring Receivable Balance",
-    "version": "15.0.1.0.0",
+    "version": "15.0.1.1.0",
     "category": "Accounting",
     "license": "AGPL-3",
     "maintainers": [
