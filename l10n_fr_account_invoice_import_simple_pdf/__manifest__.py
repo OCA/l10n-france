387a5636e7d28a608b5c9520996dc93e58946c7e
Alexis de Lattre <alexis.delattre@akretion.com>
2022-10-08 23:57:39 +0200

    Add module l10n_fr_account_invoice_import_simple_pdf

---
+++ l10n_fr_account_invoice_import_simple_pdf/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2022 Akretion France (http://www.akretion.com/)
+# @author: Alexis de Lattre <alexis.delattre@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Account Invoice Import Simple PDF - France",
+    "version": "14.0.1.0.0",
+    "category": "Accounting",
+    "license": "AGPL-3",
+    "summary": "Invoice import simple PDF: match partners using SIREN",
+    "author": "Akretion,Odoo Community Association (OCA)",
+    "maintainers": ["alexis-via"],
+    "website": "https://github.com/OCA/l10n-france",
+    "depends": ["l10n_fr_siret", "account_invoice_import_simple_pdf"],
+    "installable": True,
+    "auto_install": True,
+}
