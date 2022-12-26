3950bfca073d0eda20b05ebf4ddf7864863c570c
Alexis de Lattre <alexis.delattre@akretion.com>
2022-12-26 17:36:27 +0100

    [MIG] l10n_fr_state to v16
    Re-write pre-init hook to mutualize the code with l10n_fr_department_oversea and avoid hard-coding XMLIDs in the code

--- l10n_fr_state/pre_init_hook.py
+++ l10n_fr_state/pre_init_hook.py
@@ -1,50 +1,62 @@
-# Copyright 2017-2021 Akretion France
+# Copyright 2017-2022 Akretion France
 # @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import SUPERUSER_ID, api
+import logging
 
-fr_states = {
-    44: "res_country_state_alsace",
-    75: "res_country_state_aquitaine",
-    84: "res_country_state_auvergne",
-    27: "res_country_state_bourgogne",
-    53: "res_country_state_bretagne",
-    24: "res_country_state_centre",
-    94: "res_country_state_corse",
-    11: "res_country_state_iledefrance",
-    76: "res_country_state_languedocroussillon",
-    32: "res_country_state_nordpasdecalais",
-    28: "res_country_state_bassenormandie",
-    52: "res_country_state_paysdelaloire",
-    93: "res_country_state_provencealpescotedazur",
-}
+from lxml import etree
+
+from odoo import SUPERUSER_ID, api
+from odoo.tools import file_open
+
+logger = logging.getLogger(__name__)
 
 # This code is designed to avoid a problem in the following scenario:
 # On a new database, the administrator executes these steps in this order:
-# 1) installs base_location_geonames_import and run the geonames import wizard
+# 1) installs base_address_extended_geonames_import and run the geonames import wizard
 #  for France
 # -> it creates all the French regions (without xmlid)
-# 2) installs l10n_fr_state (directly or when installing the module
-#    for DEB l10n_fr_intrastat_product)
+# 2) installs l10n_fr_state
 # -> it tries to create the French regions, but it fails due to the unicity
 #  constraint unique(country_id, code) of res.country.state.
 
 
 def create_fr_state_xmlid(cr):
+    generic_create_state_xmlid(cr, "l10n_fr_state", "data/res_country_state.xml")
+
+
+def generic_create_state_xmlid(cr, module_name, data_file):
+    """This method is also used by l10n_fr_state and l10n_fr_department_oversea"""
+    f = file_open("%s/%s" % (module_name, data_file), "rb")
+    xml_root = etree.parse(f)
+    data = {}  # key = xmlid, value = {"code": "GP", "country_id": "base.gp"}
+    for record in xml_root.xpath("//record"):
+        xmlid = record.attrib["id"]
+        data[xmlid] = {}
+        for xfield in record.xpath("field"):
+            xfield_dict = xfield.attrib
+            data[xmlid][xfield_dict["name"]] = xfield_dict.get("ref") or xfield.text
+    logger.debug("generic_create_state_xmlid data=%s", data)
     env = api.Environment(cr, SUPERUSER_ID, {})
-    fr_country = env.ref("base.fr")
-    for region_code, xmlid in fr_states.items():
-        regions = env["res.country.state"].search(
-            [("code", "=", region_code), ("country_id", "=", fr_country.id)]
+    for xmlid, state_data in data.items():
+        country_id = env.ref(state_data["country_id"]).id
+        state = env["res.country.state"].search(
+            [("code", "=", state_data["code"]), ("country_id", "=", country_id)],
+            limit=1,
         )
-        if regions:
+        if state:
             env["ir.model.data"].create(
                 {
                     "name": xmlid,
-                    "res_id": regions[0].id,
-                    "module": "l10n_fr_state",
+                    "res_id": state.id,
+                    "module": module_name,
                     "model": "res.country.state",
                 }
             )
-    return
+            logger.info(
+                "XMLID %s.%s created for state %s code %s",
+                module_name,
+                xmlid,
+                state.name,
+                state.code,
+            )
