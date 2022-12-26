0d72c2cc79966f89097974f95b235da2db7f0188
Alexis de Lattre <alexis.delattre@akretion.com>
2022-12-26 20:56:39 +0100

    [MIG] l10n_fr_account_tax_unece to v16

--- l10n_fr_account_tax_unece/__manifest__.py
+++ l10n_fr_account_tax_unece/__manifest__.py
@@ -1,11 +1,11 @@
-# Copyright 2017-2021 Akretion France (http://www.akretion.com/)
+# Copyright 2017-2022 Akretion France (http://www.akretion.com/)
 # @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "L10n FR Account Tax UNECE",
     "summary": "Auto-configure UNECE params on French taxes",
-    "version": "15.0.1.1.0",
+    "version": "16.0.1.0.0",
     "category": "French Localization",
     "author": "Akretion,Odoo Community Association (OCA)",
     "maintainers": ["alexis-via"],
