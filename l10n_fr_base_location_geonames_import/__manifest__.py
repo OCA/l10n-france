5b6f1eefdd610d71ee4535c5a45f67af788cb376
Laurent Bélorgey <lb@lalieutenante.com>
2022-12-31 16:56:58 +0100

    [IMP] l10n_fr_base_location_geonames_import: black, isort, prettier

--- l10n_fr_base_location_geonames_import/__manifest__.py
+++ l10n_fr_base_location_geonames_import/__manifest__.py
@@ -1,17 +1,16 @@
-# -*- coding: utf-8 -*-
 # © 2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'French Localization for Base Location Geonames Import',
-    'version': '10.0.1.0.0',
-    'category': 'Extra Tools',
-    'license': 'AGPL-3',
-    'summary': 'France-specific tuning for import of better zip entries '
-    'from Geonames',
-    'author': "Akretion,Odoo Community Association (OCA)",
-    'website': 'http://www.akretion.com',
-    'depends': ['base_location_geonames_import'],
-    'external_dependencies': {'python': ['unidecode']},
-    'installable': True,
+    "name": "French Localization for Base Location Geonames Import",
+    "version": "10.0.1.0.0",
+    "category": "Extra Tools",
+    "license": "AGPL-3",
+    "summary": "France-specific tuning for import of better zip entries "
+    "from Geonames",
+    "author": "Akretion,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-france",
+    "depends": ["base_location_geonames_import"],
+    "external_dependencies": {"python": ["unidecode"]},
+    "installable": True,
 }
