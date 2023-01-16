9730c0dec9c2d8e820422cd9db3ec0aac5f95337
Alexis de Lattre <alexis.delattre@akretion.com>
2023-01-16 23:27:52 +0100

    [MIG] Chorus Pro connector to v16
    Factorize code between sale.order and invoice validation
    Add timeout in requests
    Forward-port improvements on payment mode check from v14

--- l10n_fr_account_invoice_facturx/__manifest__.py
+++ l10n_fr_account_invoice_facturx/__manifest__.py
@@ -1,10 +1,10 @@
-# Copyright 2017-2021 Akretion France
+# Copyright 2017-2022 Akretion France (http://www.akretion.com/)
 # @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "Factur-X Invoices for France",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "Localisation",
     "license": "AGPL-3",
     "summary": "France-specific module to generate Factur-X invoices",
