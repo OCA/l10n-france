4325dcb11fe9ba82bf116609e61de304016a2404
Alexis de Lattre <alexis.delattre@akretion.com>
2023-01-30 23:56:52 +0100

    [MIG] l10n_fr_intrastat_service to v16

--- l10n_fr_intrastat_service/__manifest__.py
+++ l10n_fr_intrastat_service/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "DES",
-    "version": "14.0.1.1.0",
+    "version": "16.0.1.0.0",
     "category": "Localisation/Report Intrastat",
     "license": "AGPL-3",
     "summary": "Module for Intrastat service reporting (DES) for France",
