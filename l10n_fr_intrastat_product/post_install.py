7efbb976239cea1801114813ab67dc7ffb03b1f2
Alexis de Lattre <alexis.delattre@akretion.com>
2023-02-09 00:47:43 +0100

    [MIG] l10n_fr_intrastat_product to v16
    This migration started from the v14 branch akretion/14-emebi-simplify
    It includes the changes discussed in
    https://github.com/OCA/intrastat-extrastat/issues/194 in particular the
    fact that intrastat transaction are now provided by the generic module
    intrastat_product.
    This change involve one big change:
    BEFORE: code regime = transaction_id.code ; nature de transaction =
    transaction_id.fr_transaction_code
    AFTER: code regime = fr_regime_id.code ; nature de transaction =
    transaction_id.code
    
    Adapt to changes in intrastat_base with the 'intrastat' field of fiscal
    position converted from boolean to selection with 3 possible values
    b2b/b2c/no

--- l10n_fr_intrastat_product/post_install.py
+++ l10n_fr_intrastat_product/post_install.py
@@ -10,59 +10,40 @@ logger = logging.getLogger(__name__)
 
 
 def set_fr_company_intrastat(cr, registry):
-    with api.Environment.manage():
-        env = api.Environment(cr, SUPERUSER_ID, {})
-        imdo = env["ir.model.data"]
-        afpo = env["account.fiscal.position"]
-        fr_id = env.ref("base.fr").id
-        companies = env["res.company"].search([("partner_id.country_id", "=", fr_id)])
-        out_inv_b2b_trans_id = env.ref(
-            "l10n_fr_intrastat_product.intrastat_transaction_21_11"
-        ).id
-        out_inv_b2c_trans_id = env.ref(
-            "l10n_fr_intrastat_product.intrastat_transaction_29_12"
-        ).id
-        out_ref_trans_id = env.ref(
-            "l10n_fr_intrastat_product.intrastat_transaction_25"
-        ).id
-        in_inv_trans_id = env.ref(
-            "l10n_fr_intrastat_product.intrastat_transaction_11_11"
-        ).id
-        fpdict = {
-            "intraeub2b": False,
-            "intraeub2c": out_inv_b2c_trans_id,
-        }
-        for company in companies:
-            company.write(
-                {
-                    "intrastat_accessory_costs": True,
-                    "intrastat_out_invoice_transaction_id": out_inv_b2b_trans_id,
-                    "intrastat_out_refund_transaction_id": out_ref_trans_id,
-                    "intrastat_in_invoice_transaction_id": in_inv_trans_id,
-                }
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    imdo = env["ir.model.data"]
+    afpo = env["account.fiscal.position"]
+    fr_id = env.ref("base.fr").id
+    companies = env["res.company"].search([("partner_id.country_id", "=", fr_id)])
+    fpdict = {
+        "intraeub2b": "b2b",
+        "intraeub2c": "b2c",
+    }
+    for company in companies:
+        company.write(
+            {
+                "intrastat_accessory_costs": True,
+            }
+        )
+        fps = afpo.search([("company_id", "=", company.id)])
+        for fp in fps:
+            xmlid_rec = imdo.search(
+                [
+                    ("model", "=", "account.fiscal.position"),
+                    ("module", "=like", "l10n_fr%"),
+                    ("res_id", "=", fp.id),
+                ],
+                limit=1,
             )
-            fps = afpo.search([("company_id", "=", company.id)])
-            for fp in fps:
-                xmlid_rec = imdo.search(
-                    [
-                        ("model", "=", "account.fiscal.position"),
-                        ("module", "=like", "l10n_fr%"),
-                        ("res_id", "=", fp.id),
-                    ],
-                    limit=1,
-                )
-                if xmlid_rec:
-                    for fp_type, out_inv_trans_id in fpdict.items():
-                        if xmlid_rec.name.endswith(fp_type):
-                            logger.debug(
-                                "set_fr_company_intrastat writing intrastat=True "
-                                "on fiscal position ID %d",
-                                fp.id,
-                            )
-                            vals = {"intrastat": True}
-                            if out_inv_trans_id:
-                                vals[
-                                    "intrastat_out_invoice_transaction_id"
-                                ] = out_inv_trans_id
-                            fp.write(vals)
-                            break
+            if xmlid_rec:
+                for fp_type, intrastat in fpdict.items():
+                    if xmlid_rec.name.endswith(fp_type):
+                        logger.debug(
+                            "set_fr_company_intrastat writing intrastat=%s "
+                            "on fiscal position ID %d",
+                            intrastat,
+                            fp.id,
+                        )
+                        vals = {"intrastat": intrastat}
+                        fp.write(vals)
+                        break
