7b0dbcf39cd224c1f7550cba6bc9c92452c8db4b
Alexis de Lattre <alexis.delattre@akretion.com>
2023-03-25 00:16:12 +0100

    [MIG] l10n_fr_account_invoice_import_facturx, l10n_fr_account_invoice_import_simple_pdf and l10n_fr_business_document_import from v14 to v16

--- l10n_fr_account_invoice_import_simple_pdf/__manifest__.py
+++ l10n_fr_account_invoice_import_simple_pdf/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Account Invoice Import Simple PDF - France",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "Accounting",
     "license": "AGPL-3",
     "summary": "Invoice import simple PDF: match partners using SIREN",
