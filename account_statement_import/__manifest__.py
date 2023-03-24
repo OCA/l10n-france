0555f4a28440001b45ca1fb355924a33a555b386
AG-AbdellahJ <abdellah.jrondi@auguria.fr>
2023-03-24 15:13:20 +0100

    [16][MIG] account_statement_import_fr_cfonb and account_statement_import

---
+++ account_statement_import/__manifest__.py
@@ -0,0 +1,26 @@
+# Copyright 2004-2020 Odoo S.A.
+# Copyright 2020 Akretion France (http://www.akretion.com/)
+# @author: Alexis de Lattre <alexis.delattre@akretion.com>
+# Licence LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl-3.0).
+
+{
+    "name": "Import Statement Files",
+    "category": "Accounting",
+    "version": "14.0.1.2.3",
+    "depends": ["account", "account_bank_statement_import"],
+    "author": "Odoo SA, Akretion, Odoo Community Association (OCA)",
+    "maintainers": ["alexis-via"],
+    "development_status": "Mature",
+    'license': 'LGPL-3',
+    "website": "https://github.com/OCA/bank-statement-import",
+    "data": [
+        "security/ir.model.access.csv",
+        "wizard/account_statement_import_view.xml",
+        "views/account_journal.xml",
+        "views/account_bank_statement_line.xml",
+    ],
+    "demo": [
+        "demo/partner_bank.xml",
+    ],
+    "installable": True,
+}
---
+++ account_statement_import_fr_cfonb/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2014-2020 Akretion France (http://www.akretion.com)
+# @author Alexis de Lattre <alexis.delattre@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Import French CFONB Bank Statements",
+    "version": "15.0.1.1.0",
+    "license": "AGPL-3",
+    "author": "Akretion,Odoo Community Association (OCA)",
+    "maintainers": ["alexis-via"],
+    "website": "https://github.com/OCA/l10n-france",
+    "summary": "Import CFONB bank statements files in Odoo",
+    "depends": ["account_statement_import"],
+    "data": ["views/account_statement_import.xml"],
+    "installable": True,
+}
