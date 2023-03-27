9c78e63df55a6c4aec0664402c42411aa0dcdb9b
AG-AbdellahJ <abdellah.jrondi@auguria.fr>
2023-03-28 00:07:49 +0200

    [16][UDP] account_statement_import fixe test issue to parse if check

--- account_statement_import/wizard/setup_wizards.py
+++ account_statement_import/wizard/setup_wizards.py
@@ -10,10 +10,6 @@ class AccountSetupBankManualConfig(models.TransientModel):
     def validate(self):
         """Default the bank statement source of new bank journals as 'file_import'"""
         super().validate()
-        if (
-            self.num_journals_without_account == 0
-            or self.linked_journal_id.bank_statements_source == "undefined"
-        ) and self.env[
-            "account.journal"
-        ]._get_bank_statements_available_import_formats():
+        if (self.num_journals_without_account == 0 or self.linked_journal_id.bank_statements_source == "undefined") \
+            and self.env["account.journal"]._get_bank_statements_available_import_formats():
             self.linked_journal_id.bank_statements_source = "file_import"
