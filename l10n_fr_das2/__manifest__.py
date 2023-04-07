c289741c53cd0c9e62f8f5a77913a995573fe5b4
Alexis de Lattre <alexis.delattre@akretion.com>
2023-04-08 00:07:05 +0200

    [MIG] l10n_fr_das2 and l10n_fr_cog to v15
    Improve DAS2 readme

--- l10n_fr_das2/__manifest__.py
+++ l10n_fr_das2/__manifest__.py
@@ -1,10 +1,10 @@
-# Copyright 2020-2021 Akretion France (http://www.akretion.com/)
+# Copyright 2020-2023 Akretion France (http://www.akretion.com/)
 # @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "DAS2",
-    "version": "14.0.2.0.0",
+    "version": "15.0.1.0.0",
     "category": "Invoicing Management",
     "license": "AGPL-3",
     "summary": "DAS2 (France)",
