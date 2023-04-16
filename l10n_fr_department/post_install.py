051b63baee1174c9ede12ff63081a66b59a56058
Florent de Labarre <florent.mirieu@gmail.com>
2023-04-16 06:07:13 +0200

    [FIX] l10n_fr_department: fast install
    Before this commit with a large DB it can take some hours, after a few minutes.

--- l10n_fr_department/post_install.py
+++ l10n_fr_department/post_install.py
@@ -1,7 +1,7 @@
 # Copyright 2014-2020 Akretion France (http://www.akretion.com/)
 # @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-
+from collections import defaultdict
 from odoo import SUPERUSER_ID, api
 
 
@@ -17,10 +17,32 @@ def set_department_on_partner(cr, registry):
         fr_countries = env["res.country"].search(
             [("code", "in", ("FR", "GP", "MQ", "GF", "RE", "YT"))]
         )
-        partners = (
-            env["res.partner"]
-            .with_context(active_test=False)
-            .search([("country_id", "in", fr_countries.ids)])
-        )
-        partners._compute_department()
-    return
+        rcdo = env["res.country.department"]
+        map_department = dict((d["code"], d["id"]) for d in rcdo.search_read(
+            [("country_id", "in", fr_countries.ids)], ["code", "id"]))
+
+        partners = [(p["id"], p["zip"])
+                    for p in env["res.partner"]
+                    .with_context(active_test=False)
+                    .search_read([("country_id", "in", fr_countries.ids),
+                                  ("zip", '!=', False)],
+                                 ["id", "zip"])
+                    ]
+
+        map_partners = defaultdict(set)
+
+        for partner_id, zipcode in partners:
+            if len(zipcode) == 5:
+                map_partners[zipcode].add(partner_id)
+
+        for zipcode, partner_ids in map_partners.items():
+            zipcode = zipcode.strip().replace(" ", "").rjust(5, "0")
+            code = env['res.partner']._fr_zipcode_to_department_code(zipcode)
+            dpt_id = map_department.get(code, False)
+            if not dpt_id:
+                continue
+            for sub_ids in env.cr.split_for_in_conditions(partner_ids):
+                env.cr.execute(
+                    "UPDATE res_partner SET department_id=%s WHERE id IN %s",
+                    (dpt_id, tuple(sub_ids)),
+                )
