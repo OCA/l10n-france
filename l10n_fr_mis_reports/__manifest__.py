b953e9c06cf68e9a219100590c79fecfa65d9a4b
Alexis de Lattre <alexis.delattre@akretion.com>
2023-05-04 16:12:39 +0200

    [MIG] l10n_fr_mis_reports from v14 to v16
    Change the horrible yellow colors to grey colors

--- l10n_fr_mis_reports/__manifest__.py
+++ l10n_fr_mis_reports/__manifest__.py
@@ -1,10 +1,10 @@
-# Copyright 2015-2020 Akretion (http://www.akretion.com)
+# Copyright 2015-2023 Akretion (http://www.akretion.com)
 # @author Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "MIS reports for France",
-    "version": "14.0.1.1.0",
+    "version": "16.0.1.0.0",
     "category": "Accounting & Finance",
     "license": "AGPL-3",
     "summary": "MIS Report templates for the French P&L and Balance Sheets",
