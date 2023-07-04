b65400fa145d152293d952229d47a5cd49990d8b
Alexis de Lattre <alexis.delattre@akretion.com>
2023-07-04 10:37:41 +0200

    [ADD] module l10n_fr_pos_caisse_ap_ip

---
+++ l10n_fr_pos_caisse_ap_ip/static/src/js/models.js
@@ -0,0 +1,15 @@
+/*
+    Copyright 2023 Akretion (www.akretion.com)
+    @author: Alexis de Lattre <alexis.delattre@akretion.com>
+    @author: Rémi de Lattre <remi@miluni.fr>
+    License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+*/
+
+odoo.define("pos_caisse_ap_ip.models", function (require) {
+    "use strict";
+
+    var models = require("point_of_sale.models");
+    var CaisseAPIP = require("pos_caisse_ap_ip.payment");
+    models.register_payment_method("caisse_ap_ip", CaisseAPIP);
+
+});
