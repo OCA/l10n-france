f35c1d9e32cbcf646086987275a632cbfe06c8d2
Alexis de Lattre <alexis.delattre@akretion.com>
2023-07-05 00:13:29 +0200

    l10n_fr_pos_caisse_ap_ip: Add fr translation
    Update for pre-commit

--- l10n_fr_pos_caisse_ap_ip/__manifest__.py
+++ l10n_fr_pos_caisse_ap_ip/__manifest__.py
@@ -3,17 +3,17 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'POS: Caisse-AP payment protocol for France',
-    'version': '16.0.1.0.0',
-    'category': 'Point of Sale',
-    'license': 'AGPL-3',
-    'summary': 'Add support for Caisse-AP payment protocol used in France',
-    'author': 'Akretion,Odoo Community Association (OCA)',
-    'maintainers': ['alexis-via'],
-    'website': 'https://github.com/OCA/l10n-france',
-    'depends': ['point_of_sale'],
-    'data': [
-        'views/pos_payment_method.xml',
+    "name": "POS: Caisse-AP payment protocol for France",
+    "version": "16.0.1.0.0",
+    "category": "Point of Sale",
+    "license": "AGPL-3",
+    "summary": "Add support for Caisse-AP payment protocol used in France",
+    "author": "Akretion,Odoo Community Association (OCA)",
+    "maintainers": ["alexis-via"],
+    "website": "https://github.com/OCA/l10n-france",
+    "depends": ["point_of_sale"],
+    "data": [
+        "views/pos_payment_method.xml",
     ],
     "assets": {
         "point_of_sale.assets": [
@@ -21,5 +21,5 @@
             "l10n_fr_pos_caisse_ap_ip/static/src/js/payment_caisse_ap_ip.js",
         ],
     },
-    'installable': True,
+    "installable": True,
 }
