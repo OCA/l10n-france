a73c52852c4a745d1d627d8549683ef8dfd5d7cb
Alexis de Lattre <alexis.delattre@akretion.com>
2023-09-18 17:27:56 +0200

    l10n_fr_chorus_account: add timeout on HTTPs requests

--- l10n_fr_chorus_account/models/res_company.py
+++ l10n_fr_chorus_account/models/res_company.py
@@ -25,6 +25,7 @@ QUALIF_API_URL = "https://sandbox-api.piste.gouv.fr"
 TOKEN_URL = "https://oauth.piste.gouv.fr/api/oauth/token"
 QUALIF_TOKEN_URL = "https://sandbox-oauth.piste.gouv.fr/api/oauth/token"
 MARGIN_TOKEN_EXPIRY_SECONDS = 240
+TIMEOUT = 30
 
 
 class ResCompany(models.Model):
@@ -159,6 +160,7 @@ class ResCompany(models.Model):
                     "client_secret": oauth_secret,
                     "scope": "openid",
                 },
+                timeout=TIMEOUT,
             )
             logger.debug("_get_new_token HTTP answer code=%s", r.status_code)
         except requests.exceptions.ConnectionError as e:
@@ -259,7 +261,11 @@ class ResCompany(models.Model):
         logger.info("Payload of the Chorus POST request: %s", payload)
         try:
             r = session.post(
-                url, verify=True, data=json.dumps(payload), headers=headers
+                url,
+                verify=True,
+                data=json.dumps(payload),
+                headers=headers,
+                timeout=TIMEOUT,
             )
         except requests.exceptions.ConnectionError as e:
             logger.error("Connection to %s failed. Error: %s", url, e)
