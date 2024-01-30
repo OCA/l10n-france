d3fd60a86e79a269036e7856b5ba0e08d4e2e6d2
Alexis de Lattre <alexis.delattre@akretion.com>
2024-01-30 13:10:22 +0100

    [IMP] l10n_fr_account_vat_return_teledec: add json as attachment, to keep the exact data sent

--- l10n_fr_account_vat_return_teledec/models/res_company.py
+++ l10n_fr_account_vat_return_teledec/models/res_company.py
@@ -45,3 +45,30 @@ class ResCompany(models.Model):
         help="If active, the request will be sent to the test serveur of "
         "Teledec.fr and nothing will be retransmitted to DGFiP.",
     )
+
+    def _test_fr_vat_create_company(
+        self, company_name=None, fr_vat_exigibility="on_invoice"
+    ):
+        company = super()._test_fr_vat_create_company(
+            company_name=company_name, fr_vat_exigibility=fr_vat_exigibility
+        )
+        legal_rep_partner = self.env["res.partner"].create(
+            {
+                "is_company": False,
+                "parent_id": company.partner_id.id,
+                "type": "contact",
+                "title": self.env.ref("base.res_partner_title_madam").id,
+                "name": "Stéphanie Dupont",
+                "function": "Gérante",
+                "phone": "+33 4 78 23 23 23",
+                "email": "stephanie.dupont@example.com",
+            }
+        )
+        company.write(
+            {
+                "fr_vat_teledec_legal_representative_id": legal_rep_partner.id,
+                "fr_vat_teledec_legal_form": "SRL",
+                "fr_vat_teledec_test_mode": True,
+            }
+        )
+        return company
