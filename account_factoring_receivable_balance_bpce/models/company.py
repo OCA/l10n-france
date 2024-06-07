e996be4b68f9893315629b56a44599810d6bdc20
Daniel Reis <dreis@opensourceintegrators.com>
2024-06-07 11:49:27 +0100

    [REF] account_factoring_receivable_balance_bpce: moved code from the parent module

--- account_factoring_receivable_balance_bpce/models/company.py
+++ account_factoring_receivable_balance_bpce/models/company.py
@@ -4,8 +4,9 @@
 
 import logging
 
-from odoo import _, api, fields, models
+from odoo import Command, _, api, fields, models
 from odoo.exceptions import RedirectWarning, UserError
+from odoo.tests import Form
 
 logger = logging.getLogger(__name__)
 
@@ -13,12 +14,63 @@ logger = logging.getLogger(__name__)
 class ResCompany(models.Model):
     _inherit = "res.company"
 
+    factor_config_currency_id = fields.Many2one(
+        comodel_name="res.currency",
+        string="Facto Currency",
+        help="Use to configure account and journal",
+    )
     bpce_factor_code = fields.Char(size=6, help="N° de compte chez PBCE")
     bpce_start_date = fields.Date(
         string="Start Date",
         tracking=True,
         help="No account move will be selected before this date",
     )
+
+    @api.model
+    def _create_french_company(self, company_name=None):
+        """
+        Method to use with the cmd line (demo purpose)
+        Can be called from Odoo Shell
+        """
+        demo_cpny_name = "BPCE demo"
+        previous_cpny = self.search([("name", "=", demo_cpny_name)], limit=1)
+        previous_cpny.write({"name": "Company %s" % previous_cpny.id})
+        company = self.create(
+            {
+                "name": company_name or demo_cpny_name,
+                "street": "42 rue du logiciel libre",
+                "zip": "69009",
+                "city": "Lyon",
+                "country_id": self.env.ref("base.fr").id,
+                "siret": "77788899100018",
+                "vat": "FR51777888991",
+            }
+        )
+        self.env.ref("l10n_fr.l10n_fr_pcg_chart_template")._load(20.0, 20.0, company)
+        self.env.ref("base.user_admin").company_ids = [Command.link(company.id)]
+        logger.info("Company %s" % company.name)
+        for currency in self.env["res.currency"].search([]):
+            logger.info("Journal and account for factor to create")
+            company._configure_bpce_factoring(currency=currency.name)
+        return company
+
+    def _prepare_data_for_factor(self, move_type="out_invoice"):
+        self.ensure_one()
+        move_form = Form(
+            self.env["account.move"]
+            .with_company(self.env.company)
+            .with_context(
+                default_move_type=move_type,
+                account_predictive_bills_disable_prediction=True,
+            )
+        )
+        move_form.invoice_date = fields.Date.from_string("2022-10-03")
+        move_form.date = move_form.invoice_date
+        move_form.partner_id = self.env.ref("base.res_partner_2")
+
+    def ui_populate_data_for_factor(self):
+        raise UserError(_("Not yet implemented"))
+        # self._prepare_data_for_factor()
 
     def ui_configure_bpce_factoring_balance(self):
         self.ensure_one()
@@ -49,17 +101,6 @@ class ResCompany(models.Model):
                 "next": action_id,
             },
         }
-
-    @api.model
-    def _create_french_company(self, company_name=None):
-        "Method to use with the cmd line (demo purpose)"
-        # env['res.company']._create_french_company("hi")
-        company = super()._create_french_company(company_name=company_name)
-        logger.info("Company %s" % company.name)
-        for currency in self.env["res.currency"].search([]):
-            logger.info("Journal and account for factor to create")
-            company._configure_bpce_factoring(currency=currency.name)
-        return company
 
     @api.model
     def _configure_bpce_factoring(self, currency):
