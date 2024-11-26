6d5d4d21a51e2c86b660854501b6b4cee033dc74
Alexis de Lattre <alexis.delattre@akretion.com>
2024-11-26 22:24:22 +0100

    [IMP] l10n_fr_state: use with statement for file_open()

--- l10n_fr_state/pre_init_hook.py
+++ l10n_fr_state/pre_init_hook.py
@@ -26,35 +26,35 @@ def create_fr_state_xmlid(env):
 
 def generic_create_state_xmlid(env, module_name, data_file):
     """This method is also used by l10n_fr_state and l10n_fr_department_oversea"""
-    f = file_open(f"{module_name}/{data_file}", "rb")
-    xml_root = etree.parse(f)
-    data = {}  # key = xmlid, value = {"code": "GP", "country_id": "base.gp"}
-    for record in xml_root.xpath("//record"):
-        xmlid = record.attrib["id"]
-        data[xmlid] = {}
-        for xfield in record.xpath("field"):
-            xfield_dict = xfield.attrib
-            data[xmlid][xfield_dict["name"]] = xfield_dict.get("ref") or xfield.text
-    logger.debug("generic_create_state_xmlid data=%s", data)
-    for xmlid, state_data in data.items():
-        country_id = env.ref(state_data["country_id"]).id
-        state = env["res.country.state"].search(
-            [("code", "=", state_data["code"]), ("country_id", "=", country_id)],
-            limit=1,
-        )
-        if state:
-            env["ir.model.data"].create(
-                {
-                    "name": xmlid,
-                    "res_id": state.id,
-                    "module": module_name,
-                    "model": "res.country.state",
-                }
+    with file_open(f"{module_name}/{data_file}", "rb") as f:
+        xml_root = etree.parse(f)
+        data = {}  # key = xmlid, value = {"code": "GP", "country_id": "base.gp"}
+        for record in xml_root.xpath("//record"):
+            xmlid = record.attrib["id"]
+            data[xmlid] = {}
+            for xfield in record.xpath("field"):
+                xfield_dict = xfield.attrib
+                data[xmlid][xfield_dict["name"]] = xfield_dict.get("ref") or xfield.text
+        logger.debug("generic_create_state_xmlid data=%s", data)
+        for xmlid, state_data in data.items():
+            country_id = env.ref(state_data["country_id"]).id
+            state = env["res.country.state"].search(
+                [("code", "=", state_data["code"]), ("country_id", "=", country_id)],
+                limit=1,
             )
-            logger.info(
-                "XMLID %s.%s created for state %s code %s",
-                module_name,
-                xmlid,
-                state.name,
-                state.code,
-            )
+            if state:
+                env["ir.model.data"].create(
+                    {
+                        "name": xmlid,
+                        "res_id": state.id,
+                        "module": module_name,
+                        "model": "res.country.state",
+                    }
+                )
+                logger.info(
+                    "XMLID %s.%s created for state %s code %s",
+                    module_name,
+                    xmlid,
+                    state.name,
+                    state.code,
+                )
