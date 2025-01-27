50db29fd2b9a488da7260e8ac75224eb78ee47db
Alexis de Lattre <alexis.delattre@akretion.com>
2025-01-27 15:07:21 +0100

    [MIG] l10n_fr_account_banking_pain_base: to v18
    Standard migration from Odoo v16 to Odoo v18

--- l10n_fr_account_banking_pain_base/__manifest__.py
+++ l10n_fr_account_banking_pain_base/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "France ISO 20022/PAIN",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Accounting",
     "license": "AGPL-3",
     "summary": "Regulatory reporting codes for ISO 20022/PAIN files",
