532e7744fdcb924a051b871f04bbcb8963a62212
Alexis de Lattre <alexis.delattre@akretion.com>
2025-02-01 01:36:10 +0100

    [MIG] l10n_fr_intrastat_product: migrate to v18

--- l10n_fr_intrastat_product/models/stock_location.py
+++ l10n_fr_intrastat_product/models/stock_location.py
@@ -2,18 +2,7 @@
 # @author Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import _, models
-from odoo.exceptions import UserError
-
-
-class StockWarehouse(models.Model):
-    _inherit = "stock.warehouse"
-
-    def _get_fr_department(self):
-        self.ensure_one()
-        if not self.partner_id:
-            raise UserError(_("Missing partner on warehouse '%s'.") % self.display_name)
-        return self.partner_id.country_department_id
+from odoo import models
 
 
 class StockLocation(models.Model):
@@ -22,7 +11,7 @@ class StockLocation(models.Model):
     def _get_fr_department(self):
         self.ensure_one()
         warehouse = self.env["stock.warehouse"].search(
-            [("lot_stock_id", "parent_of", self.ids)], limit=1
+            [("lot_stock_id", "parent_of", self.id)], limit=1
         )
         if warehouse:
             return warehouse._get_fr_department()
