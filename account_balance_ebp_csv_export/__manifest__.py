aa0fe6975ef5a0d6b57c564f5e70a4033c61029f
Alexis de Lattre <alexis.delattre@akretion.com>
2025-02-06 21:41:13 +0100

    [MIG] account_balance_ebp_csv_export to v18
    Standard migration from v16 to v18
    Add screenshot and create readme subdir

--- account_balance_ebp_csv_export/__manifest__.py
+++ account_balance_ebp_csv_export/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Account Balance EBP CSV export",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Accounting",
     "license": "AGPL-3",
     "author": "Akretion,Odoo Community Association (OCA)",
