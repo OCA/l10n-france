6960e93cf3c8e9472da134d54f652ef28d983f05
Alexis de Lattre <alexis.delattre@akretion.com>
2025-04-25 15:29:47 +0200

    [IMP] l10n_fr_chorus_*: chorus service source can be read from another source
    Don't hard-code chorus service source from
    invoice_partner.fr_chorus_service_id. Allows to have another source for
    chorus service by inheriting l10n_fr_chorus_account.

--- l10n_fr_chorus_sale/models/sale_order.py
+++ l10n_fr_chorus_sale/models/sale_order.py
@@ -17,8 +17,15 @@ class SaleOrder(models.Model):
             order._chorus_validation_checks()
         return super().action_confirm()
 
+    def _get_chorus_service(self):
+        self.ensure_one()
+        return self.partner_invoice_id.fr_chorus_service_id
+
     def _chorus_validation_checks(self):
         self.ensure_one()
         self.company_id._chorus_common_validation_checks(
-            self, self.partner_invoice_id, self.client_order_ref
+            self,
+            self.partner_invoice_id,
+            self.client_order_ref,
+            self._get_chorus_service(),
         )
