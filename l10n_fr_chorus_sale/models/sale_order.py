06a876212839a05a184488e603d1c2864d6b02fb
Alexis de Lattre <alexis.delattre@akretion.com>
2025-04-25 21:28:46 +0200

    [IMP] chorus: show service code in invoice and sale.order view via a dedicated field

--- l10n_fr_chorus_sale/models/sale_order.py
+++ l10n_fr_chorus_sale/models/sale_order.py
@@ -8,6 +8,17 @@ from odoo import models
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
+    # The related field below should be native... I hope we won't have conflict issues
+    # if another module defines the same related field.
+    invoice_sending_method = fields.Selection(
+        related="partner_id.commercial_partner_id.invoice_sending_method", store=True
+    )
+    chorus_service_code = fields.Char(
+        related="partner_invoice_id.fr_chorus_service_id.code",
+        string="Chorus Service Code",
+        store=True,
+    )
+
     def action_confirm(self):
         """Check validity of Chorus orders"""
         for order in self.filtered(
