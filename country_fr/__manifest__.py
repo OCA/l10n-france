f44f1528ca4d2c595f13c1658371ccb839d0f92c
David Beal <david.beal@akretion.com>
2025-05-22 12:36:59 +0200

    [18.0][ADD] country_fr module: allow to load french Chart of account without manual settings

---
+++ country_fr/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Country Fr",
+    "summary": "Set FR country on `base.main_company` record "
+    "to set up French localisation.",
+    "version": "18.0.1.0.0",
+    "author": "Akretion, Odoo Community Association (OCA)",
+    "development_status": "Beta",
+    "category": "Localisation",
+    "website": "https://github.com/OCA/l10n-france",
+    "license": "AGPL-3",
+    "depends": [
+        "base",
+    ],
+    "data": ["partner.xml"],
+    "installable": True,
+}
---
+++ country_fr/readme/DESCRIPTION.md
@@ -0,0 +1,7 @@
+Apply French Chart of Account to main company when installed before `account` module.
+
+To achieve this target current module set FR country `base.main_company` on partner.
+
+Account module will do the rest of the job.
+
+It has no effect if `account` module is already installed.
