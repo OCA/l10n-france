2991fbfa3fdf38979566d20a65ee24cd2785923b
Alexis de Lattre <alexis.delattre@akretion.com>
2025-05-26 12:14:08 +0200

    [MIG] l10n_fr_account_vat_return_einvoice_generate: migrate from v16 to v18

--- l10n_fr_account_vat_return_einvoice_generate/__manifest__.py
+++ l10n_fr_account_vat_return_einvoice_generate/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Adapt e-invoice generation to France VAT on payment",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Accounting",
     "license": "AGPL-3",
     "summary": "Glue module between account_einvoice_generate and "
