e5dcad564c11dd9ea43e73337bbc44eb060d42df
juancarlosonate-tecnativa <juancarlos.onate@tecnativa.com>
2025-07-11 12:19:43 +0200

    [FIX] l10n_fr_intrastat_product: Remove error blocking region code when warehouse has no partner
    TT55417

--- l10n_fr_intrastat_product/models/stock.py
+++ l10n_fr_intrastat_product/models/stock.py
@@ -2,8 +2,7 @@
 # @author Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import _, models
-from odoo.exceptions import UserError
+from odoo import models
 
 
 class StockWarehouse(models.Model):
@@ -11,9 +10,7 @@ class StockWarehouse(models.Model):
 
     def _get_fr_department(self):
         self.ensure_one()
-        if not self.partner_id:
-            raise UserError(_("Missing partner on warehouse '%s'.") % self.display_name)
-        return self.partner_id.country_department_id
+        return self.partner_id.country_department_id if self.partner_id else None
 
 
 class StockLocation(models.Model):
