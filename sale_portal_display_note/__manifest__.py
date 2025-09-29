b108a2b3a299fa03a5ab6d1893e2508fb46a4a0a
DUCIEL Romain <romain@mokatourisme.fr>
2025-09-29 16:07:58 +0200

    [ADD] sale_portal_display_note

---
+++ sale_portal_display_note/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2024 Moka
+# @author Damien Horvat <damien@moka.cloud>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "France - TVA sur marge",
+    "summary": "l10n_fr_vat_on_margin",
+    "version": "16.0.1.0.0",
+    "author": "Moka",
+    "website": "https://moka.cloud",
+    "license": "AGPL-3",
+    "category": "Accounting",
+    "depends": [
+        "sale",
+        ],
+    "data": [
+        'views/sale_portal_views.xml',
+    ],
+}
