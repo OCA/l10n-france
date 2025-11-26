ef5d9f895d4ea430815110028d6a7b6ba5741fe1
Alexis de Lattre <alexis.delattre@akretion.com>
2025-11-26 23:55:59 +0100

    [FIX] l10n_fr_account_invoice_import_facturx: set siren/siret on partner created from 'create or update wizard'

--- l10n_fr_account_invoice_import_facturx/wizard/account_invoice_import.py
+++ l10n_fr_account_invoice_import_facturx/wizard/account_invoice_import.py
@@ -2,7 +2,7 @@
 # @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models
+from odoo import api, models
 
 
 class AccountInvoiceImport(models.TransientModel):
@@ -23,3 +23,26 @@ class AccountInvoiceImport(models.TransientModel):
             "/ram:ID[@schemeID='0002']"
         ]
         return xpathd
+
+    # If one day we have a module l10n_fr_account_invoice_import
+    # we could move the inherit of _prepare_new_partner_context() there
+    @api.model
+    def _prepare_create_invoice_no_partner(self, parsed_inv, import_config, vals):
+        res = super()._prepare_create_invoice_no_partner(
+            parsed_inv, import_config, vals
+        )
+        if (
+            vals.get("import_partner_data")
+            and isinstance(vals["import_partner_data"], dict)
+            and parsed_inv.get("partner")
+        ):
+            if parsed_inv["partner"].get("siren"):
+                vals["import_partner_data"]["siren"] = parsed_inv["partner"]["siren"]
+            elif parsed_inv["partner"].get("siret"):
+                vals["import_partner_data"]["siren"] = parsed_inv["partner"]["siret"][
+                    :9
+                ]
+                vals["import_partner_data"]["nic"] = parsed_inv["partner"]["siret"][
+                    9:14
+                ]
+        return res
