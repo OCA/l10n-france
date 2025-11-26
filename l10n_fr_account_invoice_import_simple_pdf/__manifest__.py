745cb150a17198d74ce6a83855c22a7f04b237c3
Alexis de Lattre <alexis.delattre@akretion.com>
2025-11-27 00:03:01 +0100

    [MIG] l10n_fr_account_invoice_import_facturx, l10n_fr_account_invoice_import_simple_pdf and l10n_fr_business_document_import from 16.0 to 18.0
    FIX in l10n_fr_business_document_import: _check_company() updated to changes in base_business_document_import
    FIX l10n_fr_account_invoice_import_facturx: set siren/siret on partner created from 'create or update wizard'

--- l10n_fr_account_invoice_import_simple_pdf/__manifest__.py
+++ l10n_fr_account_invoice_import_simple_pdf/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Account Invoice Import Simple PDF - France",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Accounting",
     "license": "AGPL-3",
     "summary": "Invoice import simple PDF: match partners using SIREN",
