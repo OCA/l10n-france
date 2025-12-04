4ea2b621812524ffed24efed3759a28d163e22df
Alexis de Lattre <alexis.delattre@akretion.com>
2025-12-04 17:37:55 +0100

    [IMP] l10n_fr_account_invoice_import_facturx: update siren/siret in the partner update wizard
    Remove dead code

--- l10n_fr_account_invoice_import_facturx/wizard/account_invoice_import.py
+++ l10n_fr_account_invoice_import_facturx/wizard/account_invoice_import.py
@@ -23,14 +23,3 @@ class AccountInvoiceImport(models.TransientModel):
             "/ram:ID[@schemeID='0002']"
         ]
         return xpathd
-
-    # If one day we have a module l10n_fr_account_invoice_import
-    # we could move the inherit of _prepare_new_partner_context() there
-    def _prepare_new_partner_context(self, parsed_inv):
-        context = super()._prepare_new_partner_context(parsed_inv)
-        if parsed_inv["partner"].get("siren"):
-            context["default_siren"] = parsed_inv["partner"]["siren"]
-        elif parsed_inv["partner"].get("siret"):
-            context["default_siren"] = parsed_inv["partner"]["siret"][:9]
-            context["default_nic"] = parsed_inv["partner"]["siret"][9:14]
-        return context
