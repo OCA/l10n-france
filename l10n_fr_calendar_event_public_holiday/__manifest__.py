8a6dbc764ee3c9487500bd30659533b8b0c58bca
David Beal <david.beal@akretion.com>
2026-01-08 18:05:47 +0100

    [18.0][ADD] l10n_fr_calendar_event_public_holiday

---
+++ l10n_fr_calendar_event_public_holiday/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    "name": "French Calendar Event Public Holiday",
+    "summary": "French Calendar Event Holiday",
+    "version": "18.0.1.0.0",
+    "development_status": "Alpha",
+    "category": "Calendar",
+    "website": "https://github.com/OCA/l10n-france",
+    "author": "Akretion, Odoo Community Association (OCA)",
+    "maintainers": ["bealdav"],
+    "license": "AGPL-3",
+    "external_dependencies": {
+        "python": [],
+    },
+    "depends": [
+        "calendar",
+    ],
+    "data": [
+        "data/partner.xml",
+    ],
+    "post_init_hook": "post_init_hook",
+}
---
+++ l10n_fr_calendar_event_public_holiday/readme/DESCRIPTION.md
@@ -0,0 +1,3 @@
+Fill calendar events for French public holidays to 'Jours Fériés' partner.
+
+Allow to display public holidays in calendar views.
