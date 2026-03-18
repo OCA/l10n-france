9b4274228bda3cfae7e1f048727ac81ac85f175f
Alexis de Lattre <alexis.delattre@akretion.com>
2026-03-18 16:45:38 +0100

    [FIX] l10n_fr_account_vat_return: migration script for field on account.tax

--- l10n_fr_account_vat_return/__manifest__.py
+++ l10n_fr_account_vat_return/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "France VAT Return",
-    "version": "18.0.2.0.0",
+    "version": "18.0.2.0.1",
     "category": "Accounting",
     "license": "AGPL-3",
     "summary": "VAT return for France: CA3, 3310-A, 3519",
