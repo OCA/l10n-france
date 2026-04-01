00d8747905f5986b78af849803af449bc3ac40da
Alexis de Lattre <alexis.delattre@akretion.com>
2026-04-01 12:02:07 +0200

    [FIX] l10n_fr_intrastat_product: remove inherit of field total_amount

--- l10n_fr_intrastat_product/__manifest__.py
+++ l10n_fr_intrastat_product/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "EMEBI",
-    "version": "18.0.1.1.0",
+    "version": "18.0.2.0.0",
     "category": "Localisation/Report Intrastat",
     "license": "AGPL-3",
     "summary": "EMEBI (ex-DEB) for France",
