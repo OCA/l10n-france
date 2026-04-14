0a5efb56febb16ceaf841fc4894ca37594473cee
Alexis de Lattre <alexis.delattre@akretion.com>
2026-04-15 00:50:48 +0200

    [UDP] EMEBI 2026 requires a fake VAT for B2C regime 29

--- l10n_fr_intrastat_product/models/intrastat_product_declaration.py
+++ l10n_fr_intrastat_product/models/intrastat_product_declaration.py
@@ -405,6 +405,14 @@ class IntrastatProductComputationLine(models.Model):
             vals["amount_company_currency"] = int(
                 round(vals["amount_company_currency"])
             )
+            if (
+                self[0].transaction_id
+                and self[0].transaction_id.code == "12"
+                and self[0].fr_regime_id
+                and self[0].fr_regime_id.code == "29"
+                and not self[0].vat
+            ):
+                vals["vat"] = "QN999999999999"
         return vals
 
 
@@ -480,10 +488,10 @@ class IntrastatProductDeclarationLine(models.Model):
                 _("Missing fiscal value on declaration line %d.") % self.line_number
             )
         item.invoicedAmount = str(int(round(self.amount_company_currency)))
-        # EMEBI 2022 : Partner VAT now required for all dispatches with
-        # some exceptions for regime 29 in case of B2C
+        # EMEBI 2026 : Partner VAT now required for all dispatches
+        # including regime 29
         if decl.declaration_type == "dispatches":
-            if not self.vat and regime.code != "29":
+            if not self.vat:
                 raise UserError(
                     _("Missing VAT number on declaration line %d.") % self.line_number
                 )
