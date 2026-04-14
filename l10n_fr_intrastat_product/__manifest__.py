74211b49e2673a9743b1e72455955be4c59e8855
Alexis de Lattre <alexis.delattre@akretion.com>
2026-04-15 00:50:48 +0200

    [UDP] EMEBI 2026 requires a fake VAT for B2C regime 29

--- l10n_fr_intrastat_product/__manifest__.py
+++ l10n_fr_intrastat_product/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "EMEBI",
-    "version": "18.0.2.0.0",
+    "version": "18.0.2.1.0",
     "category": "Localisation/Report Intrastat",
     "license": "AGPL-3",
     "summary": "EMEBI (ex-DEB) for France",
