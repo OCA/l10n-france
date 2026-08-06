d57cee754e37b10237cf345e76fbab1c0d1a5760
Rémi - Le Filament <30716308+remi-filament@users.noreply.github.com>
2026-08-06 17:26:59 +0200

    [ADD] l10n_fr_account_edi_ubl_cii

---
+++ l10n_fr_account_edi_ubl_cii/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "France - UBL/CII formats",
+    "version": "18.0.1.0.0",
+    "category": "Accounting/Localizations/EDI",
+    "author": "Le Filament, OCA France, Odoo Community Association (OCA), Odoo SA",
+    "maintainers": ["remi-filament"],
+    "website": "https://github.com/OCA/l10n-france",
+    "license": "LGPL-3",
+    "depends": [
+        "l10n_fr_account",
+        "account_edi_ubl_cii_tax_extension",
+    ],
+    "data": [],
+    "post_init_hook": "_post_init",
+    "uninstall_hook": "_uninstall_hook",
+    "development_status": "Beta",
+}
---
+++ l10n_fr_account_edi_ubl_cii/readme/DESCRIPTION.md
@@ -0,0 +1,6 @@
+This module extends account_edi_ubl_cii for France.
+In particular, it adds a new format UBL 2.1 specific for France and extends Factur-x format for France.
+
+This module also adds a few functions on account.move, res.company and res.partner models useful for France e-invoicing.
+
+Most of this module comes from Odoo SA module l10n_fr_pdp from Odoo CE edition version 18.0
